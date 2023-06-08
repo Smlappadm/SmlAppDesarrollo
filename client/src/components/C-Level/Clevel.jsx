fa47f0dfb6f3ed7117e6f126b03d5d26d505779d
Jonavoe <voeffray.jonathan@gmail.com>
2023-06-07 21:21:03 -0300

    Update Clevel.jsx

--- client/src/components/C-Level/Clevel.jsx
+++ client/src/components/C-Level/Clevel.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import Nav from "../Nav/Nav";
 import { TableClevel } from "./Employees/TableEmployees/TableClevel";
-import Detail from "./Employees/Detail/Detail";
 
 function Clevel() {
   return (
     <>
       <Nav />
       <TableClevel />
-      {/* <Detail performance={true} /> */}
     </>
   );
 }
