72a25a7bddb01a292e4a04962355a2a246ddbcf1
Jonavoe <voeffray.jonathan@gmail.com>
2023-06-08 17:00:40 -0300

    Update Detail.jsx

--- client/src/components/C-Level/Employees/Detail/Detail.jsx
+++ client/src/components/C-Level/Employees/Detail/Detail.jsx
@@ -1,74 +1,61 @@
-import React, { useEffect, useState } from "react";
-import Performance from "../Performance/Performance";
-import About from "../About/About";
-import axios from "axios";
+import React from "react";
 
 function Detail({ cardEmail }) {
-  const [employ, setEmploy] = useState(null);
-
-  useEffect(() => {
-    if (cardEmail) {
-      axios
-        .get(`/employees/email/?email=${cardEmail}`)
-        .then((response) => {
-          const employData = response.data;
-          setEmploy(employData[0]);
-        })
-        .catch((error) => {
-          // Manejar el error de alguna manera
-        });
-    }
-  }, [cardEmail, setEmploy]);
-
-  console.log(employ);
-
+  console.log(cardEmail);
   return (
-    <div className="  flex bg-slate-700  justify-start items-center w-1/5 flex-col ">
+    <div className="flex bg-slate-700 justify-start items-center w-4/12 flex-col">
       {/* ********* ********* ********* ********* ********* ********* TARJETA DE USUARIO ********* ********* ********* *********  ********* ********* */}
       <div className=" h-1/4 justify-center items-center text-center flex flex-col w-full gap-2 relative">
-        <div className=" bg-emerald-700  w-20 h-20 rounded-full flex ">
+        <div className=" bg-emerald-700 w-20 h-20 rounded-full flex ">
           <img
-            src={employ && employ.photo}
+            src={cardEmail && cardEmail.photo}
             alt="avatar"
-            className="rounded-full "
+            className="rounded-full"
           />
         </div>
       </div>
 
       {/* ********* ********* ********* ********* ********* *********  INFORMACION DEL USUARIO ********* ********* ********* ********* ********* *********  */}
-      <div className=" h-1/4 flex flex-col w-4/5 gap-2">
+      <div className=" h-1/4 flex flex-col w-4/5 gap-7">
         <div className="flex justify-center">
-          <h1 className=" text-18 text-gray-200">Informacion del empleado</h1>
+          <h1 className=" text-24 text-gray-200">Informacion del empleado</h1>
         </div>
         <div className="flex  gap-2 text-left items-center">
+          <h2 className="text-white text-18">Nombre: </h2>
           <p className="font-normal text-14 text-white pt-0">
-            Nombre: {employ && employ.name}
+            {cardEmail && cardEmail.name}
           </p>
         </div>
         <div className="flex  gap-2 text-left items-center">
+          <h2 className="text-white text-18">Pais: </h2>
           <p className="font-normal text-14 text-white pt-0">
-            Pais: {employ && employ.country}
+            {cardEmail && cardEmail.country}
           </p>
         </div>
         <div className="flex  gap-2 text-left items-center">
+          <h2 className="text-white text-18">Nacimiento: </h2>
           <p className="font-normal text-14 text-white pt-0">
-            Nacimiento: {employ && employ.birthdate}
+            {cardEmail && cardEmail.birthdate}
           </p>
         </div>
         <div className="flex  gap-2 text-left items-center">
-          <p className=" font-normal text-14 pt-0 text-white">
-            Telefono: {employ && employ.contactNumber}
+          <h2 className="text-white text-18">Telefono: </h2>
+          <p className=" font-normal text-14 pt-0 text-white">{cardEmail && cardEmail.contactNumber}
           </p>
         </div>
         <div className="flex  gap-2 text-left items-center">
+          <h2 className="text-white text-18">Email: </h2>
           <p className=" font-normal text-14  text-white pt-0">
-            Email: {employ && employ.email}
+            {cardEmail && cardEmail.email}
           </p>
         </div>
-        <div className="flex  gap-2 text-left items-center">
-          <p className=" font-normal text-14  text-white pt-0">
-            Sobre el: {employ && employ.description}
-          </p>
+        <div className="flex gap-2 text-left flex-col">
+          <h2 className="text-white text-18">Sobre el: </h2>
+          <div className="flex items-center justify-center ">
+            <p className=" font-normal text-14  text-white pt-0">
+              {cardEmail && cardEmail.description}
+            </p>
+          </div>
         </div>
       </div>
 
