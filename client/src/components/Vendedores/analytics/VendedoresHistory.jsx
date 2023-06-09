bf44413c24df193623f913fcd90420cfde10041b
juanfacundotam <facutam@gmail.com>
2023-06-09 15:17:38 -0300

    Update VendedoresHistory.jsx

--- client/src/components/Vendedores/analytics/VendedoresHistory.jsx
+++ client/src/components/Vendedores/analytics/VendedoresHistory.jsx
@@ -247,7 +247,7 @@ const VendedoresHistory = () => {
             </table>
           ) : (
             <div className="flex items-center justify-center w-full h-screen">
-              <h1>LEADS NOT FOUND...</h1>
+              <h1>No hay Leads disponibles</h1>
             </div>
           )}
         </div>
