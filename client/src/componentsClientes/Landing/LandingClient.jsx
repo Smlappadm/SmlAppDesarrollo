b9ec2cf27d70c83c999450c305d7594f4c35384b
juanfacundotam <facutam@gmail.com>
2023-06-13 17:33:15 -0300

    Update LandingClient.jsx

--- client/src/componentsClientes/Landing/LandingClient.jsx
+++ client/src/componentsClientes/Landing/LandingClient.jsx
@@ -16,6 +16,13 @@ export default function LandingClient() {
           maxNumber={`10K`}
         />
       </div>
+      <div className="">
+        <Followers
+          numberInstagram={2725}
+          numberTiktok={2500}
+          maxNumber={`10K`}
+        />
+      </div>
     </div>
   );
 }
