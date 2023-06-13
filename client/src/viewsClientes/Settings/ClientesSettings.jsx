0edd486fb400110ad0f022c7d1747e5c2e40d4ce
Jonavoe <voeffray.jonathan@gmail.com>
2023-06-13 19:54:10 -0300

    Update ClientesSettings.jsx

--- client/src/viewsClientes/Settings/ClientesSettings.jsx
+++ client/src/viewsClientes/Settings/ClientesSettings.jsx
@@ -4,20 +4,21 @@ import CustomsLabelSetting from "./CustomsLabelSetting/CustomsLabelSetting";
 
 export default function ClientesSettings() {
   const texto = "Ajustes de perfil"
-  const switchs = false
-  const texto1 = "Notifícaiones"
+  const texto1 = "Notificaiones"
   const switchs1 = true
   const texto2 = "Mis pagos"
-  const switchs2 = false
+  const texto3 = "Invitar a un amigo"
   return (
-    <div className="flex flex-col h-screen w-screen">
+    <div className="flex  mx-4 flex-col h-screen w-screen">
       <div className="flex items-end justify-between pt-4">
         <h2 className="font-bold">Personal</h2>
+
         <IoCloseSharp className="font-bold text-[#fff] text-[2rem]" />
       </div>
-      <CustomsLabelSetting text={texto} switchValue ={switchs} />
+      <CustomsLabelSetting text={texto} />
       <CustomsLabelSetting text={texto1} switchValue ={switchs1} />
-      <CustomsLabelSetting text={texto2} switchValue ={switchs2} />
+      <CustomsLabelSetting text={texto2} />
+      <CustomsLabelSetting text={texto3} />
     </div>
   );
 }
