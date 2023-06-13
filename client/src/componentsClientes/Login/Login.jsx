4f7d39fce67871f533f6686c6e720b3c302ab589
Nikitoo17 <nicolasfadel17@gmail.com>
2023-06-13 18:25:10 -0300

    Create Login.jsx

---
+++ client/src/componentsClientes/Login/Login.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+export default function Login({ handleOpenRegister, handleJoin }) {
+  return (
+    <div className="w-screen h-screen bg-[#020131] flex flex-col justify-start items-center">
+      <img
+        src="https://images-ext-1.discordapp.net/external/VmotedpeNAAv9Sz0GZI5iLiobf_7NpJn24pyas4ed_Y/https/i.postimg.cc/4y1YcByV/1685492595204-removebg-preview.webp"
+        alt=""
+      />
+      <div className="flex flex-col w-3/4 gap-y-4">
+        <label className="font-bold ml-2" htmlFor="">
+          Usuario:
+        </label>
+        <input
+          className="rounded-md bg-[#404062] h-7 pl-2"
+          type="text"
+          placeholder="Ingresar Usuario"
+        />
+        <label className="font-bold ml-2" htmlFor="">
+          Contraseña:
+        </label>
+        <input
+          className="rounded-md bg-[#404062] h-7 pl-2"
+          type="text"
+          placeholder="Ingresar Contraseña"
+        />
+        <div className="flex flex-col items-center gap-y-4 mt-4">
+          <button
+            className="bg-[#07a1f8] rounded-2xl px-3 text-black"
+            onClick={handleJoin}
+          >
+            Ingresar
+          </button>
+          <div className="flex ">
+            <p>¿No tienes cuenta?</p>
+            <button onClick={handleOpenRegister} className="text-blue-600 ml-1">
+              Crea una!
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
