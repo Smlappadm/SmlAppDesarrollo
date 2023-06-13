dd9a6efbbfef5d2fed4cda606aab940b6dc75eb0
juanfacundotam <facutam@gmail.com>
2023-06-13 19:50:40 -0300

    Update Home.jsx

--- client/src/viewsClientes/Home/Home.jsx
+++ client/src/viewsClientes/Home/Home.jsx
@@ -16,12 +16,12 @@ export default function Home() {
 
       
 
-      <div className="flex gap-8">
-        <button value="vistaGeneral" onClick={handleViewChange}>Vista General</button>
-        <button value="trofeosXP" onClick={handleViewChange}>Trofeos y XP</button>
+      <div className="flex gap-8 mt-7">
+        <button value="vistaGeneral" onClick={handleViewChange} className={optionView === "vistaGeneral" ? "border-2 border-[#07A1F8] bg-[#07A1F8] text-white px-3 py-0.5 rounded-full text-18" : " text-white px-3 py-0.5 rounded-full text-18 border-2"}>Vista general</button>
+        <button value="trofeosXP" onClick={handleViewChange} className={optionView === "trofeosXP"? "border-2 border-[#07A1F8] bg-[#07A1F8] text-white px-3 py-0.5 rounded-full text-18" : "text-white px-3 py-0.5 rounded-full text-18 border-2"}>Trofeos y XP</button>
       </div>
 
-      {optionView === "vistaGeneral" && <VistaGeneral/>}
+      {optionView === "vistaGeneral" && (<VistaGeneral/>)}
       {optionView === "trofeosXP" && <TrofeosXP/>}
 
     </div>
