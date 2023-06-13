06db76be4a2e7a8bc9353414d98ffd226315a545
Nikitoo17 <nicolasfadel17@gmail.com>
2023-06-13 20:20:22 -0300

    Update ClientesHandlers.js

--- api/src/Handlers/ClientesHandlers.js
+++ api/src/Handlers/ClientesHandlers.js
@@ -1,4 +1,5 @@
 const newClient = require("../controllers/Clientes/newClient");
+const loginClient = require("../controllers/Clientes/loginClient");
 
 const newClientHandler = async (req, res) => {
   const body = req.body;
@@ -9,5 +10,14 @@ const newClientHandler = async (req, res) => {
     res.status(404).json({ error: error.message });
   }
 };
+const loginClientHandler = async (req, res) => {
+  const { username } = req.query;
+  try {
+    const client = await loginClient(username);
+    res.status(200).json(client);
+  } catch (error) {
+    res.status(404).json({ error: error.message });
+  }
+};
 
-module.exports = { newClientHandler };
+module.exports = { newClientHandler, loginClientHandler };
