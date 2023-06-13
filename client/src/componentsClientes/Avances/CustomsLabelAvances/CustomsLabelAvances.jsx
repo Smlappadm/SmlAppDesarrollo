f899ed048cde41d4d0f760ad3ad7458db7794f2a
Jonavoe <voeffray.jonathan@gmail.com>
2023-06-13 20:59:15 -0300

    Update CustomsLabelAvances.jsx

--- client/src/componentsClientes/Avances/CustomsLabelAvances/CustomsLabelAvances.jsx
+++ client/src/componentsClientes/Avances/CustomsLabelAvances/CustomsLabelAvances.jsx
@@ -3,9 +3,9 @@ import React from "react";
 export default function CustomsLabelAvances({ text, suma, value }) {
   return (
     <div className="flex justify-between gap-5 items-center rounded-xl py-4 my-2 bg-[#39394b]">
-      <p className="text-[#fff] font-bold">{text}</p>
+      <p className="text-[#fff] font-bold ml-4">{text}</p>
       <div className="flex  gap-2">
-        <p className="text-[#fff] font-bold">{suma}</p>
+        <p className="text-[#0f0] font-bold">+ {suma}</p>
         <p className="text-[#fff] font-bold mr-5">{value}</p>
       </div>
     </div>
