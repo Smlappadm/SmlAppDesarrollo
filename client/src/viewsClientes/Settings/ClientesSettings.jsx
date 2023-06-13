a18cd17407d6e4059149edbccbe35b2ce5d510b8
Jonavoe <voeffray.jonathan@gmail.com>
2023-06-13 19:46:12 -0300

    Update ClientesSettings.jsx

--- client/src/viewsClientes/Settings/ClientesSettings.jsx
+++ client/src/viewsClientes/Settings/ClientesSettings.jsx
@@ -1,15 +1,23 @@
 import React from "react";
-import { GrFormClose } from "react-icons/gr";
+import { IoCloseSharp } from "react-icons/io5";
+import CustomsLabelSetting from "./CustomsLabelSetting/CustomsLabelSetting";
 
 export default function ClientesSettings() {
+  const texto = "Ajustes de perfil"
+  const switchs = false
+  const texto1 = "Notifícaiones"
+  const switchs1 = true
+  const texto2 = "Mis pagos"
+  const switchs2 = false
   return (
-    <div className="h-screen w-screen">
-      <div className="flex">
-        <h2>Personal</h2>
-        <div>
-          <GrFormClose className="text-[red]" />
-        </div>
+    <div className="flex flex-col h-screen w-screen">
+      <div className="flex items-end justify-between pt-4">
+        <h2 className="font-bold">Personal</h2>
+        <IoCloseSharp className="font-bold text-[#fff] text-[2rem]" />
       </div>
+      <CustomsLabelSetting text={texto} switchValue ={switchs} />
+      <CustomsLabelSetting text={texto1} switchValue ={switchs1} />
+      <CustomsLabelSetting text={texto2} switchValue ={switchs2} />
     </div>
   );
 }
