94a59d0bf07fb88d9aea8768cd4d56e4c5751e1a
Jonavoe <voeffray.jonathan@gmail.com>
2023-06-13 14:42:55 -0300

    Update actions.js
    Co-Authored-By: Nikitoo17 <114702304+Nikitoo17@users.noreply.github.com>
    Co-Authored-By: Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -116,7 +116,7 @@ export const getLeadChecked = () => {
 
 export const getLeadCheckedInactive5 = (email) => {
   return async (dispatch) => {
-      if (email && email !== "undefined" && email !== null  && email !== "") {
+    if (email && email !== "undefined" && email !== null && email !== "") {
       const response = await axios.get(`/lead/checkedinactive5?email=${email}`);
       const LeadCheckedInactive5 = response.data;
       dispatch({
@@ -124,7 +124,7 @@ export const getLeadCheckedInactive5 = (email) => {
         payload: LeadCheckedInactive5,
       });
     }
-  }
+  };
 };
 
 export const orderClients = (order) => {
@@ -262,3 +262,5 @@ export const updateLeadIncidence = (client, body) => {
     await axios.put(`lead/${client}`, body);
   };
 };
+
+// *******************************Clientes *******************************
