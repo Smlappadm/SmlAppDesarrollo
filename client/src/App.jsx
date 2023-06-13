4b52fd6675012bd547cba559b6c54c13f2d1c268
Nikitoo17 <nicolasfadel17@gmail.com>
2023-06-13 18:25:13 -0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -29,7 +29,7 @@ import { useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import CorredoresHistory from "./components/Corredores/History/CorredoresHistory";
 import CorredoresAnlaytics from "./components/Corredores/Analytics/CorredoresAnalytics";
-import Home from "./viewsClientes/Home/Home";
+import LoginClientes from "./viewsClientes/Login/LoginClientes";
 const { CLERK_API_KEY } = import.meta.env;
 
 if (!"pk_test_Z3VpZGVkLWtvZGlhay0xMi5jbGVyay5hY2NvdW50cy5kZXYk") {
@@ -349,19 +349,7 @@ function ClerkProviderWithRoutes() {
             </>
           }
         />
-        <Route
-          path="/client"
-          element={
-            <>
-              <SignedIn>
-                <Home />
-              </SignedIn>
-              <SignedOut>
-                <RedirectToSignIn />
-              </SignedOut>
-            </>
-          }
-        />
+        <Route path="/clientes-login" element={<LoginClientes />} />
       </Routes>
     </ClerkProvider>
   );
