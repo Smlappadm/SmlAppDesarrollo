4787c738fc7e83e5ff1b08791bf011a982ad4dd0
Jonavoe <voeffray.jonathan@gmail.com>
2023-06-13 17:53:31 -0300

    Update LandingClient.jsx

--- client/src/componentsClientes/Landing/LandingClient.jsx
+++ client/src/componentsClientes/Landing/LandingClient.jsx
@@ -8,22 +8,7 @@ export default function LandingClient() {
     <div>
       <Header />
       <Name />
-      <Followers />
-      <div className="">
-        <Followers
-          numberInstagram={2725}
-          numberTiktok={2500}
-          maxNumber={`10K`}
-        />
-      </div>
-      <div className="">
-        <Followers
-          numberInstagram={2725}
-          numberTiktok={2500}
-          maxNumber={`10K`}
-        />
-      </div>
-      </div>
-
+      <Followers numberInstagram={2725} numberTiktok={2500} maxNumber={`10K`} />
+    </div>
   );
 }
