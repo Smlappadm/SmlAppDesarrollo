4846b5b709d467742c28ff4191e8756f41ef6954
juanfacundotam <facutam@gmail.com>
2023-06-13 17:54:23 -0300

    Update Followers.jsx

--- client/src/componentsClientes/Landing/Followers/Followers.jsx
+++ client/src/componentsClientes/Landing/Followers/Followers.jsx
@@ -1,20 +1,21 @@
 import React, { useEffect } from "react";
-import igPng from "../../../Assets/instagram.png"
-import tkPng from "../../../Assets/tik-tok.png"
+import igPng from "../../../Assets/instagram.png";
+import tkPng from "../../../Assets/tik-tok.png";
 
-export default function Followers({ numberInstagram, numberTiktok, maxNumber }) {
-
+export default function Followers({
+  numberInstagram,
+  numberTiktok,
+  maxNumber,
+}) {
   function formatearNumeroConPuntos(numero) {
     return numero.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
   }
 
-
-
   return (
-    <div className="flex flex-col justify-start items-center w-fit h-fit">
+    <div className="flex flex-col justify-center items-center w-screen h-fit">
       <div>
         <label htmlFor="" className="text-[45px] text-white font-medium px-2">
-          {formatearNumeroConPuntos((numberInstagram + numberTiktok))}
+          {formatearNumeroConPuntos(numberInstagram + numberTiktok)}
         </label>
         <label htmlFor="">/ {maxNumber}</label>
       </div>
@@ -24,15 +25,21 @@ export default function Followers({ numberInstagram, numberTiktok, maxNumber }) 
         </label>
       </div>
       <div className="flex justify-center items-center w-fit h-fit gap-4 mt-5">
-        <img src={igPng} alt="icono de Instagram" className="w-8 h-8"/>
-      <label htmlFor="" className="text-[27px] text-white font-medium  px-2 pb-1">
+        <img src={igPng} alt="icono de Instagram" className="w-8 h-8" />
+        <label
+          htmlFor=""
+          className="text-[27px] text-white font-medium  px-2 pb-1"
+        >
           {formatearNumeroConPuntos(numberInstagram)}
         </label>
         <label className=" border-white bg-white w-[0.5px] h-5  text-white text-18"></label>
-        <label htmlFor="" className="text-[27px] text-white font-medium px-2 pb-1">
+        <label
+          htmlFor=""
+          className="text-[27px] text-white font-medium px-2 pb-1"
+        >
           {formatearNumeroConPuntos(numberTiktok)}
         </label>
-        <img src={tkPng} alt="icono de Tiktok" className="w-8 h-8"/>
+        <img src={tkPng} alt="icono de Tiktok" className="w-8 h-8" />
       </div>
     </div>
   );
