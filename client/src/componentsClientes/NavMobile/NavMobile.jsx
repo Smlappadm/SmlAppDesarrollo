612f19b9d83dbe933ade2c5d0cc2ce2f41ccc20b
juanfacundotam <facutam@gmail.com>
2023-06-13 15:54:44 -0300

    Update NavMobile.jsx

--- client/src/componentsClientes/NavMobile/NavMobile.jsx
+++ client/src/componentsClientes/NavMobile/NavMobile.jsx
@@ -1,7 +1,33 @@
 import React from 'react'
+import { NavLink } from "react-router-dom"
 
 export default function NavMobile() {
   return (
-    <div>NavMobile</div>
+    <div className={style.divLinks}>
+    <NavLink
+      to={`/home`}
+      className={({ isActive }) =>
+        isActive ? style.active : style.disable
+      }
+    >
+      Home
+    </NavLink>
+    <NavLink
+      to={`/about`}
+      className={({ isActive }) =>
+        isActive ? style.active : style.disable
+      }
+    >
+      About
+    </NavLink>
+    <NavLink
+      to={`/favorites`}
+      className={({ isActive }) =>
+        isActive ? style.active : style.disable
+      }
+    >
+      Favorites
+    </NavLink>
+  </div>
   )
 }
