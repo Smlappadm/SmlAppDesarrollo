4089e42bf7ecb39ded5ed63a5bfb05ace1960d10
juanfacundotam <facutam@gmail.com>
2023-06-13 20:45:08 -0300

    Update VistaGeneral.jsx

--- client/src/viewsClientes/VistaGeneral/VistaGeneral.jsx
+++ client/src/viewsClientes/VistaGeneral/VistaGeneral.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import Recursos from '../../componentsClientes/Recursos/Recursos'
+import  {Agendar}  from '../../componentsClientes/Agendar/Agendar'
 
 export const VistaGeneral = () => {
   return (
-    <div>VistaGeneral</div>
+    <div>
+      <Recursos/>
+      <Agendar/>
+    </div>
+
   )
 }
