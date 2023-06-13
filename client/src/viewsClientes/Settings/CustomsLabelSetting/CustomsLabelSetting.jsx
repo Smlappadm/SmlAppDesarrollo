c233b67b49f0e81d6b84753bd3c9e442087ccd8e
Jonavoe <voeffray.jonathan@gmail.com>
2023-06-13 20:19:00 -0300

    Update CustomsLabelSetting.jsx

--- client/src/viewsClientes/Settings/CustomsLabelSetting/CustomsLabelSetting.jsx
+++ client/src/viewsClientes/Settings/CustomsLabelSetting/CustomsLabelSetting.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import CustomizedSwitches from "./MUI/CustomizedSwitches";
 
-export default function CustomsLabelSetting({ text, switchValue,invitar }) {
+export default function CustomsLabelSetting({ text, switchValue, invitar }) {
   return (
     <div className="flex justify-center gap-5 items-center rounded-xl py-4 my-2 bg-[#39394b]">
       <p className="text-[#fff] font-bold">{text}</p>
@@ -11,7 +11,7 @@ export default function CustomsLabelSetting({ text, switchValue,invitar }) {
         </div>
       ) : null}
       {invitar ? (
-        <div className="">
+        <div className="bg-[#188ffd] hover:bg-[#1263af] text-[#fff] rounded-full px-4">
           <p>Invitar</p>
         </div>
       ) : null}
