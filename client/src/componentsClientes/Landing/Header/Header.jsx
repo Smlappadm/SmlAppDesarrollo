46414537f102a9054be3137ea90c9cdfca1bb72d
Jonavoe <voeffray.jonathan@gmail.com>
2023-06-13 17:17:29 -0300

    Update Header.jsx

--- client/src/componentsClientes/Landing/Header/Header.jsx
+++ client/src/componentsClientes/Landing/Header/Header.jsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { IoAdd, IoSettingsOutline } from "react-icons/io5";
+import CircularProgressBar from "../CircularProgressbar/CircularProgressbar";
 
 export default function Header() {
+  const customValue = 10;
+  const imgInstagram = "https://i.postimg.cc/tT405K0C/image.png";
   return (
-    <div>Header</div>
-  )
+    <div className="flex flex-row justify-between p-4 w-screen">
+      <div>
+        <img
+          className="w-[5rem]"
+          src="https://i.postimg.cc/4y1YcByV/1685492595204-removebg-preview.webp"
+          alt=""
+        />
+      </div>
+      <div className="pt-10">
+        <div>
+          <div className="">
+            <CircularProgressBar value={customValue} imageSrc={imgInstagram} />
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col justify-between">
+        <div className="flex flex-row gap-2">
+          <Link to="addvideos">
+            <IoAdd className="font-bold" color="#fff" size={30} />
+          </Link>
+          <Link to="setting">
+            <IoSettingsOutline className="font-bold" color="#fff" size={26} />
+          </Link>
+        </div>
+        <p className="font-bold">53.5%</p>
+      </div>
+    </div>
+  );
 }
