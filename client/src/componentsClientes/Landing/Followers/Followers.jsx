3853c3d7ab48e27886797b5ba7b9f00cf6579cf6
juanfacundotam <facutam@gmail.com>
2023-06-13 15:54:39 -0300

    Update Followers.jsx

--- client/src/componentsClientes/Landing/Followers/Followers.jsx
+++ client/src/componentsClientes/Landing/Followers/Followers.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from "react";
 
-export default function Followers() {
+export default function Followers({ number, maxNumber }) {
   return (
-    <div>Followers</div>
-  )
+    <div className="flex flex-col justify-start items-center w-fit h-fit">
+      <div>
+        <label htmlFor="" className="text-[45px] text-white font-medium px-2">
+          {number}
+        </label>
+        <label htmlFor="">/ {maxNumber}</label>
+      </div>
+      <div className="">
+        <label htmlFor="" className="font-thin">
+          followers
+        </label>
+      </div>
+    </div>
+  );
 }
