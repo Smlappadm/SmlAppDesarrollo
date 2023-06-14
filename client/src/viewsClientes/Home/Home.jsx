a5a7f13d10361dd8f6c7ca1bf92bb7c74a67180f
Nikitoo17 <nicolasfadel17@gmail.com>
2023-06-14 20:27:52 -0300

    Update Home.jsx

--- client/src/viewsClientes/Home/Home.jsx
+++ client/src/viewsClientes/Home/Home.jsx
@@ -2,27 +2,44 @@ import React, { useState } from "react";
 import LandingClient from "../../componentsClientes/Landing/LandingClient";
 import { VistaGeneral } from "../VistaGeneral/VistaGeneral";
 import { TrofeosXP } from "../TrofeosXP/TrofeosXP";
-import { useUser } from "@clerk/clerk-react";
+import { useClerk, useUser } from "@clerk/clerk-react";
 import axios from "axios";
 
 export default function Home() {
+  const [optionView, setOptionView] = useState("vistaGeneral");
+  // const [access, setAccess] = useState(false);
+  // const { signOut } = useClerk();
   // const { user } = useUser();
   // if (!user || !user.emailAddresses || !user.emailAddresses[0]) {
   //   return <div>Loading...</div>;
   // }
   // const userEmail = user.emailAddresses[0].emailAddress;
   // console.log(userEmail);
-  // const response = axios.get("/clientes/");
-  // const client = response.data;
-  // console.log(client);
-  const [optionView, setOptionView] = useState("vistaGeneral");
+  // const fetchData = async () => {
+  //   try {
+  //     const response = await axios.get("/clientes/");
+  //     const client = response.data;
+  //     const findClient = client.some((item) => item.email === userEmail);
+  //     setAccess(findClient);
+  //     console.log(findClient);
+  //   } catch (error) {
+  //     console.error("Error al obtener los datos:", error);
+  //   }
+  // };
 
+  // fetchData();
+
+  // const handleLogout = async () => {
+  //   await signOut();
+  // };
   const handleViewChange = (event) => {
     setOptionView(event.target.value);
   };
 
   return (
-    <div className="flex flex-col items-center bg-[#020131] w-screen h-full pb-32">
+    <div className="flex flex-col items-center bg-[#020131] w-screen h-screen pb-32">
+      {/* {access ? ( */}
+      <>
         <LandingClient />
         {/* otras cosas del header */}
 
@@ -53,6 +70,13 @@ export default function Home() {
 
         {optionView === "vistaGeneral" && <VistaGeneral />}
         {optionView === "trofeosXP" && <TrofeosXP />}
-      </div>
+      </>
+      {/* ) : (
+        <>
+          <p>SIN ACCESO</p>
+          <button onClick={handleLogout}>LogOut</button>
+        </>
+      )} */}
+    </div>
   );
 }
