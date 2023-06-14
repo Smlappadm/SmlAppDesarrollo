37f4d1028c0da39a8240033a375ea2959921fc14
juanfacundotam <facutam@gmail.com>
2023-06-13 22:04:01 -0300

    Update Agendar.jsx

--- client/src/componentsClientes/Agendar/Agendar.jsx
+++ client/src/componentsClientes/Agendar/Agendar.jsx
@@ -3,16 +3,16 @@ import avatar from "../../Assets/avatar.png"
 export const Agendar = ({nameAgendar}) => {
   return (
     <div className="flex flex-col justify-center items-center gap-3 w-screen mt-8">
-    <h1 className="w-4/6 text-white text-18">Agendar con</h1>
-    <div>
+    <h1 className="w-10/12 text-white text-18">Agendar con</h1>
+
       <button
         value="vistaGeneral"
-        className="flex justify-start items-center border-2 border-[#07A1F8] bg-[#07A1F8] text-white px-3 py-0.5 rounded-full text-18 w-72 h-12"
+        className="flex justify-start items-center border-2 border-[#07A1F8] bg-[#07A1F8] text-white px-3 py-0.5 rounded-full text-18 w-10/12 h-12"
       >
       <img src={avatar} alt="icono de Tiktok" className="bg-[#f8cc07] mx-3 w-10 h-10 rounded-full" />
         Juan Pedro Gomez
       </button>
-    </div>
+
 
   </div>
   )
