aff5e75f2b8e7acd28d2301c15e05b9713072c92
juanfacundotam <facutam@gmail.com>
2023-06-14 16:56:10 -0300

    Update VistaGeneral.jsx

--- client/src/viewsClientes/VistaGeneral/VistaGeneral.jsx
+++ client/src/viewsClientes/VistaGeneral/VistaGeneral.jsx
@@ -5,10 +5,16 @@ import Avances from "../../componentsClientes/Avances/Avances";
 
 export const VistaGeneral = () => {
   return (
-    <div className="mb-20">
+   
+      <div className="border-2 flex justify-center items-center w-full">
+        <div className="border-2 w-full">
       <Avances />
+
+        </div>
+      <div className="flex flex-col border-2 w-full ">
       <Recursos />
       <Agendar />
+      </div>
     </div>
   );
 };
