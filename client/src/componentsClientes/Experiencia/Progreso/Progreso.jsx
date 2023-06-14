42959228bdeb759a9dd8b92fdacfbc25dd622d0c
juanfacundotam <facutam@gmail.com>
2023-06-14 16:25:20 -0300

    Update Progreso.jsx

--- client/src/componentsClientes/Experiencia/Progreso/Progreso.jsx
+++ client/src/componentsClientes/Experiencia/Progreso/Progreso.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
+import React from 'react';
+import ProgressBar from "@ramonak/react-progress-bar";
+import { Progress } from 'flowbite-react';
+import LinearDeterminate from './LinearDeterminate';
+
 
 export default function Progreso() {
+  function formatearNumeroConPuntos(numero) {
+    return numero.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+  }
+
   return (
-    <div className=" flex  justify-center items-center gap-6 bg-[#404062] w-10/12 h-24 rounded-xl mb-24">
-    <div className="  flex flex-col justify-center items-center relative h-fit w-20 ">
-      <label htmlFor="" className="text-18 text-white font-medium top-0 absolute">Nivel</label>
-      <label htmlFor="" className=" text-[65px] text-white mt-2">12</label>
+    <div className=" border-2 flex flex-col justify-center items-center gap-2 bg-[#404062] w-10/12 h-20 rounded-xl mb-24">
+    <div className=" border-2 flex w-full justify-end items-center gap-5 mr-10">
+      <label htmlFor="" className="text-green-400 font-semibold">+505</label>
+      <label htmlFor="" className="font-bold text-24 text-white">{formatearNumeroConPuntos("23775")}</label>
+      <label htmlFor="" className="font-bold text-24">/ {formatearNumeroConPuntos("35000")}</label>
     </div>
-    <div className="flex flex-col justify-center items-center relative w-20 h-fit ">
-      <label htmlFor="" className="text-18 text-white font-normal top-0 absolute">Ranking</label>
-      <label htmlFor="" className=" text-[40px] text-white mt-4">2</label>
+    <div className='border-2 w-full flex justify-center'>
+    <LinearDeterminate/>
     </div>
   </div>
   )
 }
+
+
