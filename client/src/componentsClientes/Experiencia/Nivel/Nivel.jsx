15755bc04f9b12088c4499c51ef73ab9065fae24
juanfacundotam <facutam@gmail.com>
2023-06-14 15:16:34 -0300

    Update Nivel.jsx

--- client/src/componentsClientes/Experiencia/Nivel/Nivel.jsx
+++ client/src/componentsClientes/Experiencia/Nivel/Nivel.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 import gdImage from "../../../Assets/googleDrive.png"
 export default function Nivel() {
   return (
-    <div className="border-2 flex  justify-center items-center gap-3 w-screen bg-[#818080] h-32">
-      <div className=" border-2 flex flex-col justify-center items-center w-screen relative">
-        <label htmlFor="" className="text-18 text-white font-bold top-0 absolute">Nivel</label>
-        <p htmlFor="" className=" text-[65px] text-white p-0 mt-4">12</p>
+    <div className=" flex  justify-center items-center gap-6 bg-[#404062] w-10/12 h-28 rounded-xl">
+      <div className="flex flex-col justify-start items-center relative h-24 w-20 ">
+        <label htmlFor="" className="text-18 text-white font-medium ">Nivel</label>
+        <label htmlFor="" className=" text-[65px] text-white absolute top-3">12</label>
       </div>
-      <div className="flex flex-col justify-center items-center  w-screen">
+      <div className="bg-yellow-400 rounded-full flex flex-col justify-center items-center w-24 h-24">
         <img src={gdImage} alt="imagen de trofeo" />
       </div>
-      <div className="flex flex-col justify-center items-center  w-screen">
-        <label htmlFor="">Ranking</label>
-        <label htmlFor="">2</label>
+      <div className="flex flex-col justify-start items-center relative h-24 w-20 mt-4 ">
+        <label htmlFor="" className="text-18 text-white font-medium ">Ranking</label>
+        <label htmlFor="" className=" text-[40px] text-white absolute top-5">2</label>
       </div>
     </div>
   )
