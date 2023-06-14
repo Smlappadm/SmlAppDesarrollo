5ea099ef6ac7cbbb1c4032173c022838e6c1e532
juanfacundotam <facutam@gmail.com>
2023-06-14 20:58:21 -0300

    Update LoginClientes.jsx

--- client/src/viewsClientes/Login/LoginClientes.jsx
+++ client/src/viewsClientes/Login/LoginClientes.jsx
@@ -14,11 +14,12 @@ export default function Home() {
   };
 
   return (
-    <div className="w-screen h-screen bg-[#020131] flex flex-col justify-start items-center">
-      <div className="justify-between w-96">
+    <div className="w-screen h-screen bg-[#020131] flex flex-col justify-start items-center pt-10">
+      <div className="flex flex-col justify-center items-center w-96">
       <img
         src="https://images-ext-1.discordapp.net/external/VmotedpeNAAv9Sz0GZI5iLiobf_7NpJn24pyas4ed_Y/https/i.postimg.cc/4y1YcByV/1685492595204-removebg-preview.webp"
         alt=""
+        className="w-28 h-28"
         onClick={handleOpenRegister}
       />
       {register === false ? (
