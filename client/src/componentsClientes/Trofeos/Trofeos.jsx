b6f8b9f6f4fcc56db79914c5d7dd1e3060ffe871
Jonavoe <voeffray.jonathan@gmail.com>
2023-06-14 15:42:56 -0300

    Update Trofeos.jsx

--- client/src/componentsClientes/Trofeos/Trofeos.jsx
+++ client/src/componentsClientes/Trofeos/Trofeos.jsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import React, { useState } from "react";
+import CustomsIcons from "./CustomsIcons/CustomsIcons";
+import trofy1 from "../../Assets/med1.png";
+import trofy2 from "../../Assets/med2.png";
+import trofy3 from "../../Assets/med3.png";
+import trofy4 from "../../Assets/med4.png";
 
 export default function Trofeos() {
+  const [trofeos, setTrofeos] = useState([
+    { imagen: trofy2, isVisible: true },
+    { imagen: null, isVisible: false },
+    { imagen: null, isVisible: false },
+    { imagen: null, isVisible: false },
+    { imagen: trofy4, isVisible: true },
+    { imagen: null, isVisible: false },
+    { imagen: null, isVisible: false },
+    { imagen: null, isVisible: false },
+    { imagen: trofy3, isVisible: true },
+    { imagen: null, isVisible: false },
+    { imagen: trofy1, isVisible: true },
+    { imagen: null, isVisible: false },
+  ]);
+
   return (
-    <div>Trofeos</div>
-  )
+    <div className="flex flex-col mx-4 gap-4">
+      <h2 className="text-[#fff]">Trofeos</h2>
+      <div className="flex justify-center items-center">
+        <div className="grid grid-cols-3 gap-4 w-10/12">
+          {trofeos.map((trofeo, index) => (
+            <div key={index}>
+              <CustomsIcons
+                imagen={trofeo.imagen}
+                isVisible={trofeo.isVisible}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
