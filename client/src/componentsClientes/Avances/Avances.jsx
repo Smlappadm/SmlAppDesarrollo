2c5e4158eb13643ccce0787102485917760a537a
Jonavoe <voeffray.jonathan@gmail.com>
2023-06-14 19:51:55 -0300

    Update Avances.jsx

--- client/src/componentsClientes/Avances/Avances.jsx
+++ client/src/componentsClientes/Avances/Avances.jsx
@@ -11,18 +11,22 @@ export default function Avances() {
 
   return (
     <div className="flex flex-col justify-center items-center gap-3 w-96 mt-8 ">
-      <h1 className=" text-white text-18 w-10/12 md:w-fit">Avances</h1>
-        <div>
-          {avances.map((avance, index) => (
-            <CustomsLabelAvances
-              key={index}
-              text={avance.texto}
-              suma={avance.suma}
-              value={avance.value}
-            />
-          ))}
-        </div>
+      <div>
+        <h1 className=" text-white text-18 w-10/12 md:w-fit">Avances</h1> 
+        <select>
+          <option value="">desde siempre</option>
+        </select>
       </div>
-  
+      <div>
+        {avances.map((avance, index) => (
+          <CustomsLabelAvances
+            key={index}
+            text={avance.texto}
+            suma={avance.suma}
+            value={avance.value}
+          />
+        ))}
+      </div>
+    </div>
   );
 }
