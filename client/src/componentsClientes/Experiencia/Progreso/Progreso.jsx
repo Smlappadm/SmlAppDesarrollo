612bd70ffb75d8db016d7c79f835f1a0dfce1dcb
juanfacundotam <facutam@gmail.com>
2023-06-14 15:16:31 -0300

    Update Progreso.jsx

--- client/src/componentsClientes/Experiencia/Progreso/Progreso.jsx
+++ client/src/componentsClientes/Experiencia/Progreso/Progreso.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 export default function Progreso() {
   return (
-    <div>Progreso</div>
+    <div className=" flex  justify-center items-center gap-6 bg-[#404062] w-10/12 h-24 rounded-xl mb-24">
+    <div className="  flex flex-col justify-center items-center relative h-fit w-20 ">
+      <label htmlFor="" className="text-18 text-white font-medium top-0 absolute">Nivel</label>
+      <label htmlFor="" className=" text-[65px] text-white mt-2">12</label>
+    </div>
+    <div className="flex flex-col justify-center items-center relative w-20 h-fit ">
+      <label htmlFor="" className="text-18 text-white font-normal top-0 absolute">Ranking</label>
+      <label htmlFor="" className=" text-[40px] text-white mt-4">2</label>
+    </div>
+  </div>
   )
 }
