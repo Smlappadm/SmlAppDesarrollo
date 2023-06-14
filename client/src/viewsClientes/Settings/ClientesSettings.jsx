9c478736a8b75b3121f64a3e442ec89197a7b867
juanfacundotam <facutam@gmail.com>
2023-06-14 19:16:38 -0300

    Update ClientesSettings.jsx

--- client/src/viewsClientes/Settings/ClientesSettings.jsx
+++ client/src/viewsClientes/Settings/ClientesSettings.jsx
@@ -12,7 +12,7 @@ export default function ClientesSettings() {
   const invitar3 = true;
 
   return (
-    <div className="flex gap-5 mx-4 flex-col justify-between h-screen w-screen">
+    <div className="border-2 flex gap-5  flex-col justify-between h-screen w-screen">
       <div>
         <div className="flex mb-4 items-end justify-between pt-4">
           <h2 className="font-bold">Personal</h2>
@@ -20,14 +20,14 @@ export default function ClientesSettings() {
             <IoCloseSharp className="font-bold text-[#fff] text-[2rem]" />
           </Link>
         </div>
-        <div>
+
           <CustomsLabelSetting text={texto} />
           <CustomsLabelSetting text={texto1} switchValue={switchs1} />
           <CustomsLabelSetting text={texto2} />
           <div className="mt-32">
             <CustomsLabelSetting text={texto3} invitar={invitar3} />
           </div>
-        </div>
+
       </div>
       <div className="flex justify-center items-center mb-4">
         <p>Cerrar Sesión</p>
