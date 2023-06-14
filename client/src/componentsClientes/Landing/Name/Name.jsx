12057c5e2a48ab230c345568306c008873340f57
Jonavoe <voeffray.jonathan@gmail.com>
2023-06-14 20:41:06 -0300

    Update Name.jsx

--- client/src/componentsClientes/Landing/Name/Name.jsx
+++ client/src/componentsClientes/Landing/Name/Name.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { useUser } from "@clerk/clerk-react";
 
 export default function Name() {
+  const user = useUser().user;
+  const name = user.fullName;
+
+  console.log(name);
+
   return (
     <div className="flex flex-col gap-1 mt-5 justify-center items-center">
-      <span className="text-white font-bold text-3xl">Jonathan Voeffray</span>
+      <span className="text-white font-bold text-3xl">{name}</span>
       <span className="text-xl">@jonavoe</span>
     </div>
   );
