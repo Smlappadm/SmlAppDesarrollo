eba4ccf8960edb9823db64116ab0915e306afc29
Nikitoo17 <nicolasfadel17@gmail.com>
2023-06-14 19:11:08 -0300

    Update ClientesRouter.js

--- api/src/routes/ClientesRouter.js
+++ api/src/routes/ClientesRouter.js
@@ -2,10 +2,12 @@ const { Router } = require("express");
 const {
   newClientHandler,
   loginClientHandler,
+  getAllClientesHandler,
 } = require("../Handlers/ClientesHandlers");
 const ClientesRouter = Router();
 
 ClientesRouter.post("/new", newClientHandler);
 ClientesRouter.get("/username", loginClientHandler);
+ClientesRouter.get("/", getAllClientesHandler);
 
 module.exports = ClientesRouter;
