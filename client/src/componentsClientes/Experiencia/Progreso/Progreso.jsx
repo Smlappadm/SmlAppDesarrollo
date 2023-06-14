ea0ff9b5291cc21ed813d8dae514cab0a1430c37
Jonavoe <voeffray.jonathan@gmail.com>
2023-06-14 16:30:08 -0300

    Update Progreso.jsx

--- client/src/componentsClientes/Experiencia/Progreso/Progreso.jsx
+++ client/src/componentsClientes/Experiencia/Progreso/Progreso.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import ProgressBar from "@ramonak/react-progress-bar";
-import { Progress } from 'flowbite-react';
 import LinearDeterminate from './LinearDeterminate';
 
 
