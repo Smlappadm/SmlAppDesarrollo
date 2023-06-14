d678fd02b5960bb84948703d184a8607a7c55a9b
juanfacundotam <facutam@gmail.com>
2023-06-14 16:27:08 -0300

    Update Progreso.jsx

--- client/src/componentsClientes/Experiencia/Progreso/Progreso.jsx
+++ client/src/componentsClientes/Experiencia/Progreso/Progreso.jsx
@@ -10,13 +10,13 @@ export default function Progreso() {
   }
 
   return (
-    <div className=" border-2 flex flex-col justify-center items-center gap-2 bg-[#404062] w-10/12 h-20 rounded-xl mb-24">
-    <div className=" border-2 flex w-full justify-end items-center gap-5 mr-10">
+    <div className="  flex flex-col justify-center items-center gap-2 bg-[#404062] w-10/12 h-20 rounded-xl mb-24">
+    <div className=" flex w-full justify-end items-center gap-5 mr-10">
       <label htmlFor="" className="text-green-400 font-semibold">+505</label>
       <label htmlFor="" className="font-bold text-24 text-white">{formatearNumeroConPuntos("23775")}</label>
       <label htmlFor="" className="font-bold text-24">/ {formatearNumeroConPuntos("35000")}</label>
     </div>
-    <div className='border-2 w-full flex justify-center'>
+    <div className=' w-full flex justify-center'>
     <LinearDeterminate/>
     </div>
   </div>
