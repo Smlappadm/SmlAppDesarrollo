84cfc10cde746d6560c2592b89ca52841a119b5d
juanfacundotam <facutam@gmail.com>
2023-06-14 14:50:23 -0300

    Update Experiencia.jsx

--- client/src/componentsClientes/Experiencia/Experiencia.jsx
+++ client/src/componentsClientes/Experiencia/Experiencia.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from "react";
+import Nivel from "./Nivel/Nivel";
+import Progreso from "./Progreso/Progreso";
 
 export default function Experiencia() {
   return (
-    <div>Experiencia</div>
-  )
+    <div className="flex flex-col justify-center items-center gap-3 w-screen mt-8">
+        <h1 className=" text-white text-18 w-10/12">Experiencia</h1>
+      <Nivel />
+      <Progreso />
+    </div>
+  );
 }
