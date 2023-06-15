aa285ef2054891bfb93f278cdf717033c795af32
juanfacundotam <facutam@gmail.com>
2023-06-15 15:28:53 -0300

    Update VendedoresHistory.jsx

--- client/src/components/Vendedores/analytics/VendedoresHistory.jsx
+++ client/src/components/Vendedores/analytics/VendedoresHistory.jsx
@@ -4,37 +4,38 @@ import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import PaginationOutlined from "../../pagination/PaginationOutlined";
 import { filterLevel, getVendedorAllLeads } from "../../../redux/actions";
-import { AiOutlinePhone } from "react-icons/ai";
 import { IoGrid, IoStatsChart } from "react-icons/io5";
 import { FaHistory } from "react-icons/fa";
 import { CiWarning, CiInstagram, CiMail } from "react-icons/ci";
 import { useUser } from "@clerk/clerk-react";
 import { MdOutlineAttachMoney } from "react-icons/md";
-import { RiMoneyDollarBoxFill } from "react-icons/ri";
+import SelectLevel from "../Dashboard/SelectLevel";
 
 import Nav from "../../Nav/Nav";
 
 const VendedoresHistory = () => {
   const [data, setData] = useState([]);
-  const { leadCheckedInactive5 } = useSelector((state) => state);
-  const { vendedorAllLeads } = useSelector((state) => state);
+  const  {vendedorAllLeadsHistory}  = useSelector((state) => state);
   const dispatch = useDispatch();
   const [showCopiedMessage, setShowCopiedMessage] = useState(false);
   const user = useUser().user;
   const email = user?.emailAddresses[0].emailAddress;
+  const [openFilterName, setOpenFilterName] = useState(false)
 
   useEffect(() => {
     dispatch(getVendedorAllLeads(email));
   }, [dispatch]);
   useEffect(() => {
-    setData(vendedorAllLeads);
-  }, [vendedorAllLeads]);
+    setData(vendedorAllLeadsHistory);
+  }, [vendedorAllLeadsHistory]);
+
+
   const [pageStyle, setPageStyle] = useState(1);
   const [currentPage, setCurrentPage] = useState(1);
   const [cardXPage, setCardXpage] = useState(10);
   const indexLastCard = currentPage * cardXPage;
   const indexFirstCard = indexLastCard - cardXPage;
-  const currentCard = data.slice(indexFirstCard, indexLastCard);
+  const currentCard = data && data.slice(indexFirstCard, indexLastCard);
   const pages = (pageNumber) => {
     setCurrentPage(pageNumber);
   };
@@ -65,8 +66,11 @@ const VendedoresHistory = () => {
   const onChangeLevel = (value) => {
     setLevelValue(value);
     dispatch(filterLevel(value));
-    setData(vendedorAllLeads);
+    setData(vendedorAllLeadsHistory);
     setCurrentPage(1);
+    if(!value){
+      setFilters({...filters, level: !filters.level})
+    }
   };
   //*********** */
 
@@ -90,8 +94,12 @@ const VendedoresHistory = () => {
 
   const updateLeads = () => {
     dispatch(getVendedorAllLeads(email));
-    setData(vendedorAllLeads);
+    setData(vendedorAllLeadsHistory);
   };
+
+  const handlerOpenFilterName = () => {
+    setOpenFilterName(true)
+  }
 
   return (
     <>
@@ -122,49 +130,66 @@ const VendedoresHistory = () => {
               <Link className="text-5xl" to={"/vendedores-analytics"}>
                 <IoStatsChart className="text-[2rem] text-[#418df0] hover:text-[#3570bd]" />
               </Link>
-              <div className="absolute right-14">
+              
+                {filters.level === true ? (
+                  <SelectLevel onChange={onChangeLevel} value={levelValue} className="border-2 w-64"/>
+                ) : (
+                  ""
+                )}
+              
                 {/* <select className="w-32 h-10 rounded-lg bg-purple-500 text-white text-center">
                   <option className="py-1">2023</option>
                 </select> */}
-              </div>
             </div>
           </div>
 
-          {vendedorAllLeads.length > 0 ? (
-            <table className={style.table}>
+          {vendedorAllLeadsHistory && vendedorAllLeadsHistory.length ? (
+            <div className={style.table}>
               <div className="flex justify-start items-center  mx-6">
-                <label className="text-start w-[20%] px-3">Nombre</label>
+              <button
+                  className="text-start w-[20%] px-3"
+                  onClick={handlerOpenFilterName}
+                >
+                  Nombre
+                </button>
                 <label className="text-start w-[15%] px-3">Sector</label>
                 <label className="text-start w-[10%] px-3">País</label>
                 <label className="text-center w-[5%] ">Email</label>
                 <label className="text-center w-[5%] ">Instagram</label>
                 <label className="text-center w-[15%] ">Phone</label>
-                <button className="text-center  w-[10%]">Nivel</button>
+                <button
+                  className="  w-[10%] text-center"
+                  onClick={() => handlerFilter("level")}
+                >
+                  Nivel
+                </button>
                 <label className="text-center w-[20%] ">Status</label>
-                
               </div>
 
-              <tbody className="">
-                {currentCard.map((item, index) => (
-                   <div key={item._id} className=" flex items-center justify-start bg-[#39394B] text-sm text-gray-300 p-2 m-3 min-h-14 rounded-lg">
+              <div className="">
+                {currentCard && currentCard.map((item, index) => (
+                  <div
+                    key={item._id}
+                    className=" flex items-center justify-start bg-[#39394B] text-sm text-gray-300 p-2 m-3 min-h-14 rounded-lg"
+                  >
                     <div className=" w-[20%] flex justify-start items-center  p-0 ">
                       <p className="w-64 p-1 px-3 rounded-full text-ellipsis text-18 opacity-1 overflow-hidden whitespace-nowrap hover:overflow-visible hover:bg-[#e3e1e1] hover:w-fit hover:text-black z-111 hover:absolute">
                         {item.name}
                       </p>
-                      </div>
-                      <div className=" w-[15%] flex justify-start items-center p-0 ">
+                    </div>
+                    <div className=" w-[15%] flex justify-start items-center p-0 ">
                       <p className="w-40 p-1 px-3 rounded-full text-ellipsis text-18 opacity-1 overflow-hidden whitespace-nowrap hover:overflow-visible hover:bg-[#e3e1e1] hover:w-fit hover:text-black z-111 hover:absolute">
                         {item.category}
                       </p>
-                      </div>
+                    </div>
 
-                      <div className=" w-[10%] flex justify-start items-center p-0">
+                    <div className=" w-[10%] flex justify-start items-center p-0">
                       <p className="text-start w-24 p-1 px-3 rounded-full text-ellipsis text-18 opacity-1 overflow-hidden whitespace-nowrap hover:overflow-visible hover:bg-[#e3e1e1] hover:w-fit hover:text-black z-111 hover:absolute">
                         {item.province}
                       </p>
-                      </div>
+                    </div>
 
-                      <div className=" w-[5%] flex justify-center items-center p-0">
+                    <div className=" w-[5%] flex justify-center items-center p-0">
                       {item.email !== "-" ? (
                         <div onClick={() => handleCopyClick(item.email)}>
                           <div className="cursor-pointer">
@@ -197,8 +222,8 @@ const VendedoresHistory = () => {
                       >
                         {item.telephone}
                       </p>
-                      </div>
-                      <div className=" w-[10%] flex justify-center items-center p-0">
+                    </div>
+                    <div className=" w-[10%] flex justify-center items-center p-0">
                       {item.level !== "incidencia" ? (
                         <p className="bg-[#6254ff] text-[#ffffff] w-[40px] rounded h-10 flex items-center justify-center text-[35px] drop-shadow-xl">
                           {item.level}
@@ -208,8 +233,8 @@ const VendedoresHistory = () => {
                           <CiWarning className="text-[#fdfa3a] p-0 text-[35px] font-bold" />
                         </div>
                       )}
-                     </div>
-                     <div className=" w-[20%] flex justify-center items-start p-0">
+                    </div>
+                    <div className=" w-[20%] flex justify-center items-start p-0">
                       {item.status === "Contratado" && (
                         <p className="bg-[#26af7f] w-44 h-11 flex justify-center items-center text-white rounded-3xl text-18">
                           Contratado
@@ -231,19 +256,17 @@ const VendedoresHistory = () => {
                         </p>
                       )}
                     </div>
-
-                   
-                    </div>
+                  </div>
                 ))}
-              </tbody>
-            </table>
+              </div>
+            </div>
           ) : (
             <div className="flex items-center justify-center w-full h-screen">
               <h1>No hay Leads disponibles</h1>
             </div>
           )}
         </div>
-        {data.length > 10 && (
+        {data && data.length > 10 && (
           <div className="mb-5">
             <PaginationOutlined
               pageStyle={pageStyle}
