0f81ba2e754fe273a702446cca6fd14e966bdac6
Jonavoe <voeffray.jonathan@gmail.com>
2023-06-14 21:21:58 -0300

    Update Home.jsx

--- client/src/viewsClientes/Home/Home.jsx
+++ client/src/viewsClientes/Home/Home.jsx
@@ -77,7 +77,6 @@ export default function Home() {
       )}
     </div>
   );
-  
 
   // <div className="text-white  rounded-full px-6 py-2 bg-[#07a1f8] hover:bg-[#127fbe]">
   // <p className="text-[1.4rem] text-center">Añadir</p>
@@ -85,7 +84,5 @@ export default function Home() {
   // <Link to={"/clientes-home"}>
   // <div className=" rounded-full px-6 py-2  text-gray-900 focus:outline-none bg-white  border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
   //   <p className="text-[1.4rem] text-center">Cancelar</p>
-  // </div> 
+  // </div>
 }
-
-
