dd0463285145b4362ca07cd908bc69a2c8f50b77
Nikitoo17 <nicolasfadel17@gmail.com>
2023-06-15 16:08:13 -0300

    Update Register.jsx

--- client/src/componentsClientes/Login/Register.jsx
+++ client/src/componentsClientes/Login/Register.jsx
@@ -27,17 +27,16 @@ export default function Register({ handleOpenRegister }) {
     const fetchData = async () => {
       await dispatch(getAllClientes());
     };
-
     fetchData();
   }, [dispatch]);
   useEffect(() => {
-    console.log(allClientes);
+    console.log(allClientes[1]);
   }, [allClientes]);
 
   const validateEmail = async (email) => {
-    const clientEmailVerify = allClientes.some(
-      (client) => client.email === email
-    );
+    const clientEmailVerify =
+      allClientes && allClientes.some((client) => client.email === email);
+    console.log();
     const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
     if (!emailRegex.test(email)) {
       return "Por favor, ingresa un correo electrónico válido";
