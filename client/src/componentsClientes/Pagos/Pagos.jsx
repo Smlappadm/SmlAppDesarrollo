e51656f4503068b67d522fb4a8069013a8bdd5d6
juanfacundotam <facutam@gmail.com>
2023-06-16 19:27:17 -0300

    Update Pagos.jsx

--- client/src/componentsClientes/Pagos/Pagos.jsx
+++ client/src/componentsClientes/Pagos/Pagos.jsx
@@ -2,15 +2,31 @@ import React from "react";
 import {Elements} from '@stripe/react-stripe-js';
 import {loadStripe} from '@stripe/stripe-js';
 import CheckoutForm from "./CheckoutForm"
+// require('dotenv').config();
+
+// const { STRIPE_SECRET_KEY } = process.env;
 // Make sure to call `loadStripe` outside of a component’s render to avoid
 // recreating the `Stripe` object on every render.
 const stripePromise = loadStripe('pk_test_51NJhsbGpn5uZGCfpbyEu252jvDVNlqDiljFxifEkG5rAba4tu11lt9wl3m3UP1xFL3tnUGtPxT0KLMjNSnl6SO7o00xs2avzC5');
 
 export const Pagos = () => {
-  const options = {
-    // passing the client secret obtained from the server
-    clientSecret: '{{CLIENT_SECRET}}',
-  };
+
+// const options = {
+//     passing the client secret obtained in step 3
+//     clientSecret: STRIPE_SECRET_KEY,
+//     Fully customizable with appearance API.
+//     appearance: {/*...*/},
+//   };
+
+//   return (
+//     // <Elements stripe={stripePromise} options={options}>
+//         <Elements stripe={stripePromise} >
+//       <CheckoutForm />
+//     </Elements>
+//   );
+// };
+
+
 
   return (
     <div className="flex bg-[#020131] gap-5  flex-col justify-center items-center h-screen w-screen">
