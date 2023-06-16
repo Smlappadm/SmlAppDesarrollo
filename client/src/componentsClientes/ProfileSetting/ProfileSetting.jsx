c8c0e87ce88e0785c31e9bbc8ea8629beafe3325
Nikitoo17 <nicolasfadel17@gmail.com>
2023-06-16 15:49:18 -0300

    Update ProfileSetting.jsx

--- client/src/componentsClientes/ProfileSetting/ProfileSetting.jsx
+++ client/src/componentsClientes/ProfileSetting/ProfileSetting.jsx
@@ -13,27 +13,30 @@ export default function ProfileSetting({ handleProfileSetting }) {
           <IoCloseSharp className="font-bold text-[#fff] text-[2rem]" />
         </button>
       </div>
-      <div className="w-full flex flex-col gap-3 items-center">
+      <div className="w-full flex flex-col gap-4 items-center">
         <input
           type="text"
           placeholder="Cambiar Nombre de Usuario"
-          className="w-10/12 text-center rounded-md h-[40px] bg-transparent border placeholder:text-white text-white"
+          className="w-10/12 text-center rounded-md h-[40px] bg-gradient-to-br from-black via-[#020131]  to-blue-950 border placeholder:text-gray-500 text-white"
         />
         <input
           type="text"
           placeholder="Ingresar Instagram"
-          className="w-10/12 text-center rounded-md h-[40px] bg-transparent border placeholder:text-white text-white"
+          className="w-10/12 text-center rounded-md h-[40px] bg-gradient-to-br from-black via-[#020131]  to-blue-950 border placeholder:text-gray-500 text-white"
         />
         <input
           type="text"
           placeholder="Ingresar TikTok"
-          className="w-10/12 text-center rounded-md h-[40px] bg-transparent border placeholder:text-white text-white"
+          className="w-10/12 text-center rounded-md h-[40px] bg-gradient-to-br from-black via-[#020131]  to-blue-950 border placeholder:text-gray-500 text-white"
         />
         <input
           type="text"
           placeholder="Ingresar Google Drive"
-          className="w-10/12 text-center rounded-md h-[40px] bg-transparent border placeholder:text-white text-white"
+          className="w-10/12 text-center rounded-md h-[40px] bg-gradient-to-br from-black via-[#020131]  to-blue-950 border placeholder:text-gray-500 text-white"
         />
+        <button className="w-5/12 text-center rounded-md mt-6 border h-[40px] px-3 bg-gradient-to-t from-black via-[#020131]  to-blue-600 ">
+          Guardar
+        </button>
       </div>
     </>
   );
