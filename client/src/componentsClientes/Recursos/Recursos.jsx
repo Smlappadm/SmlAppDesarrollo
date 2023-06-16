596bcc313c75e29a508b62b2dc4a3e6d8b0e5117
Nikitoo17 <nicolasfadel17@gmail.com>
2023-06-16 18:47:16 -0300

    Update Recursos.jsx

--- client/src/componentsClientes/Recursos/Recursos.jsx
+++ client/src/componentsClientes/Recursos/Recursos.jsx
@@ -12,6 +12,7 @@ export default function Recursos() {
   const { user } = useUser();
   const userEmail = user.emailAddresses[0].emailAddress;
   const dispatch = useDispatch();
+  const linkVerify = /^(ftp|http|https):\/\/[^ "]+$/;
   useEffect(() => {
     dispatch(getClientByEmail(userEmail && userEmail));
   }, [dispatch]);
@@ -19,33 +20,55 @@ export default function Recursos() {
   return (
     <div className="flex flex-col justify-center items-center gap-3 w-96 mt-8 ">
       <h1 className=" text-white text-18 w-10/12 md:w-fit">Recursos</h1>
-
-      <Link
-        to={`${client && client.drive}`}
-        target={"_blank"}
-        className="flex justify-start items-center border-2 border-[#07A1F8] bg-[#07A1F8] text-white px-3 py-0.5 rounded-full text-18 w-10/12 h-12"
-      >
-        <img src={gdPng} alt="icono de Tiktok" className="mx-3 w-8 h-8" />
-        Mi Carpeta Drive
-      </Link>
-
-      <Link
-        to={`${client && client.instagram}`}
-        target={"_blank"}
-        className="flex justify-start items-center border-2 border-[#07A1F8] bg-[#07A1F8] text-white px-3 py-0.5 rounded-full text-18 w-10/12 h-12"
-      >
-        <img src={igPng} alt="icono de Tiktok" className="mx-3 w-8 h-8" />
-        Mi Instagram
-      </Link>
-
-      <Link
-        to={`${client && client.tiktok}`}
-        target={"_blank"}
-        className="flex justify-start items-center border-2 border-[#07A1F8] bg-[#07A1F8] text-white px-3 py-0.5 rounded-full text-18 w-10/12 h-12"
-      >
-        <img src={tkPng} alt="icono de Tiktok" className="mx-3 w-8 h-8" />
-        Mi Tiktok
-      </Link>
+      {linkVerify.test(client && client.drive) ? (
+        <Link
+          to={`${client && client.drive}`}
+          target={"_blank"}
+          className="flex justify-start items-center border-2 border-[#07A1F8] bg-[#07A1F8] text-white px-3 py-0.5 rounded-full text-18 w-10/12 h-12"
+        >
+          <img src={gdPng} alt="icono de Tiktok" className="mx-3 w-8 h-8" />
+          Mi Carpeta Drive
+        </Link>
+      ) : (
+        <Link className="flex justify-start items-center border-2 border-[#07A1F8] bg-gray-500 text-white px-3 py-0.5 rounded-full text-18 w-10/12 h-12">
+          <img src={gdPng} alt="icono de Tiktok" className="mx-3 w-8 h-8" />
+          Mi Carpeta Drive
+        </Link>
+      )}
+      {linkVerify.test(client && client.instagram) ? (
+        <Link
+          to={`${client && client.instagram}`}
+          target={"_blank"}
+          className="flex justify-start items-center border-2 border-[#07A1F8] bg-[#07A1F8] text-white px-3 py-0.5 rounded-full text-18 w-10/12 h-12"
+        >
+          <img src={igPng} alt="icono de Tiktok" className="mx-3 w-8 h-8" />
+          Mi Instagram
+        </Link>
+      ) : (
+        <Link
+          to={`${client && client.instagram}`}
+          target={"_blank"}
+          className="flex justify-start items-center border-2 border-[#07A1F8] bg-gray-500 text-white px-3 py-0.5 rounded-full text-18 w-10/12 h-12"
+        >
+          <img src={igPng} alt="icono de Tiktok" className="mx-3 w-8 h-8" />
+          Mi Instagram
+        </Link>
+      )}
+      {linkVerify.test(client && client.tiktok) ? (
+        <Link
+          to={`${client && client.tiktok}`}
+          target={"_blank"}
+          className="flex justify-start items-center border-2 border-[#07A1F8] bg-[#07A1F8] text-white px-3 py-0.5 rounded-full text-18 w-10/12 h-12"
+        >
+          <img src={tkPng} alt="icono de Tiktok" className="mx-3 w-8 h-8" />
+          Mi Tiktok
+        </Link>
+      ) : (
+        <Link className="flex justify-start items-center border-2 border-[#07A1F8] bg-gray-500 text-white px-3 py-0.5 rounded-full text-18 w-10/12 h-12">
+          <img src={tkPng} alt="icono de Tiktok" className="mx-3 w-8 h-8" />
+          Mi Tiktok
+        </Link>
+      )}
     </div>
   );
 }
