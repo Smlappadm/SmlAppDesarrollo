4833163cb083fcbfa1c64a9b275c73a6c7f6fbff
Jonavoe <voeffray.jonathan@gmail.com>
2023-06-16 17:21:52 -0300

    Update Progreso.jsx

--- client/src/componentsClientes/Experiencia/Progreso/Progreso.jsx
+++ client/src/componentsClientes/Experiencia/Progreso/Progreso.jsx
@@ -1,22 +1,13 @@
 import React, { useEffect, useState } from "react";
 import LinearDeterminate from "./LinearDeterminate";
 
-export default function Progreso() {
-  const [progress, setProgress] = useState(0);
-  const [goals, setGoals] = useState(5000);
-
-  React.useEffect(() => {
-    setProgress(0);
-    if (progress >= goals) {
-      setGoals(goals + 5000);
-    }
-  }, []);
+export default function Progreso({goals, progress}) {
 
   return (
     <div className="flex flex-col justify-center items-center gap-2 bg-[#404062] w-10/12 h-20 rounded-xl">
-      <div className="flex w-full justify-end items-center gap-5 mr-10">
+      <div className="flex w-full justify-center items-center gap-5">
         <label htmlFor="" className="text-green-400 font-semibold">
-          +505
+          +{100 - progress}
         </label>
         <label htmlFor="" className="font-bold text-24 text-white">
           {progress}
@@ -26,11 +17,11 @@ export default function Progreso() {
         </label>
 
         <label htmlFor="" className="text-red-400 font-semibold">
-          {5000 - progress}
+          -{100 - progress}
         </label>
       </div>
       <div className="w-full flex justify-center">
-        <LinearDeterminate value={progress} minValue={0} maxValue={5000} />
+        <LinearDeterminate value={progress} minValue={0} maxValue={100} />
       </div>
     </div>
   );
