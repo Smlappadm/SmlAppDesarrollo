06a559154a4d3fe89ca7f12af4189950c55810f7
Jonavoe <voeffray.jonathan@gmail.com>
2023-06-16 18:02:22 -0300

    Update Nivel.jsx

--- client/src/componentsClientes/Experiencia/Nivel/Nivel.jsx
+++ client/src/componentsClientes/Experiencia/Nivel/Nivel.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import medImage from "../../../Assets/med1.png";
-export default function Nivel({nivel}) {
+export default function Nivel({ nivel }) {
   return (
     <div className=" flex  justify-center items-center gap-6 bg-[#404062] w-10/12 h-28 rounded-xl">
       <div className="flex flex-col justify-start items-center relative h-24 w-20 ">
@@ -8,7 +8,9 @@ export default function Nivel({nivel}) {
           Nivel
         </label>
         <label htmlFor="" className=" text-[65px]  absolute top-3">
-          <span class="bg-gradient-to-t from-blue-500 to-purple-500 text-transparent bg-clip-text font-extrabold">{nivel}</span>
+          <span class="bg-gradient-to-t from-blue-500 to-purple-500 text-transparent bg-clip-text font-extrabold">
+            {nivel}
+          </span>
         </label>
       </div>
       <div className=" rounded-full flex flex-col justify-center items-center w-24 h-24">
