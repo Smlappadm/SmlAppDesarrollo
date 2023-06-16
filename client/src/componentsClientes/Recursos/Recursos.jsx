ce4e7af946ece5322357bf9602093ae290890f9b
Nikitoo17 <nicolasfadel17@gmail.com>
2023-06-16 18:36:53 -0300

    Update Recursos.jsx

--- client/src/componentsClientes/Recursos/Recursos.jsx
+++ client/src/componentsClientes/Recursos/Recursos.jsx
@@ -20,13 +20,14 @@ export default function Recursos() {
     <div className="flex flex-col justify-center items-center gap-3 w-96 mt-8 ">
       <h1 className=" text-white text-18 w-10/12 md:w-fit">Recursos</h1>
 
-      <button
-        value="vistaGeneral"
+      <Link
+        to={`${client && client.drive}`}
+        target={"_blank"}
         className="flex justify-start items-center border-2 border-[#07A1F8] bg-[#07A1F8] text-white px-3 py-0.5 rounded-full text-18 w-10/12 h-12"
       >
         <img src={gdPng} alt="icono de Tiktok" className="mx-3 w-8 h-8" />
         Mi Carpeta Drive
-      </button>
+      </Link>
 
       <Link
         to={`${client && client.instagram}`}
@@ -37,13 +38,14 @@ export default function Recursos() {
         Mi Instagram
       </Link>
 
-      <button
-        value="vistaGeneral"
+      <Link
+        to={`${client && client.tiktok}`}
+        target={"_blank"}
         className="flex justify-start items-center border-2 border-[#07A1F8] bg-[#07A1F8] text-white px-3 py-0.5 rounded-full text-18 w-10/12 h-12"
       >
         <img src={tkPng} alt="icono de Tiktok" className="mx-3 w-8 h-8" />
         Mi Tiktok
-      </button>
+      </Link>
     </div>
   );
 }
