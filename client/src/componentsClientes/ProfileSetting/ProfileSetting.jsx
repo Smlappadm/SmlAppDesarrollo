fcf919dd01594b62b67fedf309a5d368571a8927
Nikitoo17 <nicolasfadel17@gmail.com>
2023-06-16 15:58:49 -0300

    Update ProfileSetting.jsx

--- client/src/componentsClientes/ProfileSetting/ProfileSetting.jsx
+++ client/src/componentsClientes/ProfileSetting/ProfileSetting.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoCloseSharp } from "react-icons/io5";
 
 export default function ProfileSetting({ handleProfileSetting }) {
+  const [username, setUsername] = useState();
+  const [instagram, setInstagram] = useState();
+  const [tiktok, setTiktok] = useState();
+  const [drive, setDrive] = useState();
+
+  const handleSaveChanges = () => {
+    console.log(username);
+    console.log(instagram);
+    console.log(tiktok);
+    console.log(drive);
+  };
+
   return (
     <>
       <div className=" flex mb-4 items-end justify-between pt-4">
@@ -16,25 +28,36 @@ export default function ProfileSetting({ handleProfileSetting }) {
       <div className="w-full flex flex-col gap-4 items-center">
         <input
           type="text"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
           placeholder="Cambiar Nombre de Usuario"
           className="w-10/12 text-center rounded-md h-[40px] bg-gradient-to-br from-black via-[#020131]  to-blue-950 border placeholder:text-gray-500 text-white"
         />
         <input
           type="text"
+          value={instagram}
+          onChange={(event) => setInstagram(event.target.value)}
           placeholder="Ingresar Instagram"
           className="w-10/12 text-center rounded-md h-[40px] bg-gradient-to-br from-black via-[#020131]  to-blue-950 border placeholder:text-gray-500 text-white"
         />
         <input
           type="text"
+          value={tiktok}
+          onChange={(event) => setTiktok(event.target.value)}
           placeholder="Ingresar TikTok"
           className="w-10/12 text-center rounded-md h-[40px] bg-gradient-to-br from-black via-[#020131]  to-blue-950 border placeholder:text-gray-500 text-white"
         />
         <input
           type="text"
+          value={drive}
+          onChange={(event) => setDrive(event.target.value)}
           placeholder="Ingresar Google Drive"
           className="w-10/12 text-center rounded-md h-[40px] bg-gradient-to-br from-black via-[#020131]  to-blue-950 border placeholder:text-gray-500 text-white"
         />
-        <button className="w-5/12 text-center rounded-md mt-6 border h-[40px] px-3 bg-gradient-to-t from-black via-[#020131]  to-blue-600 ">
+        <button
+          className="w-5/12 text-center rounded-md mt-6 border h-[40px] px-3 bg-gradient-to-t from-black via-[#020131]  to-blue-600 "
+          onClick={handleSaveChanges}
+        >
           Guardar
         </button>
       </div>
