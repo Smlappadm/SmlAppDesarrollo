3035d50489a9205da7f84aaed1a72b0a94ef91d2
Jonavoe <voeffray.jonathan@gmail.com>
2023-06-16 21:10:26 -0300

    Update Landing.jsx

--- client/src/views/Landing/Landing.jsx
+++ client/src/views/Landing/Landing.jsx
@@ -87,16 +87,6 @@ function Landing() {
           )}
         </div>
       </div>
-
-      <script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=UA-54550355-2"
-      ></script>
-      <script>
-        window.dataLayer = window.dataLayer || []; function gtag()
-        {dataLayer.push(arguments)}
-        gtag('js', new Date()); gtag('config', 'UA-54550355-2');
-      </script>
     </>
   );
 }
