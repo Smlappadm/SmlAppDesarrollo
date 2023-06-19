e63fa33e036c4af30e44b6dcee022a516f020854
juanfacundotam <facutam@gmail.com>
2023-06-19 16:07:59 -0300

    Update ClientesHandlers.js

--- api/src/Handlers/ClientesHandlers.js
+++ api/src/Handlers/ClientesHandlers.js
@@ -51,12 +51,11 @@ const getClientByEmailHandler = async (req, res) => {
   }
 };
 const paymentClienteHandler = async (req, res) => {
-
   // const { email } = req.query;
   // console.log("entro")
+  const pago = 200
   try {
-    // const pago= await createPayment(pago);
-    const pago= await createPayment();
+    const pago= await createPayment(pago);
     res.status(200).json(pago);
   } catch (error) {
     res.status(404).json({ error: error.message });
