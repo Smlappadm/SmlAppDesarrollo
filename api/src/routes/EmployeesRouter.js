b4a6493edd0713df395ad903886a3c957b3b673c
Jonavoe <voeffray.jonathan@gmail.com>
2023-06-19 16:31:27 -0300

    Update EmployeesRouter.js

--- api/src/routes/EmployeesRouter.js
+++ api/src/routes/EmployeesRouter.js
@@ -5,13 +5,17 @@ const {
   updateEmployByEmailHandler,
   getAllEmployeesHandler,
   deleteEmployeesByEmailHandler,
+  updateBannedEmployHandler,
+  getEmployeesBannedHandler,
 } = require("../Handlers/employeesHandlers");
 const EmployeesRouter = Router();
 
 EmployeesRouter.get("/", getAllEmployeesHandler);
+EmployeesRouter.get("/banned", getEmployeesBannedHandler);
 EmployeesRouter.post("/", postEmployeesHandler);
 EmployeesRouter.get("/email", getEmployeesByEmailHandler);
 EmployeesRouter.delete("/", deleteEmployeesByEmailHandler);
 EmployeesRouter.put("/email", updateEmployByEmailHandler);
+EmployeesRouter.put("/email/banned", updateBannedEmployHandler);
 
 module.exports = EmployeesRouter;
