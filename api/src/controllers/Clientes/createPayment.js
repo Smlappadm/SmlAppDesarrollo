df9bbdd81c6e04d2b021a3c586791f0f6337d74b
juanfacundotam <facutam@gmail.com>
2023-06-19 16:07:57 -0300

    Update createPayment.js

--- api/src/controllers/Clientes/createPayment.js
+++ api/src/controllers/Clientes/createPayment.js
@@ -1,24 +1,24 @@
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-const paymentIntent = async (req, res) => {
-  console.log("asdasdasdasdasd");
+const createPayment = async (pago) => {
+  
+  // try {
+    // const paymentIntent = await stripe.paymentIntents.create({
+    //   amount: 100, // Precio en centavos (por ejemplo, $1.00)
+    //   currency: "usd",
+    //   payment_method: "", // Aquí debes proporcionar el ID del método de pago
+    //   confirm: true,
+    // });
 
-  try {
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: 100, // Precio en centavos (por ejemplo, $1.00)
-      currency: "usd",
-      payment_method: "", // Aquí debes proporcionar el ID del método de pago
-      confirm: true,
-    });
-
-    res.send({
-      clientSecret: paymentIntent.client_secret,
-    });
-  } catch (error) {
-    res.send(error.raw.message);
-    console.log(error.raw.message);
-  }
+    // res.send({
+    //   clientSecret: paymentIntent.client_secret,
+    // });
+    return stripe;
+  // } catch (error) {
+  //   res.send(error.raw.message);
+  //   console.log(error.raw.message);
+  // }
 };
 
-module.exports = { paymentIntent };
+module.exports = createPayment;
 
