f63512262c528a33d1bd3f5321bdb6f7572259c8
juanfacundotam <facutam@gmail.com>
2023-06-19 15:00:34 -0300

    Update Pagos.jsx

--- client/src/componentsClientes/Pagos/Pagos.jsx
+++ client/src/componentsClientes/Pagos/Pagos.jsx
@@ -8,7 +8,8 @@ import ConfirmacionPago from "./ConfirmacionPago";
 // const { STRIPE_SECRET_KEY } = process.env;
 // Make sure to call `loadStripe` outside of a component’s render to avoid
 // recreating the `Stripe` object on every render.
-const stripePromise = loadStripe('pk_test_51NJhsbGpn5uZGCfpbyEu252jvDVNlqDiljFxifEkG5rAba4tu11lt9wl3m3UP1xFL3tnUGtPxT0KLMjNSnl6SO7o00xs2avzC5');
+const stripePublicKey = 'pk_test_51NJhsbGpn5uZGCfpbyEu252jvDVNlqDiljFxifEkG5rAba4tu11lt9wl3m3UP1xFL3tnUGtPxT0KLMjNSnl6SO7o00xs2avzC5'
+const stripePromise = loadStripe(stripePublicKey);
 
 export const Pagos = () => {
 
