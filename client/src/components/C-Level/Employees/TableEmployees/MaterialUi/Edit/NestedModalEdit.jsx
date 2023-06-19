35c8b05e4ec141405d887ec29462f8ad1cda6d9b
Jonavoe <voeffray.jonathan@gmail.com>
2023-06-19 20:48:52 -0300

    Update NestedModalEdit.jsx

--- client/src/components/C-Level/Employees/TableEmployees/MaterialUi/Edit/NestedModalEdit.jsx
+++ client/src/components/C-Level/Employees/TableEmployees/MaterialUi/Edit/NestedModalEdit.jsx
@@ -53,7 +53,7 @@ function ChildModalDelete({
   const handleCreate = async () => {
     try {
       if (itemRol === "clevel" || itemRol === "leader") {
-        await axios.put(`/${itemRol}/email/email?email=${itemEmail}`, {
+        await axios.put(`/${itemRol}/email?email=${itemEmail}`, {
           deleted: true,
         });
         await axios.put(`/corredor/email/email?email=${itemEmail}`, {
