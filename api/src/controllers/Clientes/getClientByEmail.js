66394a2d94b752273c225df32aba3b9fb1436c83
Jonavoe <voeffray.jonathan@gmail.com>
2023-06-21 15:04:37 -0300

    Update getClientByEmail.js

--- api/src/controllers/Clientes/getClientByEmail.js
+++ api/src/controllers/Clientes/getClientByEmail.js
@@ -2,7 +2,6 @@ const Clientes = require("../../models/Clientes");
 
 const getClientByEmail = async (email) => {
   const client = await Clientes.findOne({ email });
-  console.log(client);
   return client;
 };
 
