11540968b50dacd770572c1a982e013b9b63fb21
juanfacundotam <facutam@gmail.com>
2023-06-21 17:50:49 -0300

    Create CheckoutPage.jsx

---
+++ client/src/viewsClientes/CheckoutPage/CheckoutPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const CheckoutPage = () => {
+  return (
+    <div className="flex flex-col justify-center items-center w-screen h-screen">
+      <p>pago realizado</p>
+      <p>enviar mail</p>
+    </div>
+  );
+};
