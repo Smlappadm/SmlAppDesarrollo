5afb5ba5ca634b8523d2ebdd84639110f9bce235
juanfacundotam <facutam@gmail.com>
2023-06-22 17:28:31 -0300

    Update CheckoutPage.jsx

--- client/src/viewsClientes/CheckoutPage/CheckoutPage.jsx
+++ client/src/viewsClientes/CheckoutPage/CheckoutPage.jsx
@@ -4,7 +4,8 @@ export const CheckoutPage = () => {
   return (
     <div className="flex flex-col justify-center items-center w-screen h-screen">
       <p>pago realizado</p>
-      <p>enviar mail</p>
+      <p>enviar mail cliente del pago</p>
+      <p>enviar mail a tesorería para registrar el pago</p>
     </div>
   );
 };
