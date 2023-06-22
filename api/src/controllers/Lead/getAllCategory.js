2d2976987c475fe2d9bb8ed6f2dbb6574cf5f86c
Jonavoe <voeffray.jonathan@gmail.com>
2023-06-22 17:19:24 -0300

    Revert "Create getAllCategory.js"
    This reverts commit ca0462d6b8c392f35027bfb7337b1a441432ee12.

--- api/src/controllers/Lead/getAllCategory.js
+++
@@ -1,8 +0,0 @@
-const Lead = require("../../models/Lead");
-
-const getAllCategory = async () => {
-  const professions = await Lead.distinct("category", { checked: false });
-  return professions;
-};
-
-module.exports = getAllCategory;
