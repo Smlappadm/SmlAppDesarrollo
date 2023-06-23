3d589e3af5fd128ae7d24206f6e427b32701a223
Jonavoe <voeffray.jonathan@gmail.com>
2023-06-22 21:42:26 -0300

    Update findLeadCorredorNameAllInfo.js

--- api/src/controllers/Lead/findLeadCorredorNameAllInfo.js
+++ api/src/controllers/Lead/findLeadCorredorNameAllInfo.js
@@ -1,12 +1,6 @@
 const Lead = require("../../models/Lead");
 
-const findLeadCorredorNameAllInfo = async (
-  name,
-  month,
-  year,
-  fromDay,
-  toDay
-) => {
+const findLeadCorredorNameAllInfo = async (name, fromDay, toDay) => {
   const regex = name ? new RegExp(name, "i") : /.*/;
   const query = {
     corredor: regex,
@@ -14,9 +8,26 @@ const findLeadCorredorNameAllInfo = async (
     view: true,
   };
 
-  if (month && year && fromDay && toDay) {
-    const startDate = new Date(year, month - 1, fromDay, 0, 0, 0);
-    const endDate = new Date(year, month - 1, toDay, 23, 59, 59);
+  if (fromDay && toDay) {
+    const [fromYear, fromMonth, fromDayOfMonth] = fromDay.split("-");
+    const [toYear, toMonth, toDayOfMonth] = toDay.split("-");
+
+    const startDate = new Date(
+      parseInt(fromYear),
+      parseInt(fromMonth) - 1,
+      parseInt(fromDayOfMonth),
+      0,
+      0,
+      0
+    );
+    const endDate = new Date(
+      parseInt(toYear),
+      parseInt(toMonth) - 1,
+      parseInt(toDayOfMonth),
+      23,
+      59,
+      59
+    );
     query.updatedAt = {
       $gte: startDate,
       $lt: endDate,
