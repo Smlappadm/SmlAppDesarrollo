e7c9036f82484d9c7ebad3e8a4552f2861b27e92
Jonavoe <voeffray.jonathan@gmail.com>
2023-06-23 18:48:33 -0300

    Update getCorredores.js

--- api/src/controllers/Lead/getCorredores.js
+++ api/src/controllers/Lead/getCorredores.js
@@ -16,22 +16,3 @@ const getCorredores = async () => {
 };
 
 module.exports = getCorredores;
-
-// const Lead = require("../../models/Lead");
-
-// const getCorredores = async () => {
-//   const corredores = await Lead.distinct("corredor", { corredor: { $ne: "" } });
-
-//   const correosSet = new Set();
-
-//   corredores.forEach((corredor) => {
-//     const correo = corredor.split("fullName=")[0];
-//     correosSet.add(correo);
-//   });
-
-//   const correos = Array.from(correosSet);
-
-//   return correos;
-// };
-
-// module.exports = getCorredores;
