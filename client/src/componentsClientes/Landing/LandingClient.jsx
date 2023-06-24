2f4232c69910d0de95b1cd789cd48febae5fdb8a
Nikitoo17 <nicolasfadel17@gmail.com>
2023-06-23 21:07:13 -0300

    Update LandingClient.jsx

--- client/src/componentsClientes/Landing/LandingClient.jsx
+++ client/src/componentsClientes/Landing/LandingClient.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import Name from "./Name/Name";
 import Header from "./Header/Header";
 import Followers from "./Followers/Followers";
+
 export default function LandingClient({
   imgInstagram,
   setMaxNumber,
