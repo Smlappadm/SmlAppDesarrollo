c450b44469a9075e843dea24355647561343ce82
Nikitoo17 <nicolasfadel17@gmail.com>
2023-06-28 18:55:12 -0300

    Update VistaGeneral.jsx

--- client/src/viewsClientes/VistaGeneral/VistaGeneral.jsx
+++ client/src/viewsClientes/VistaGeneral/VistaGeneral.jsx
@@ -3,10 +3,18 @@ import Recursos from "../../componentsClientes/Recursos/Recursos";
 import { Agendar } from "../../componentsClientes/Agendar/Agendar";
 import Avances from "../../componentsClientes/Avances/Avances";
 
-export const VistaGeneral = ({ seguidores, seguidoresGanados }) => {
+export const VistaGeneral = ({
+  seguidores,
+  seguidoresGanados,
+  videosPublicados,
+}) => {
   return (
     <div className="flex flex-col md:flex-row justify-center md:gap-9 items-center w-full h-full mt-8">
-      <Avances seguidores={seguidores} seguidoresGanados={seguidoresGanados} />
+      <Avances
+        seguidores={seguidores}
+        seguidoresGanados={seguidoresGanados}
+        videosPublicados={videosPublicados}
+      />
 
       <div className="flex flex-col">
         <Recursos />
