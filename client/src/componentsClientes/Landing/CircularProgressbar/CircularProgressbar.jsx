9647deb10403452235dd092fe648a6c1c23f04a6
Nikitoo17 <nicolasfadel17@gmail.com>
2023-06-28 20:20:27 -0300

    Update CircularProgressbar.jsx

--- client/src/componentsClientes/Landing/CircularProgressbar/CircularProgressbar.jsx
+++ client/src/componentsClientes/Landing/CircularProgressbar/CircularProgressbar.jsx
@@ -4,16 +4,34 @@ import "react-circular-progressbar/dist/styles.css";
 
 const CircularProgressBar = ({ value, imageSrc }) => {
   const percentage = (value / 10000) * 100;
+  const getPathColor = (percentage) => {
+    let color;
+    if (percentage >= 0 && percentage <= 25) {
+      color = "linear-gradient(to right, #570387, red)";
+    } else if (percentage > 25 && percentage <= 50) {
+      color = "linear-gradient(to right, red, orange)";
+    } else if (percentage > 50 && percentage <= 75) {
+      color = "linear-gradient(to right, orange, yellow)";
+    } else if (percentage > 75 && percentage <= 100) {
+      color = "linear-gradient(to right, red, #570387)";
+    } else {
+      color = "#570387";
+    }
+    return color;
+  };
+
+  const styles = buildStyles({
+    pathColor: "#570387",
+    trailColor: `transparent`,
+    backgroundColor: `linear-gradient(to right, purple, red)`,
+  });
 
   return (
     <div style={{ width: "8.2rem", position: "relative" }}>
       <CircularProgressbar
         value={percentage}
         strokeWidth={10}
-        styles={buildStyles({
-          pathColor: "#570387",
-          trailColor: "transparent",
-        })}
+        styles={styles}
       />
       <img
         src={imageSrc}
