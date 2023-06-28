fd1f7e75925eba78f55b8b434c875758d90777d7
Nikitoo17 <nicolasfadel17@gmail.com>
2023-06-27 21:48:33 -0300

    Update Home.jsx

--- client/src/viewsClientes/Home/Home.jsx
+++ client/src/viewsClientes/Home/Home.jsx
@@ -99,7 +99,7 @@ export default function Home() {
     //   `https://apiflask-td8y.onrender.com/obtener_info_instagram?username=${userIG}`
     // );
     // const infoIG = responseIG.data;
-    const infoIG = { seguidores: "2005" };
+    const infoIG = { seguidores: "2006" };
     const infoTT = { seguidores: "200", likes: "10" };
     setNumberInstagram(parseInt(infoIG.seguidores));
     setNumberTiktok(parseInt(infoTT.seguidores));
