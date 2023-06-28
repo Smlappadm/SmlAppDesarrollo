1af48a25bf48f172de279e53548c05a2d37f6683
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-06-28 18:34:18 -0300

    Create InputRunner.jsx

---
+++ client/src/components/Vendedores/Dashboard/Select/InputRunner.jsx
@@ -0,0 +1,196 @@
+import React, { useEffect, useState } from "react";
+import Box from "@mui/material/Box";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import Button from "@mui/material/Button";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  findCorredoresByNameAllInfo,
+  getAllCategory,
+  getAllCountries,
+  getAllProfesion,
+} from "../../../../redux/actions";
+
+export default function InputName() {
+  const dispatch = useDispatch();
+  const [profesion, setProfesion] = useState("");
+  const [category, setCategory] = useState("");
+  const [country, setCountry] = useState("");
+
+  const { allProfesion } = useSelector((state) => state);
+  const { allCategory } = useSelector((state) => state);
+  const { allCountries } = useSelector((state) => state);
+
+  useEffect(() => {
+    dispatch(getAllProfesion());
+    dispatch(getAllCategory());
+    dispatch(getAllCountries());
+  }, [dispatch]);
+
+  const handleChangeProfesion = (event) => {
+    let value = event.target.value;
+    setProfesion(value);
+  };
+
+  const handleChangeCategory = (event) => {
+    let value = event.target.value;
+    setCategory(value);
+  };
+
+  const handleChangeCountries = (event) => {
+    let value = event.target.value;
+    setCountry(value);
+  };
+
+
+  const handleFilterClick = () => {
+    dispatch(
+      findCorredoresByNameAllInfo(
+        profesion,
+        country,
+        category,
+      )
+    );
+  };
+
+  const handleFilterReset = () => {
+    setProfesion("");
+    setCategory("");
+    setCountry("");
+  };
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: "10px",
+        width: "50%",
+        height: "33px",
+        color: "gray",
+        paddingBottom: "10px",
+        margin: "0px 10px",
+        "& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
+          borderColor: "white",
+        },
+        "&:hover .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
+          borderColor: "white",
+        },
+        "&.focused .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
+          borderColor: "green",
+        },
+      }}
+    >
+      <div className="flex gap-5">
+        <div className="flex flex-col">
+          <label>Profesion:</label>
+          <Select
+            value={profesion}
+            onChange={handleChangeProfesion}
+            label=""
+            id="runner"
+            size="small"
+            variant="outlined"
+            displayEmpty
+            inputProps={{
+              style: {
+                color: "white",
+              },
+            }}
+            sx={{
+              color: "white",
+              "& .MuiOutlinedInput-input": {
+                padding: "9.5px 14px",
+              },
+              "& .MuiSelect-outlined": {
+                paddingRight: "28px",
+              },
+            }}
+          >
+            <MenuItem value="">Profesion</MenuItem>
+            {allProfesion.map((profesion) => (
+              <MenuItem key={profesion} value={profesion}>
+                {profesion}
+              </MenuItem>
+            ))}
+          </Select>
+        </div>
+        <div className="flex flex-col">
+          <label>Categoria:</label>
+          <Select
+            value={category}
+            onChange={handleChangeCategory}
+            label=""
+            id="runner"
+            size="small"
+            variant="outlined"
+            displayEmpty
+            inputProps={{
+              style: {
+                color: "white",
+              },
+            }}
+            sx={{
+              color: "white",
+              "& .MuiOutlinedInput-input": {
+                padding: "9.5px 14px",
+              },
+              "& .MuiSelect-outlined": {
+                paddingRight: "28px",
+              },
+            }}
+          >
+            <MenuItem value="">Categoria</MenuItem>
+            {allCategory.map((category) => (
+              <MenuItem key={category} value={category}>
+                {category}
+              </MenuItem>
+            ))}
+          </Select>
+        </div>
+        <div className="flex flex-col">
+          <label>Paises:</label>
+          <Select
+            value={country}
+            onChange={handleChangeCountries}
+            label=""
+            id="runner"
+            size="small"
+            variant="outlined"
+            displayEmpty
+            inputProps={{
+              style: {
+                color: "white",
+              },
+            }}
+            sx={{
+              color: "white",
+              "& .MuiOutlinedInput-input": {
+                padding: "9.5px 14px",
+              },
+              "& .MuiSelect-outlined": {
+                paddingRight: "28px",
+              },
+            }}
+          >
+            <MenuItem value="">Pais</MenuItem>
+            {allCountries.map((country) => (
+              <MenuItem key={country} value={country}>
+                {country}
+              </MenuItem>
+            ))}
+          </Select>
+        </div>
+        <div className="flex gap-5 items-end justify-center">
+          <Button onClick={handleFilterClick} variant="contained" size="large">
+            Filtrar
+          </Button>
+          <Button onClick={handleFilterReset} variant="outlined" size="large">
+            Reset
+          </Button>
+        </div>
+      </div>
+    </Box>
+  );
+}
