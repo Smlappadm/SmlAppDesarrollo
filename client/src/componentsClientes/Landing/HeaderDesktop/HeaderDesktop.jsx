de0a04b9051dcda9aa0e0b198816bc01b3200488
Jonavoe <voeffray.jonathan@gmail.com>
2023-06-29 16:46:07 -0300

    Update HeaderDesktop.jsx

--- client/src/componentsClientes/Landing/HeaderDesktop/HeaderDesktop.jsx
+++ client/src/componentsClientes/Landing/HeaderDesktop/HeaderDesktop.jsx
@@ -9,8 +9,9 @@ export default function HeaderDesktop({
   setCustomValue,
   imgInstagram,
 }) {
+  const nameIG = localStorage.getItem("instagram");
   return (
-    <div className="flex flex-col justify-center items-center w-screen">
+    <div className="flex flex-col justify-center items-center w-10/12">
       <div className="flex justify-between w-screen">
         <div className=" justify-center items-center relative">
           <img
@@ -19,24 +20,32 @@ export default function HeaderDesktop({
             alt=""
           />
         </div>
-        <div className="flex  justify-center items-start gap-3 pt-4">
-          <Link
-            to="/clientes-addvideos"
-            className="font-bold  md:border-2 md:border-[#211f52] md:rounded-lg hover:bg-[#2a286e] "
-          >
-            <AiOutlineVideoCameraAdd
-              className="font-bold"
-              color="#fff"
-              size={30}
-            />
+
+        {nameIG !== "" ? (
+          <span className="text-xl">@{nameIG}</span>
+        ) : (
+          <Link to="/clientes-settings">
+            <button className="border-2 border-[#07A1F8] bg-[#07A1F8] text-white px-6 py-2  rounded-full">
+              Agregar Instagram
+            </button>
           </Link>
-          <Link
-            to="/clientes-settings"
-            className="flex justify-center items-center font-bold pt-0.5 md:pt-0 md:border-2 md:border-[#211f52] md:rounded-lg md:w-8 md:h-8 hover:bg-[#2a286e] "
-          >
-            <IoSettingsOutline className="font-bold" color="#fff" size={26} />
-          </Link>
-        </div>
+        )}
+        <Link
+          to="/clientes-addvideos"
+          className="font-bold  md:border-2 md:border-[#211f52] md:rounded-lg hover:bg-[#2a286e] "
+        >
+          <AiOutlineVideoCameraAdd
+            className="font-bold"
+            color="#fff"
+            size={30}
+          />
+        </Link>
+        <Link
+          to="/clientes-settings"
+          className="flex justify-center items-center font-bold pt-0.5 md:pt-0 md:border-2 md:border-[#211f52] md:rounded-lg md:w-8 md:h-8 hover:bg-[#2a286e] "
+        >
+          <IoSettingsOutline className="font-bold" color="#fff" size={26} />
+        </Link>
       </div>
     </div>
   );
