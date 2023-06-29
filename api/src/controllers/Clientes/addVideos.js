db22911cd3ab872b89a0747f083c1bb5e3ea5420
Nikitoo17 <nicolasfadel17@gmail.com>
2023-06-29 14:44:28 -0300

    Update addVideos.js

--- api/src/controllers/Clientes/addVideos.js
+++ api/src/controllers/Clientes/addVideos.js
@@ -1,11 +1,17 @@
 const Clientes = require("../../models/Clientes");
 
 const addVideos = async (email, linksVideos) => {
-  const newVideo = await Clientes.findOneAndUpdate(
-    { email: email },
-    { $addToSet: { videosPublicados: linksVideos } },
-    { new: true }
-  );
-  return newVideo;
+  const linked = await Clientes.findOne({
+    email: email,
+    "videosPublicados.link": linksVideos.link,
+  });
+  if (!linked) {
+    const newVideo = await Clientes.findOneAndUpdate(
+      { email: email },
+      { $addToSet: { videosPublicados: linksVideos } },
+      { new: true }
+    );
+    return newVideo;
+  }
 };
 module.exports = addVideos;
