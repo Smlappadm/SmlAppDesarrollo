912fa915eac87dcb23fefd3edf9ce6fa2536bea8
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-06-29 20:16:24 -0300

    Update createPayment.js

--- api/src/controllers/Clientes/createPayment.js
+++ api/src/controllers/Clientes/createPayment.js
@@ -23,16 +23,22 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 //   // }
 // };
 
-const createPayment = async ({ id, amount, name }) => {
+const createPayment = async ({ id, name, monto, cuotas, cuotasRestantes, valorCuota }) => {
+  // console.log(name)
+  // console.log(valorCuota)
+  const description = `cuotas ${cuotasRestantes}/${cuotas}`
   const session = await stripe.checkout.sessions.create({
     line_items: [
       {
         price_data: {
           product_data: {
+            images: ["https://images-ext-1.discordapp.net/external/VmotedpeNAAv9Sz0GZI5iLiobf_7NpJn24pyas4ed_Y/https/i.postimg.cc/4y1YcByV/1685492595204-removebg-preview.webp"],
             name: name,
+            description: description
+            // description: `cuotas ${cuotasRestantes}/${cuotas}`
           },
           currency: "usd",
-          unit_amount: amount,
+          unit_amount: (valorCuota * 100),
         },
         quantity: 1,
       },
