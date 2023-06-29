0d5819214d8a1fa25f07b4de4c255507628fd2b5
Nikitoo17 <nicolasfadel17@gmail.com>
2023-06-29 14:06:44 -0300

    Update CustomsLabelAvances.jsx

--- client/src/componentsClientes/Avances/CustomsLabelAvances/CustomsLabelAvances.jsx
+++ client/src/componentsClientes/Avances/CustomsLabelAvances/CustomsLabelAvances.jsx
@@ -1,6 +1,13 @@
 import React from "react";
-
-export default function CustomsLabelAvances({ text, suma, sumaTotal, value }) {
+import igPng from "../../../Assets/instagram.png";
+import tkPng from "../../../Assets/tik-tok.png";
+export default function CustomsLabelAvances({
+  text,
+  ganadosTT,
+  ganadosIG,
+  IG,
+  TT,
+}) {
   return (
     // Contenedor principal del avance
     <div className="flex justify-between gap-5 items-center rounded-xl py-4 my-2 bg-[#282828]">
@@ -9,16 +16,22 @@ export default function CustomsLabelAvances({ text, suma, sumaTotal, value }) {
         <p className="text-[#fff] text-[.7rem] font-bold ml-4">{text}</p>
       </div>
       {/* Contenedor para mostrar las sumas y el valor */}
-      <div className="flex gap-2">
-        <div className="flex gap-5 justify-center items-center">
-          {/* Suma total */}
-          {sumaTotal ? (
-            <p className="text-[#0f0] font-bold">+{sumaTotal}</p>
-          ) : null}
-          {/* Suma */}
-          {/* <p className="text-[#0f0] text-[0.8rem] font-bold">+ {suma}</p> */}
-          {/* Valor */}
-          <p className="text-[#fff] text-[1.2rem] font-bold mr-5">{value}</p>
+      <div className="flex flex-col">
+        <div className="flex gap-2">
+          <div className="flex gap-5 justify-center items-center">
+            {ganadosTT ? (
+              <p className="text-[#0f0] font-bold">+{ganadosIG}</p>
+            ) : null}
+            <p className="text-[#fff] text-[1.2rem] font-bold mr-5">{IG}</p>
+          </div>
+        </div>
+        <div className="flex gap-2">
+          <div className="flex gap-5 justify-center items-center">
+            {ganadosTT ? (
+              <p className="text-[#0f0] font-bold">+{ganadosTT}</p>
+            ) : null}
+            <p className="text-[#fff] text-[1.2rem] font-bold mr-5">{TT}</p>
+          </div>
         </div>
       </div>
     </div>
