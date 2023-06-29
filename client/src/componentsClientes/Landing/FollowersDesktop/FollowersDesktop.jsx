67d42050b5cad7f545c15edb91570844e8dfcec6
Jonavoe <voeffray.jonathan@gmail.com>
2023-06-29 16:46:09 -0300

    Update FollowersDesktop.jsx

--- client/src/componentsClientes/Landing/FollowersDesktop/FollowersDesktop.jsx
+++ client/src/componentsClientes/Landing/FollowersDesktop/FollowersDesktop.jsx
@@ -26,6 +26,9 @@ export default function FollowersDesktop({
 
   return (
     <div className="flex flex-col justify-center items-center w-screen h-fit relative mt-3">
+      <div className="flex w-full justify-center items-center">
+        <span className="text-white font-bold text-[2rem]">{name}</span>
+      </div>
       <div className="flex justify-between w-7/12">
         <div className=" pt-10 justify-center items-center relative">
           <CircularProgressBar value={numberTotal} imageSrc={imgInstagram} />
@@ -33,52 +36,42 @@ export default function FollowersDesktop({
             {(numberTotal / 10000) * 100}%
           </p>
         </div>
-        <div>
-          <div className="flex flex-col">
-            <span className="text-white font-bold text-3xl">{name}</span>
-            {/* {nameIG !== "" ? (
-              <span className="text-xl">@{nameIG}</span>
-            ) : (
-              <Link to="/clientes-settings">
-                <button className="border-2 border-[#07A1F8] bg-[#07A1F8] text-white px-6 py-2  rounded-full">
-                  Agregar Instagram
-                </button>
-              </Link>
-            )} */}
-          </div>
-          <div className="">
-            <label
-              htmlFor=""
-              className="text-[45px] font-semibold text-white  px-2"
-            >
-              {formatearNumeroConPuntos(numberInstagram + numberTiktok)}
-            </label>
+        <div className="">
+          <div className="flex flex-col justify-center items-center">
+            <div className="">
+              <label
+                htmlFor=""
+                className="text-[45px] font-semibold text-white  px-2"
+              >
+                {formatearNumeroConPuntos(numberInstagram + numberTiktok)}
+              </label>
 
-            <label htmlFor="" className="text-[.7rem] top-9 absolute">
-              / {maxNumber}
-            </label>
-          </div>
-          <div className="absolute top-13">
-            <label htmlFor="" className="font-thin ">
-              followers
-            </label>
-          </div>
-          <div className="flex justify-center items-center w-fit h-fit gap-4 mt-6">
-            <img src={igPng} alt="icono de Instagram" className="w-8 h-8" />
-            <label
-              htmlFor=""
-              className="text-[27px] text-white font-semibold  px-2 pb-1"
-            >
-              {formatearNumeroConPuntos(numberInstagram)}
-            </label>
-            <label className=" border-white bg-white w-[0.5px] h-5  text-white text-18"></label>
-            <label
-              htmlFor=""
-              className="text-[27px] text-white font-semibold px-2 pb-1"
-            >
-              {formatearNumeroConPuntos(numberTiktok)}
-            </label>
-            <img src={tkPng} alt="icono de Tiktok" className="w-8 h-8" />
+              <label htmlFor="" className="text-[1rem] top-20 absolute">
+                / {maxNumber}
+              </label>
+            </div>
+            <div className="absolute">
+              <label htmlFor="" className="font-thin ">
+                followers
+              </label>
+            </div>
+            <div className="flex justify-center items-center w-fit h-fit gap-4 mt-6">
+              <img src={igPng} alt="icono de Instagram" className="w-8 h-8" />
+              <label
+                htmlFor=""
+                className="text-[27px] text-white font-semibold  px-2 pb-1"
+              >
+                {formatearNumeroConPuntos(numberInstagram)}
+              </label>
+              <label className=" border-white bg-white w-[0.5px] h-5  text-white text-18"></label>
+              <label
+                htmlFor=""
+                className="text-[27px] text-white font-semibold px-2 pb-1"
+              >
+                {formatearNumeroConPuntos(numberTiktok)}
+              </label>
+              <img src={tkPng} alt="icono de Tiktok" className="w-8 h-8" />
+            </div>
           </div>
         </div>
       </div>
