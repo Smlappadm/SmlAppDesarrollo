41706f25e26a58670af29c70e52e3e34e2ee4180
Nikitoo17 <nicolasfadel17@gmail.com>
2023-06-29 16:44:33 -0300

    Update CustomsLabelAvances.jsx

--- client/src/componentsClientes/Avances/CustomsLabelAvances/CustomsLabelAvances.jsx
+++ client/src/componentsClientes/Avances/CustomsLabelAvances/CustomsLabelAvances.jsx
@@ -42,15 +42,15 @@ export default function CustomsLabelAvances({
         </div>
       ) : (
         // Contenedor principal del avance
-        <div className="flex justify-between gap-5 items-center rounded-xl py-4 my-2 bg-[#282828]">
+        <div className="flex justify-between gap-5 items-center rounded-xl py-1 my-2 bg-[#282828]">
           {/* Texto del avance */}
           <div className="w-96">
             <p className="text-[#fff] text-[20px] font-bold ml-4">{text}</p>
           </div>
           {/* Contenedor para mostrar las sumas y el valor */}
           <div className="flex flex-col">
-            <div className="flex gap-2 justify-end">
-              <div className="flex gap-3 justify-center items-center">
+            <div className="flex gap-x-2 justify-end">
+              <div className="flex gap-x-3 justify-center items-center">
                 {ganadosTT ? (
                   <p className="text-[#0f0] font-bold">+{ganadosIG}</p>
                 ) : null}
