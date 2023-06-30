cc3ba70292e0a62615e608856a6616c07acdd795
Nikitoo17 <nicolasfadel17@gmail.com>
2023-06-30 17:01:28 -0300

    Update LoginClientes.jsx

--- client/src/viewsClientes/Login/LoginClientes.jsx
+++ client/src/viewsClientes/Login/LoginClientes.jsx
@@ -5,6 +5,7 @@ import LoginDesktop from "../../componentsClientes/Login/LoginDesktop";
 import Register from "../../componentsClientes/Login/Register";
 import background from "../../Assets/borde1.png";
 import background2 from "../../Assets/borde2.png";
+import RegisterDesktop from "../../componentsClientes/Login/RegisterDesktop";
 
 export default function Home({ tamañoPantalla }) {
   const navigate = useNavigate();
@@ -53,12 +54,12 @@ export default function Home({ tamañoPantalla }) {
       }
       style={styles()}
     >
-      <div className="flex flex-col justify-center items-center w-96">
+      <div className="flex flex-col justify-center items-center w-screen">
         {tamañoPantalla === "Pequeña" ? (
           <img
             src="https://images-ext-1.discordapp.net/external/VmotedpeNAAv9Sz0GZI5iLiobf_7NpJn24pyas4ed_Y/https/i.postimg.cc/4y1YcByV/1685492595204-removebg-preview.webp"
             alt=""
-            className="w-40 h-40"
+            className="w-40 h-40 absolute left-3 top-3"
             onClick={handleOpenRegister}
           />
         ) : null}
@@ -75,8 +76,13 @@ export default function Home({ tamañoPantalla }) {
               handleJoin={handleJoin}
             />
           )
-        ) : (
+        ) : tamañoPantalla === "Pequeña" ? (
           <Register
+            handleOpenRegister={handleOpenRegister}
+            refeerred={referred}
+          />
+        ) : (
+          <RegisterDesktop
             handleOpenRegister={handleOpenRegister}
             refeerred={referred}
           />
