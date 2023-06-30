41366e930de56493be3384b5f7316c038f3c7f6b
Nikitoo17 <nicolasfadel17@gmail.com>
2023-06-30 16:43:59 -0300

    Update RegisterDesktop.jsx

--- client/src/componentsClientes/Login/RegisterDesktop.jsx
+++ client/src/componentsClientes/Login/RegisterDesktop.jsx
@@ -1,4 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { IoEyeOffSharp, IoEyeSharp } from "react-icons/io5";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllClientes } from "../../redux/actions";
 
 export default function RegisterDesktop() {
   return <div>RegisterDesktop</div>;
