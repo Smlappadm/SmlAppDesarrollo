26898bbd5ffae0346b8d87639f8504b49e7be9f8
Nikitoo17 <nicolasfadel17@gmail.com>
2023-06-30 15:51:32 -0300

    Update LoginDesktop.jsx

--- client/src/componentsClientes/Login/LoginDesktop.jsx
+++ client/src/componentsClientes/Login/LoginDesktop.jsx
@@ -13,5 +13,16 @@ export default function LoginDesktop({ handleOpenRegister, handleJoin }) {
   const handlePasswordView = () => {
     setShowView(!showView);
   };
+  const handleSubmit = async () => {
+    const response = await axios.get(`/clientes/username?username=${username}`);
+    const client = response.data;
+    console.log(client);
+    if (username === client.username && password === client.password) {
+      handleJoin();
+    } else {
+      console.log("todo mal");
+    }
+  };
+
   return <div>LoginDesktop</div>;
 }
