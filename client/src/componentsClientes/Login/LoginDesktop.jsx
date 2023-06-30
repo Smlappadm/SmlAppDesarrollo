23f2f602b7b1b0f3d5d8fd563cf7c0adcbd3af9a
Nikitoo17 <nicolasfadel17@gmail.com>
2023-06-30 15:51:22 -0300

    Update LoginDesktop.jsx

--- client/src/componentsClientes/Login/LoginDesktop.jsx
+++ client/src/componentsClientes/Login/LoginDesktop.jsx
@@ -5,5 +5,13 @@ import { IoEyeOffSharp, IoEyeSharp } from "react-icons/io5";
 import { SignIn, useUser } from "@clerk/clerk-react";
 
 export default function LoginDesktop({ handleOpenRegister, handleJoin }) {
+  const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [showView, setShowView] = useState(false);
+
+  const handlePasswordView = () => {
+    setShowView(!showView);
+  };
   return <div>LoginDesktop</div>;
 }
