d6ce558a7bada36a3bdf1275944db2ceb80b4f16
Nikitoo17 <nicolasfadel17@gmail.com>
2023-06-30 16:01:47 -0300

    Update LoginDesktop.jsx

--- client/src/componentsClientes/Login/LoginDesktop.jsx
+++ client/src/componentsClientes/Login/LoginDesktop.jsx
@@ -24,50 +24,52 @@ export default function LoginDesktop({ handleOpenRegister, handleJoin }) {
   //   };
 
   return (
-    <div className="flex flex-col">
-      <div className="flex flex-col items-center gap-y-4 mt-8">
-        <SignIn
-          routing="path"
-          path="/sign-in"
-          appearance={{
-            variables: {
-              spacingUnit: "0.8rem",
-              borderRadius: "10px",
-            },
-            layout: {
-              socialButtonsVariant: "blockButton",
-              socialButtonsPlacement: "top",
-            },
-            elements: {
-              socialButtonsBlockButton: "text-white bg-[#404062] m-0 ",
-              formButtonPrimary: "hidden",
-              formFieldInput: "hidden",
-              card: " bg-transparent m-0 p-0 flex items-center h-fit",
-              main: "flex flex-col p-0 m-0  w-3/4 bg-transparent",
-              form: "hidden",
-              formField: "hidden",
-              dividerRow: "hidden",
-              formFieldLabel: "hidden",
-              footerActionText: "hidden",
-              logoImage: "hidden",
-              headerTitle: "hidden",
-              headerSubtitle: "hidden",
-              socialButtonsBlockButton__slack: "hidden",
-              button: "hidden",
-              footerAction__signUp: "hidden",
-              footer: "hidden",
-              logoBox: "hidden",
-              navbar: "hidden",
-            },
-          }}
-          afterSignInUrl="/clientes-home"
-        />
+    <div className="flex">
+      <div className="flex flex-col">
+        <div className="flex flex-col items-center gap-y-4 mt-8">
+          <SignIn
+            routing="path"
+            path="/sign-in"
+            appearance={{
+              variables: {
+                spacingUnit: "0.8rem",
+                borderRadius: "10px",
+              },
+              layout: {
+                socialButtonsVariant: "blockButton",
+                socialButtonsPlacement: "top",
+              },
+              elements: {
+                socialButtonsBlockButton: "text-white bg-[#404062] m-0 ",
+                formButtonPrimary: "hidden",
+                formFieldInput: "hidden",
+                card: " bg-transparent m-0 p-0 flex items-center h-fit",
+                main: "flex flex-col p-0 m-0  w-3/4 bg-transparent",
+                form: "hidden",
+                formField: "hidden",
+                dividerRow: "hidden",
+                formFieldLabel: "hidden",
+                footerActionText: "hidden",
+                logoImage: "hidden",
+                headerTitle: "hidden",
+                headerSubtitle: "hidden",
+                socialButtonsBlockButton__slack: "hidden",
+                button: "hidden",
+                footerAction__signUp: "hidden",
+                footer: "hidden",
+                logoBox: "hidden",
+                navbar: "hidden",
+              },
+            }}
+            afterSignInUrl="/clientes-home"
+          />
 
-        <div className="flex ">
-          <p>¿No tienes cuenta?</p>
-          <button onClick={handleOpenRegister} className="text-blue-600 ml-1">
-            Crea una!
-          </button>
+          <div className="flex ">
+            <p>¿No tienes cuenta?</p>
+            <button onClick={handleOpenRegister} className="text-blue-600 ml-1">
+              Crea una!
+            </button>
+          </div>
         </div>
       </div>
     </div>
