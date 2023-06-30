b378fa31c4645ca423c0ee79ebe5112be5e27b3c
Jonavoe <voeffray.jonathan@gmail.com>
2023-06-30 19:36:47 -0300

    Create HandleParametros.jsx

---
+++ client/src/componentsClientes/Guiones/MUI/HandleParametros.jsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+
+export default function HandleParametros() {
+  const [categoria, setCategoria] = React.useState("");
+
+  const handleChange = (event) => {
+    setCategoria(event.target.value);
+  };
+
+  return (
+    <Box sx={{ width: 280 }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Parámetros</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={categoria}
+          label="Categoria"
+          onChange={handleChange}
+        >
+          <MenuItem value={1}>1</MenuItem>
+          <MenuItem value={2}>2</MenuItem>
+          <MenuItem value={3}>3</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
+  );
+}
