3dcbe705123b87b33cf4c0392f50dbb4d6aa0b9e
Nikitoo17 <nicolasfadel17@gmail.com>
2023-06-30 16:45:09 -0300

    Update RegisterDesktop.jsx

--- client/src/componentsClientes/Login/RegisterDesktop.jsx
+++ client/src/componentsClientes/Login/RegisterDesktop.jsx
@@ -62,5 +62,83 @@ export default function RegisterDesktop({ handleOpenRegister, refeerred }) {
     }
     return "";
   };
+  const validateRegister = () => {
+    let updateErrors = { ...errors };
+    if (name === "") {
+      updateErrors.name = "Por favor, ingresa un nombre";
+    }
+    if (name !== "") {
+      updateErrors.name = "";
+    }
+    if (username === "") {
+      updateErrors.username = "Por favor, ingresa un usuario";
+    }
+    if (username !== "") {
+      updateErrors.username = "";
+    }
+    if (password === "") {
+      updateErrors.password = "Por favor, ingresa una contraseña";
+    } else {
+      updateErrors.password = validatePassword(password);
+    }
+    if (email === "") {
+      updateErrors.email = "Por favor, ingresa un correo electrónico";
+    } else {
+      updateErrors.email = validateEmail(email);
+    }
+    if (
+      updateErrors.username === "" &&
+      updateErrors.name === "" &&
+      updateErrors.password === "" &&
+      updateErrors.email === ""
+    ) {
+      setErrors({ ...updateErrors, validate: true });
+    } else {
+      setErrors(updateErrors);
+    }
+  };
+  const newClient = async () => {
+    await axios.post("/clientes/new", body);
+    console.log("Formulario enviado");
+    setUsername("");
+    setEmail("");
+    setName("");
+    setPassword("");
+    handleOpenRegister();
+    console.log("entro");
+  };
+  const serRef = async () => {
+    try {
+      await axios.put("/clientes/referred", body);
+      console.log("seteo referido");
+      console.log(body.email);
+      console.log(body.referred);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  let body = {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    validateRegister();
+    console.log(errors);
+    if (errors.validate) {
+      console.log("sadasdas");
+      body = {
+        username,
+        name,
+        password,
+        email,
+        photo: "",
+        rol: "cliente",
+        referred: referred ?? "",
+      };
+      newClient();
+      serRef();
+    } else {
+      console.log("murio");
+    }
+  };
+
   return <div>RegisterDesktop</div>;
 }
