e01e0532358b99b0ed2e76b2075eb0d3e0b373d8
Nikitoo17 <nicolasfadel17@gmail.com>
2023-06-30 16:44:25 -0300

    Update RegisterDesktop.jsx

--- client/src/componentsClientes/Login/RegisterDesktop.jsx
+++ client/src/componentsClientes/Login/RegisterDesktop.jsx
@@ -5,5 +5,20 @@ import { useDispatch, useSelector } from "react-redux";
 import { getAllClientes } from "../../redux/actions";
 
 export default function RegisterDesktop({ handleOpenRegister, refeerred }) {
+  const [username, setUsername] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [referred, setReferred] = useState("");
+  const [showView, setShowView] = useState(false);
+  const { allClientes } = useSelector((state) => state);
+  const [errors, setErrors] = useState({
+    username: "",
+    name: "",
+    password: "",
+    email: "",
+    validate: false,
+  });
+  const dispatch = useDispatch();
   return <div>RegisterDesktop</div>;
 }
