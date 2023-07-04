214bbed7398dd04ebc9a9169bf8e2c9c0953b37d
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-07-04 18:01:01 -0300

    Update Modal.jsx

--- client/src/components/Vendedores/Dashboard/Modal/Modal.jsx
+++ client/src/components/Vendedores/Dashboard/Modal/Modal.jsx
@@ -1205,7 +1205,6 @@ export default function NestedModal({
                       }
                       className="bbg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                       placeholder=""
-                      // value="USD"
                       required
                     />
                   </div>
