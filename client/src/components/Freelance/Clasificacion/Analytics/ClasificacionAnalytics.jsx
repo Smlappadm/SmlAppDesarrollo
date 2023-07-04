b05831e28381d81931469450499b0101ae022565
Jonavoe <voeffray.jonathan@gmail.com>
2023-07-04 20:31:28 -0300

    Update ClasificacionAnalytics.jsx

--- client/src/components/Freelance/Clasificacion/Analytics/ClasificacionAnalytics.jsx
+++ client/src/components/Freelance/Clasificacion/Analytics/ClasificacionAnalytics.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
-import Nav from "../../Nav/Nav";
 import { IoGrid, IoStatsChart } from "react-icons/io5";
 import { FaHistory } from "react-icons/fa";
+import Nav from "../../../Nav/Nav";
 
 const ClasificacionAnalytics = () => {
   return (
@@ -10,9 +10,7 @@ const ClasificacionAnalytics = () => {
       <div className=" flex flex-col justify-start items-center w-full h-screen mx-5 ">
         <div className="w-full m-5 h-screen bg-[#222131]">
           <div className="flex gap-10 items-center mt-2 mx-5 ">
-            <h1 className="font-bold text-[#e2e2e2] text-lg">
-              Analytics
-            </h1>
+            <h1 className="font-bold text-[#e2e2e2] text-lg">Analytics</h1>
             <div className="flex gap-5">
               <Link to={"/corredores"}>
                 <IoGrid className="text-[2rem] text-[#418df0] hover:text-[#3570bd]" />
