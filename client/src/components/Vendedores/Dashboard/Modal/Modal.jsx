53033f508f6b13509b67b364d782b9f98ea6a4c0
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-07-04 18:01:49 -0300

    Update Modal.jsx

--- client/src/components/Vendedores/Dashboard/Modal/Modal.jsx
+++ client/src/components/Vendedores/Dashboard/Modal/Modal.jsx
@@ -1278,7 +1278,6 @@ export default function NestedModal({
                     type="text"
                     id="last_name"
                     name="cuotas"
-                    // defaultValue={item.status_op}
                     className="text-center bbg-gray-50 border border-gray-300 text-gray-900 text-14 rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-16 p-2.5 px-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     // placeholder={item.email}
                     placeholder="Cuotas"
