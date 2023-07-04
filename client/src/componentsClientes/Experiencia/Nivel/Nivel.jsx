005910c7593223b4ee2494bb0d31b65a9a17f0be
Nikitoo17 <nicolasfadel17@gmail.com>
2023-07-04 16:49:42 -0300

    Update Nivel.jsx

--- client/src/componentsClientes/Experiencia/Nivel/Nivel.jsx
+++ client/src/componentsClientes/Experiencia/Nivel/Nivel.jsx
@@ -10,12 +10,12 @@ export default function Nivel({ nivel }) {
         {/* Etiqueta para el texto "Nivel" */}
         <p className="text-18 text-white font-medium">Nivel</p>
         {/* Etiqueta para mostrar el nivel */}
-        <label className="text-[65px] absolute top-3">
+        <p className="text-[65px] absolute top-3">
           {/* Span con estilo de gradiente para el valor del nivel */}
           <span className="bg-gradient-to-t from-blue-500 to-purple-500 text-transparent bg-clip-text font-extrabold">
             {nivel}
           </span>
-        </label>
+        </p>
       </div>
       {/* Contenedor para la imagen del trofeo */}
       <div className="rounded-full flex flex-col justify-center items-center w-24 h-24">
