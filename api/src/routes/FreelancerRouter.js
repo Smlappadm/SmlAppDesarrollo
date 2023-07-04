c1607abe2a9a7cebdc723224fc4a6aca86f7b122
Nikitoo17 <nicolasfadel17@gmail.com>
2023-07-04 20:33:46 -0300

    Update FreelancerRouter.js

--- api/src/routes/FreelancerRouter.js
+++ api/src/routes/FreelancerRouter.js
@@ -1,3 +1,8 @@
 const { Router } = require("express");
+const { postFreelancerHandler } = require("../Handlers/FreelancerHandler");
+
 const FreelancerRouter = Router();
+
+FreelancerRouter.post("/new", postFreelancerHandler);
+
 module.exports = FreelancerRouter;
