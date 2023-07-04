4296c98dfcd2c0c89e89e136184e761af600aca9
Jonavoe <voeffray.jonathan@gmail.com>
2023-07-03 21:43:03 -0300

    Update db.js

--- api/src/db.js
+++ api/src/db.js
@@ -1,19 +1,19 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-// const { URI } = ;
-// const DB_URI = URI;
+const { URI } = process.env;
+const DB_URI = URI;
 
 mongoose
-  .connect(process.env.URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("********* CONEXIÓN CORRECTA *******");
-  })
-  .catch((err) => {
-    console.log("********* ERROR DE CONEXIÓN *******");
-    console.error(err);
-  });
+	.connect(DB_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => {
+		console.log('********* CONEXIÓN CORRECTA *******');
+	})
+	.catch((err) => {
+		console.log('********* ERROR DE CONEXIÓN *******');
+		console.error(err);
+	});
