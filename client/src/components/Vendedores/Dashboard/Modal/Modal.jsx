9098fd3a094f8a366e44626815342b5700877cf1
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-07-04 17:59:39 -0300

    Update Modal.jsx

--- client/src/components/Vendedores/Dashboard/Modal/Modal.jsx
+++ client/src/components/Vendedores/Dashboard/Modal/Modal.jsx
@@ -904,7 +904,6 @@ export default function NestedModal({
                         onChange={handleChangeEmail}
                         defaultValue={updatedEmail}
                         className=" bbg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-64 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-
                         disabled={!editEmail}
                         required
                       />
@@ -929,8 +928,6 @@ export default function NestedModal({
                         onChange={handleChangeInstagram}
                         defaultValue={updatedInstagram}
                         className=" bbg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-64 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        // placeholder={inputEmail}
-                        // value={inputEmail}
                         disabled={!editInstagram}
                         required
                       />
