99d93260ff24b607d220300c5c9b859021d215c2
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-07-03 21:55:37 -0300

    Create pagosHandler.js

---
+++ api/src/Handlers/pagosHandler.js
@@ -0,0 +1,19 @@
+// const getAllLeaders = require("../controllers/Leader/getAllLeaders");
+
+
+
+const updatePagosInfoHandler = async (req, res) => {
+    console.log("entro a pagos")
+//   const email = req.query.email;
+//   const updatedData = req.body;
+
+//   try {
+//     const leader = await updateLeaderByEmail(email, updatedData);
+//     res.status(200).json(leader);
+//   } catch (error) {
+//     res.status(404).json({ error: error.message });
+//   }
+};
+
+module.exports = updatePagosInfoHandler
+
