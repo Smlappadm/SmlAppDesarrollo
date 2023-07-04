d6eab0ed95bfc7e847d0047a68d6abc845085605
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-07-04 18:00:05 -0300

    Update Modal.jsx

--- client/src/components/Vendedores/Dashboard/Modal/Modal.jsx
+++ client/src/components/Vendedores/Dashboard/Modal/Modal.jsx
@@ -952,8 +952,6 @@ export default function NestedModal({
                         onChange={handleChangeTelephone}
                         defaultValue={updatedTelephone}
                         className=" bbg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-64 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        // placeholder={inputEmail}
-                        // value={inputEmail}
                         disabled={!editTelephone}
                         required
                       />
