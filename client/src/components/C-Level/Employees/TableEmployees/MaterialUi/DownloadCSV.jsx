602760c616a33237a877bf58d6770db6d53a5a29
Nikitoo17 <nicolasfadel17@gmail.com>
2023-07-05 15:17:06 -0300

    Create DownloadCSV.jsx

---
+++ client/src/components/C-Level/Employees/TableEmployees/MaterialUi/DownloadCSV.jsx
@@ -0,0 +1,58 @@
+import { Box, Button, Modal } from "@mui/material";
+import { useState } from "react";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  borderRadius: "30px",
+  boxShadow: 24,
+  textAlign: "center",
+  color: "white",
+  pt: 2,
+  px: 4,
+  pb: 3,
+};
+
+export default function DownloadCSV() {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Button variant="contained" onClick={handleOpen}>
+        Descargar CSV
+      </Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="parent-modal-title"
+        aria-describedby="parent-modal-description"
+      >
+        <Box
+          sx={{
+            ...style,
+            width: "30%",
+            height: "50%",
+            bgcolor: "#39394b",
+          }}
+        >
+          <div>
+            <div className="flex flex-col gap-5 my-5">
+              <h2 id="parent-modal-title">Nuevo empleado</h2>
+              <p id="parent-modal-description">Regristar nuevo empleado</p>
+            </div>
+            <div className="flex flex-col items-center justify-center gap-5"></div>
+          </div>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
