e69893cd08be79b1949f9067186eccb2a5648bee
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-07-05 19:00:31 -0300

    Update getLeadChecked.js

--- api/src/controllers/Freelancer/getLeadChecked.js
+++ api/src/controllers/Freelancer/getLeadChecked.js
@@ -1,29 +1,54 @@
 const Lead = require("../../models/Lead");
 
 const getLeadChecked = async (body) => {
+  // console.log("entranding")
+  //   await Lead.updateMany(
+  //     { vendedor: body.email, status: "Sin contactar" },
+  //     {
+  //       $set: {
+  //         status_op: "",
+  //         llamados: 0,
+  //         vendedor: "",
+  //         vendedor_name: "",
+  //         checked: true,
+  //         view: true,
+  //         deleted: false,
+  //       },
+  //     }
+  //   );
 
-  await Lead.updateMany(
-    { vendedor: body.email, status: "Sin contactar" },
-    {
-      $set: {
-        status_op: "",
-        llamados: 0,
-        vendedor: "",
-        vendedor_name: "",
-        checked: true,
-        view: true,
-        deleted: false,
-      },
-    }
-  );
-  
   //BUSCA LOS QUE TENGA MI MAIL
-  let leadQuery = {
+  // let leadQuery = {
+  //   checked: true,
+  //   status: "Sin contactar",
+  //   level: { $nin: ["incidencia", "0", "", "-"] },
+  // };
+
+  // if (body.email) {
+  //   leadQuery["email"] = body.email;
+  // }
+  // if (body.country) {
+  //   leadQuery["country"] = body.country;
+  // }
+  // if (body.profesion) {
+  //   leadQuery["profesion"] = body.profesion;
+  // }
+  // if (body.level && (body.level === "1" || body.level === "2")) {
+  //   leadQuery["level"] = body.level;
+  // }
+  // if (body.level && (body.level === "aleatorio")) {
+  //   leadQuery["level"] = { $nin: ["incidencia", "0", "", "-"] };
+  // }
+
+  // const leadChequedInactive = await Lead.find(leadQuery).limit(5).exec();
+
+  //--------------------------------------------------
+
+  leadQuery = {
     checked: true,
     status: "Sin contactar",
-    level: { $nin: ["incidencia", "0", "", "-"] },
+    level: { $nin: ["incidencia", "0", "", "-", "1"] },
   };
-  
   if (body.email) {
     leadQuery["email"] = body.email;
   }
@@ -36,64 +61,7 @@ const getLeadChecked = async (body) => {
   if (body.level && (body.level === "1" || body.level === "2")) {
     leadQuery["level"] = body.level;
   }
-  if (body.level && (body.level === "aleatorio")) {
-    leadQuery["level"] = { $nin: ["incidencia", "0", "", "-"] };
-  }
-  
-  const leadChequedInactive = await Lead.find(leadQuery).limit(5).exec();
-  
-  //BUSCA LOS NO RESPONDE --------------------------
-  const leadChequedInactiveNoResponde = await Lead.find({
-    checked: true,
-    vendedor: body.email,
-    status: "No responde",
-    level: { $nin: ["incidencia", "0", "", "-"] },
-  });
-
-  const leadsNoRespondenSorted = leadChequedInactiveNoResponde.sort((a, b) => {
-    const dateA = a.updatedAt.toISOString();
-    const dateB = b.updatedAt.toISOString();
-
-    if (dateA.slice(0, 4) !== dateB.slice(0, 4)) {
-      return dateA.slice(0, 4) - dateB.slice(0, 4);
-    }
-
-    if (dateA.slice(5, 7) !== dateB.slice(5, 7)) {
-      return dateA.slice(5, 7) - dateB.slice(5, 7);
-    }
-    
-    if (dateA.slice(8, 10) !== dateB.slice(8, 10)) {
-      return dateA.slice(8, 10) - dateB.slice(8, 10);
-    }
-
-    if (dateA.slice(11, 13) !== dateB.slice(11, 13)) {
-      return dateA.slice(11, 13) - dateB.slice(11, 13);
-    }
-    
-    if (dateA.slice(14, 16) !== dateB.slice(14, 16)) {
-      return dateA.slice(14, 16) - dateB.slice(14, 16);
-    }
-
-    return 0;
-  });
-  //--------------------------------------------------
-  
-  leadQuery = {
-    vendedor: "",
-    checked: true,
-    status: "Sin contactar",
-    level: { $nin: ["incidencia", "0", "", "-", "1"] },
-  };
-  if (body.country) {
-    leadQuery["country"] = body.country;
-  }
-  if (body.profesion) {
-    leadQuery["profesion"] = body.profesion;
-  }
-  if (body.level && (body.level === "1" || body.level === "2")) {
-    leadQuery["level"] = body.level;
-  }
-  if (body.level && (body.level === "aleatorio")) {
+  if (body.level && body.level === "aleatorio") {
     leadQuery["level"] = { $nin: ["incidencia", "0", "", "-"] };
   }
 
@@ -102,14 +70,14 @@ const getLeadChecked = async (body) => {
   let leadRest = [];
   let leadRestNivel2 = [];
   let leadRestNivel1 = [];
-  
+
   if (count) {
     if (count > 0 && count <= 5) {
       if (body.level === "aleatorio") {
         leadRestNivel2 = await Lead.find(leadQuery).limit(count).exec();
       } else {
         leadRestNivel2 = await Lead.find(leadQuery).limit(count).exec();
-        
+
         let count2 = 0;
         count2 = count - leadRestNivel2.length;
         leadQuery = {
@@ -134,13 +102,8 @@ const getLeadChecked = async (body) => {
         }
       }
 
-      
       leadRest = [...leadRestNivel2, ...leadRestNivel1];
-      
 
-      console.log(leadRest.length)
-
-      
       if (leadRest.length > 0) {
         await Promise.all(
           leadRest.map(async (element) => {
@@ -148,13 +111,48 @@ const getLeadChecked = async (body) => {
             element.vendedor_name = body.name;
             await element.save();
           })
-          );
-        }
+        );
       }
+    }
   }
 
-  return [...leadChequedInactive, ...leadRest, ...leadsNoRespondenSorted];
+  //BUSCA LOS NO RESPONDE --------------------------
+  const leadChequedInactiveNoResponde = await Lead.find({
+    checked: true,
+    vendedor: body.email,
+    status: "No responde",
+    level: { $nin: ["incidencia", "0", "", "-"] },
+  });
 
+  const leadsNoRespondenSorted = leadChequedInactiveNoResponde.sort((a, b) => {
+    const dateA = a.updatedAt.toISOString();
+    const dateB = b.updatedAt.toISOString();
+
+    if (dateA.slice(0, 4) !== dateB.slice(0, 4)) {
+      return dateA.slice(0, 4) - dateB.slice(0, 4);
+    }
+
+    if (dateA.slice(5, 7) !== dateB.slice(5, 7)) {
+      return dateA.slice(5, 7) - dateB.slice(5, 7);
+    }
+
+    if (dateA.slice(8, 10) !== dateB.slice(8, 10)) {
+      return dateA.slice(8, 10) - dateB.slice(8, 10);
+    }
+
+    if (dateA.slice(11, 13) !== dateB.slice(11, 13)) {
+      return dateA.slice(11, 13) - dateB.slice(11, 13);
+    }
+
+    if (dateA.slice(14, 16) !== dateB.slice(14, 16)) {
+      return dateA.slice(14, 16) - dateB.slice(14, 16);
+    }
+
+    return 0;
+  });
+
+  return [...leadRest, ...leadsNoRespondenSorted];
+  // return [...leadChequedInactive, ...leadRest, ...leadsNoRespondenSorted];
 };
 
 module.exports = getLeadChecked;
