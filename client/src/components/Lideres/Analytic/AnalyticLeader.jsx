7d9ac6f1a8dc4dd6d48c47142574fae04a907833
Nikitoo17 <nicolasfadel17@gmail.com>
2023-07-05 18:59:25 -0300

    Update AnalyticLeader.jsx

--- client/src/components/Lideres/Analytic/AnalyticLeader.jsx
+++ client/src/components/Lideres/Analytic/AnalyticLeader.jsx
@@ -145,8 +145,8 @@ export const AnalyticLeader = () => {
     <>
       <Nav />
       <Card className="w-full h-full bg-[#222131] rounded-none p-5 relative">
-        <div className="flex justify-between items-center mx-5 mb-0">
-          <div className="flex gap-5">
+        <div className="flex justify-between items-center mx-5 mb-10 ">
+          <div className="flex gap-5 h-[37px]">
             <Title className={style.title}>Analytics</Title>
             <Link to={"/lideres/"}>
               <IoGrid className="text-[2rem] text-[#418df0] hover:text-[#3570bd]" />
