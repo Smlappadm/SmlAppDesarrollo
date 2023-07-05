2fa291ff898051562be371aa5e8620f6cf5f23e8
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-07-05 14:41:22 -0300

    Update postFreelancer.js

--- api/src/controllers/Freelancer/postFreelancer.js
+++ api/src/controllers/Freelancer/postFreelancer.js
@@ -11,40 +11,17 @@ const postFreelancer = async ({
   rol,
   deleted,
 }) => {
-
-  let leadQuery = {};
-
-  if (name) {
-    leadQuery["name"] = body.email;
-  }
-  if (email) {
-    leadQuery["email"] = body.email;
-  }
-  if (birthdate) {
-    leadQuery["birthdate"] = body.email;
-  }
-  if (photo) {
-    leadQuery["photo"] = body.email;
-  }
-  if (country) {
-    leadQuery["country"] = body.email;
-  }
-  if (contactNumber) {
-    leadQuery["contactNumber"] = body.email;
-  }
-  if (rol) {
-    leadQuery["rol"] = body.email;
-  }
-  if (description) {
-    leadQuery["description"] = body.email;
-  }
-  if (deleted) {
-    leadQuery["deleted"] = body.email;
-  }
-
-
-
-  const freelancer = await Freelancer.create({leadQuery});
+  const freelancer = await Freelancer.create({
+    name,
+    email,
+    birthdate,
+    photo,
+    country,
+    contactNumber,
+    description,
+    rol,
+    deleted,
+  });
   return freelancer;
 };
 
