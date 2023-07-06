3b92c7e48a18e98af91f39784908038d9c885e18
Jonavoe <voeffray.jonathan@gmail.com>
2023-07-06 19:32:54 -0300

    Update asignacionFreelancer.js

--- api/src/controllers/Lead/asignacionFreelancer.js
+++ api/src/controllers/Lead/asignacionFreelancer.js
@@ -12,10 +12,8 @@ const asignacionFreelancer = async (data) => {
   const totalProfesiones = profesiones.length;
 
   const asignacionesPromises = [];
-  let counter = 0;
-
-  while (asignacionesPromises.length < parsedLeads) {
-    const profesion = profesiones[counter % totalProfesiones];
+  for (let i = 0; i < parsedLeads; i++) {
+    const profesion = profesiones[i % totalProfesiones];
 
     const lead = await Lead.findOneAndUpdate(
       {
@@ -38,64 +36,12 @@ const asignacionFreelancer = async (data) => {
       { new: true }
     );
 
-    if (lead !== null) {
-      asignacionesPromises.push(lead);
-    }
-
-    counter++;
+    asignacionesPromises.push(lead);
   }
 
   const asignaciones = await Promise.all(asignacionesPromises);
 
-  return asignaciones;
+  return asignaciones.filter((lead) => lead !== null);
 };
 
 module.exports = asignacionFreelancer;
-
-// const Lead = require("../../models/Lead");
-
-// const asignacionFreelancer = async (data) => {
-//   const { name, email, leads } = data;
-//   const parsedLeads = parseInt(leads, 10);
-
-//   if (isNaN(parsedLeads) || !Number.isInteger(parsedLeads)) {
-//     throw new Error("El valor de leads debe ser un número entero");
-//   }
-
-//   const profesiones = await Lead.distinct("profesion");
-//   const totalProfesiones = profesiones.length;
-
-//   const asignacionesPromises = [];
-//   for (let i = 0; i < parsedLeads; i++) {
-//     const profesion = profesiones[i % totalProfesiones];
-
-//     const lead = await Lead.findOneAndUpdate(
-//       {
-//         profesion,
-//         checked: false,
-//         freelancer: false,
-//         corredor: "",
-//         status: "Sin contactar",
-//       },
-//       {
-//         $set: {
-//           vendedor: email,
-//           vendedor_name: name,
-//           corredor: email,
-//           corredor_name: name,
-//           freelancer: true,
-//           checked: false,
-//         },
-//       },
-//       { new: true }
-//     );
-
-//     asignacionesPromises.push(lead);
-//   }
-
-//   const asignaciones = await Promise.all(asignacionesPromises);
-
-//   return asignaciones.filter((lead) => lead !== null);
-// };
-
-// module.exports = asignacionFreelancer;
