d3ae65f3212e0ac51ca7e114c3a786ce9f521ed1
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-07-06 18:37:19 -0300

    Update postInfoPago.js

--- api/src/controllers/Pagos/postInfoPago.js
+++ api/src/controllers/Pagos/postInfoPago.js
@@ -1,8 +1,20 @@
 const Pagos = require("../../models/Pagos");
+const Lead = require("../../models/Lead");
 
-const postInfoPago = async (objeto) => {
-  const infoSave = await Pagos.create(objeto);
+const postInfoPago = async (info) => {
+  const infoSave = await Pagos.create({info: info});
+  
+  // if(infoSave)
+  // const leadUpdate = await Lead.findOneAndUpdate(
+  //   { emailApp: objeto.email },
+  //   { },
+  //   { new: true }
+  // );
+  
+  
+  
   return infoSave;
+
 };
 
 module.exports = postInfoPago;
