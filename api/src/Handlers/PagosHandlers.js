0d68ddd986d61f2c146e768485f794657dca0dbb
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-07-06 18:09:31 -0300

    Update PagosHandlers.js

--- api/src/Handlers/PagosHandlers.js
+++ api/src/Handlers/PagosHandlers.js
@@ -2,10 +2,14 @@ const postInfoPago = require("../controllers/Pagos/postInfoPago");
 
 const updatePagosInfoHandler = async (req, res) => {
   const info = req.body
-  console.log(info.data.object.customer_details.email)
+  const objeto = {
+    email: info.data.object.customer_details.email,
+    monto: info.data.object.amount_total,
+    status: info.data.object.status,
+  }
 
   try {
-    const infoSave = await postInfoPago(info);
+    const infoSave = await postInfoPago(objeto);
     res.status(200).json(infoSave);
   } catch (error) {
     res.status(404).json({ error: error.message });
