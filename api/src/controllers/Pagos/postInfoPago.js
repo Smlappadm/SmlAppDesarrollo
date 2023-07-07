6e419725602b3947c84f8136ab990f88bf64342f
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-07-07 18:24:04 -0300

    Update postInfoPago.js

--- api/src/controllers/Pagos/postInfoPago.js
+++ api/src/controllers/Pagos/postInfoPago.js
@@ -9,8 +9,26 @@ const postInfoPago = async (objeto) => {
   let leadUpdate = ""
   if(objeto.status === "complete"){
   // if(true){
-  // const leadUpdate = await Lead.find({ emailApp: objeto.email });
-  leadUpdate = await Lead.find({ emailApp: "facutam@gmail.com" });
+  const leadUpdate = await Lead.find({ emailApp: objeto.email });
+  // leadUpdate = await Lead.updateOne({ emailApp: "facutam@gmail.com" });
+  // leadUpdate = await Lead.updateOne(
+  //   { emailApp: objeto.email},
+  //   {
+  //     $set: {
+  //       status: "Sin contactar",
+  //       status_op: "",
+  //       llamados: 0,
+  //       vendedor: "",
+  //       vendedor_name: "",
+  //       corredor: "",
+  //       corredor_name: "",
+  //       checked: false,
+  //       freelancer: false,
+  //       view: false,
+  //       deleted: false,
+  //     },
+  //   }
+  // );
   console.log(leadUpdate)
   // leadUpdate.pagos.cuotasPagadas ++;
   // await leadUpdate.save();
