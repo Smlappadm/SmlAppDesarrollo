3d91352a1245e37ed3e9b713ecf00b2592e7b627
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-07-07 18:54:51 -0300

    Update postInfoPago.js

--- api/src/controllers/Pagos/postInfoPago.js
+++ api/src/controllers/Pagos/postInfoPago.js
@@ -4,21 +4,24 @@ const Lead = require("../../models/Lead");
 const postInfoPago = async (objeto) => {
   const infoSave = await Pagos.create({ info: objeto });
 
-  let leadUpdate = "";
+  let leadUpdate = {};
   // if (objeto.status === "complete") {
-    // const leadUpdate = await Lead.findOne({ emailApp: objeto.email });
-     leadUpdate = await Lead.findOne({ emailApp: "facutam@gmail.com" });
-
+  // const leadUpdate = await Lead.findOne({ emailApp: objeto.email });
+  try {
+    leadUpdate = await Lead.findOne({ emailApp: "facutam@gmail.com" });
+  
     if (leadUpdate) {
       leadUpdate.pagos.detallesRestantes.shift();
       leadUpdate.pagos.cuotasPagadas++;
       await leadUpdate.save();
-
-      console.log(leadUpdate);
+  
+      console.log("Cambios guardados correctamente:", leadUpdate);
     } else {
       console.log("No se encontró ningún Lead con ese correo electrónico.");
     }
-  // }
+  } catch (error) {
+    console.log("Error al guardar los cambios:", error);
+  }
 
   return { info: infoSave, lead: leadUpdate };
 };
