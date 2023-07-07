cec9f9368c1afffbd55f66bb6f68b75356e03724
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-07-07 18:37:28 -0300

    Update postInfoPago.js

--- api/src/controllers/Pagos/postInfoPago.js
+++ api/src/controllers/Pagos/postInfoPago.js
@@ -2,43 +2,24 @@ const Pagos = require("../../models/Pagos");
 const Lead = require("../../models/Lead");
 
 const postInfoPago = async (objeto) => {
+  const infoSave = await Pagos.create({ info: objeto });
 
+  let leadUpdate = "";
+  if (objeto.status === "complete") {
+    const leadUpdate = await Lead.findOne({ emailApp: objeto.email });
 
-  const infoSave = await Pagos.create({info: objeto});
-  
-  let leadUpdate = ""
-  if(objeto.status === "complete"){
-  // if(true){
-  const leadUpdate = await Lead.find({ emailApp: objeto.email });
-  // leadUpdate = await Lead.updateOne({ emailApp: "facutam@gmail.com" });
-  // leadUpdate = await Lead.updateOne(
-  //   { emailApp: objeto.email},
-  //   {
-  //     $set: {
-  //       status: "Sin contactar",
-  //       status_op: "",
-  //       llamados: 0,
-  //       vendedor: "",
-  //       vendedor_name: "",
-  //       corredor: "",
-  //       corredor_name: "",
-  //       checked: false,
-  //       freelancer: false,
-  //       view: false,
-  //       deleted: false,
-  //     },
-  //   }
-  // );
-  console.log(leadUpdate)
-  // leadUpdate.pagos.cuotasPagadas ++;
-  // await leadUpdate.save();
+    if (leadUpdate) {
+      leadUpdate.pagos.detallesRestantes.shift();
+      leadUpdate.pagos.cuotasPagadas++;
+      await leadUpdate.save();
+
+      console.log(leadUpdate);
+    } else {
+      console.log("No se encontró ningún Lead con ese correo electrónico.");
+    }
   }
-  
-  
-  
 
-  return {info: infoSave, lead: leadUpdate};
-
+  return { info: infoSave, lead: leadUpdate };
 };
 
 module.exports = postInfoPago;
