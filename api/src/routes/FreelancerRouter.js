56d03b5e78e9fc5d93f6ebb6eab9b6744ef1970c
Nikitoo17 <nicolasfadel17@gmail.com>
2023-07-10 16:15:29 -0300

    Update FreelancerRouter.js

--- api/src/routes/FreelancerRouter.js
+++ api/src/routes/FreelancerRouter.js
@@ -3,14 +3,14 @@ const {
   postFreelancerHandler,
   getAllFreelancerHandler,
   getLeadCheckedFreelanceHandler,
-  getFreelancerByEmailHandler,
+  getAllFreelancersHandler,
 } = require("../Handlers/FreelancerHandler");
 
 const FreelancerRouter = Router();
 
 FreelancerRouter.post("/", postFreelancerHandler);
 FreelancerRouter.get("/", getAllFreelancerHandler);
-FreelancerRouter.get("/one", getFreelancerByEmailHandler);
+FreelancerRouter.get("/one", getAllFreelancersHandler);
 FreelancerRouter.put("/checkedfreelance", getLeadCheckedFreelanceHandler);
 
 module.exports = FreelancerRouter;
