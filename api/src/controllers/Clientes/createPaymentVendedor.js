42deb8376a7674cc20df0c37a3e4b981cf2dc845
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-07-11 16:28:33 -0300

    Update createPaymentVendedor.js

--- api/src/controllers/Clientes/createPaymentVendedor.js
+++ api/src/controllers/Clientes/createPaymentVendedor.js
@@ -2,12 +2,7 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 
 
-const createPayment = async ({ id, name, monto, cuotas, cuotasRestantes, valorCuota, link }) => {
-
-//     const infoLead = await find({emailApp:emailApp})
-// console.log(infoLead.pagos)
-
-// return infoLead.pagos
+const createPayment = async ({ id, name, monto, cuotas, cuotasRestantes, valorCuota, link, email }) => {
 
   const description = `cuotas ${cuotasRestantes + 1}/${cuotas}`
 
@@ -19,7 +14,6 @@ const createPayment = async ({ id, name, monto, cuotas, cuotasRestantes, valorCu
             images: ["https://images-ext-1.discordapp.net/external/VmotedpeNAAv9Sz0GZI5iLiobf_7NpJn24pyas4ed_Y/https/i.postimg.cc/4y1YcByV/1685492595204-removebg-preview.webp"],
             name: name,
             description: description
-            // description: `cuotas ${cuotasRestantes}/${cuotas}`
           },
           currency: "eur",
           unit_amount: (valorCuota * 100),
@@ -38,32 +32,12 @@ const createPayment = async ({ id, name, monto, cuotas, cuotasRestantes, valorCu
       // },
     ],
     mode: "payment",
-    payment_method_types: ["card"],
-    // payment_intent_data: {
-    //   payment_method_options: {
-    //     card: {
-    //       installments: cuotas // Establece el número de cuotas del pago
-    //     }
-    //   },
-    //   description: description // Establece el detalle del pago
-    // },
-    // success_url: "www.google.com.ar",
     success_url: link,
+    customer_email: email,
     // cancel_url: "http://localhost:3002/cancel",
-    // success_url: "http://localhost:3001/success",
-    // cancel_url: "http://localhost:3002/cancel",
+
     locale: "es",
   });
-
-
-    // await stripe.webhookEndpoints.create({
-    //   url: "https://tu-domino.com/stripe/payment_completed",
-    //   enabled_events: ["checkout.session.completed"],
-    // });
-  
-
-//   console.log(session)
-
 
   return session;
 };
