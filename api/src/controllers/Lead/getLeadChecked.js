46d0e13895c45066fa78fc5cf416c7f696f6cc1a
Jonavoe <voeffray.jonathan@gmail.com>
2023-07-12 15:29:15 -0300

    Update getLeadChecked.js

--- api/src/controllers/Lead/getLeadChecked.js
+++ api/src/controllers/Lead/getLeadChecked.js
@@ -3,8 +3,15 @@ const Lead = require("../../models/Lead");
 const getLeadChecked = async () => {
   const leadChequed = await Lead.find({
     checked: true,
+    freelancer: {$ne: true},
     status: {
-      $nin: ["No responde", "Agendar 2do llamado", "incidencia", "discard", "discard_bot"],
+      $nin: [
+        "No responde",
+        "Agendar 2do llamado",
+        "incidencia",
+        "discard",
+        "discard_bot",
+      ],
     },
   });
   return leadChequed;
