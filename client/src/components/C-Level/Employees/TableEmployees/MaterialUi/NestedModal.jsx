fba3e20fc370f7a8bcef8e0c4f3b0ee4f79f77a3
Jonavoe <voeffray.jonathan@gmail.com>
2023-07-12 19:15:46 -0300

    Update NestedModal.jsx

--- client/src/components/C-Level/Employees/TableEmployees/MaterialUi/NestedModal.jsx
+++ client/src/components/C-Level/Employees/TableEmployees/MaterialUi/NestedModal.jsx
@@ -133,14 +133,39 @@ function ChildModal({
         setLoading(true);
 
 
-
-        const response = await axios.get(
-          `https://apisml.onrender.com/freelance-desarrollo?freelance=${inputName}&email=${inputEmail}&num_leads=${leadAsigned}&profesion=${profesion}&country=${country}`
-        );
+        if (axios.defaults.baseURL === "https://sml-app.com/api") {
+      const response = await axios.get(
+        `https://apisml.onrender.com/freelance?freelance=${inputName}&email=${inputEmail}&num_leads=${leadAsigned}&profesion=${profesion}&country=${country}`
+      );
+      if (response.data.message === "Carga completa de leads") {
+        okLeads(response.data.message);
+      } else if (
+        response.data.message === "No hay leads con la profesion solicitada"
+      ) {
+        errorLeads(response.data.message);
+      } else {
+        almostLeads(response.data.message);
+      }
+    } else if (
+      axios.defaults.baseURL === "http://localhost:3001/api" ||
+      axios.defaults.baseURL === "https://smlapp.onrender.com/api"
+    ) {
+      const response = await axios.get(
+        `https://apisml.onrender.com/freelance_desarrollo?freelance=${inputName}&email=${inputEmail}&num_leads=${leadAsigned}&profesion=${profesion}&country=${country}`
+      );
+      if (response.data.message === "Carga completa de leads") {
+        okLeads(response.data.message);
+      } else if (
+        response.data.message === "No hay leads con la profesion solicitada"
+      ) {
+        errorLeads(response.data.message);
+      } else {
+        almostLeads(response.data.message);
+      }
+    }
 
         setLoading(false);
 
-        console.log(response.data);
 
         // const response = await axios.put("/lead/asignacion", {
         //   name: inputName,
