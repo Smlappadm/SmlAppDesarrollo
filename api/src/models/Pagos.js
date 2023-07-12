9734c4e1ff67891627436f695b963034d8e22128
Jonavoe <voeffray.jonathan@gmail.com>
2023-07-12 15:27:54 -0300

    Create Pagos.js

---
+++ api/src/models/Pagos.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+
+const PagosSchema = new mongoose.Schema(
+  {
+    info: {
+      type: Object,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+
+const Pagos = new mongoose.model("pagos", PagosSchema);
+
+module.exports = Pagos;
