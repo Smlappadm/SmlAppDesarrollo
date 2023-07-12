e96641d428a90b408802604fdae2b93083d493cf
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-07-12 14:57:36 -0300

    Update Modal.jsx

--- client/src/components/Vendedores/Dashboard/Modal/Modal.jsx
+++ client/src/components/Vendedores/Dashboard/Modal/Modal.jsx
@@ -1366,7 +1366,7 @@ export default function NestedModal({
                           ? llamadoVenta.contacto
                           : item.llamada_venta.contacto
                       }
-                      className="bg-gray-50  text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      className="bg-gray-50 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                       placeholder=""
                       required
                     />
