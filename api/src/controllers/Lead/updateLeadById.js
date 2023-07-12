bef9f60abaa12d2a48089981769e52cb33f53b2a
Jonavoe <voeffray.jonathan@gmail.com>
2023-07-12 15:27:34 -0300

    Update updateLeadById.js

--- api/src/controllers/Lead/updateLeadById.js
+++ api/src/controllers/Lead/updateLeadById.js
@@ -1,10 +1,16 @@
 const Lead = require("../../models/Lead");
 
 const updateLeadById = async (id, updatedData) => {
+  const date = new Date();
+  const formattedTime = date.toISOString();
+
+  updatedData.updateCorredor = formattedTime;
+
   try {
     const lead = await Lead.findByIdAndUpdate(id, updatedData, {
       new: true,
     });
+
     return lead;
   } catch (error) {
     throw new Error(`Error updating lead with id ${id}: ${error.message}`);
