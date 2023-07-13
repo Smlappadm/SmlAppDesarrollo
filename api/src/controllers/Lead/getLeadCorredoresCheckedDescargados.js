6c8b30a1edf65ca3d0efb3addaa40d78bd7e3221
Nikitoo17 <nicolasfadel17@gmail.com>
2023-07-13 18:58:47 -0300

    Update getLeadCorredoresCheckedDescargados.js

--- api/src/controllers/Lead/getLeadCorredoresCheckedDescargados.js
+++ api/src/controllers/Lead/getLeadCorredoresCheckedDescargados.js
@@ -1,6 +1,7 @@
 const Lead = require("../../models/Lead");
 
 const getLeadCorredoresCheckedDescargados = async (email) => {
+  console.log(email);
   const leads = await Lead.find({
     corredor: email,
     checked: true,
