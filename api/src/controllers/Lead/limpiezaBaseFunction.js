5ab917d9c730380ec89fdcbf74ec09f840bbc916
Jonavoe <voeffray.jonathan@gmail.com>
2023-07-13 15:21:15 -0300

    Update limpiezaBaseFunction.js

--- api/src/controllers/Lead/limpiezaBaseFunction.js
+++ api/src/controllers/Lead/limpiezaBaseFunction.js
@@ -2,97 +2,96 @@
 
 // const limpiezaBaseFunction = async () => {
 //   const leadResult = await Lead.updateMany(
-
+//     { descargadosLeader: { $ne: true } },
 //     {
-//       updateVendedor: ""
+//       descargadosLeader: false,
 //     }
-
 //   );
 //   return leadResult;
 // };
 
 // module.exports = limpiezaBaseFunction;
 
-const Lead = require("../../models/Lead");
+// const Lead = require("../../models/Lead");
 
-const limpiezaBaseFunction = async () => {
-  const leadResult = await Lead.updateMany(
-    { corredor_name: "Nicky Free" },
-    {
-      $set: {
-        status: "Sin contactar",
-        status_op: "",
-        llamados: 0,
-        vendedor: "",
-        vendedor_name: "",
-        corredor: "",
-        corredor_name: "",
-        checked: false,
-        freelancer: false,
-        view: false,
-        deleted: false,
-      },
-    }
-    //     // {
-    //     //   $set: {
-    //     //     corredor_name: "Florencia Carballo",
-    //     //   },
-    //     // }
-    //     // { vendedor: "undefined" },
-    //     // {
-    //     //   $set: {
-    //     //     // level: "",
-    //     //     status: "Sin contactar",
-    //     //     status_op: "",
-    //     //     llamados: 0,
-    //     //     vendedor: "",
-    //     //     vendedor_name: "",
-    //     //     // corredor: "",
-    //     //     // corredor_name: "",
-    //     //     checked: true,
-    //     //     view: true,
-    //     //     deleted: false,
-    //     //     // instagram: ""
-    //     //   },
-    //     // }
+// const limpiezaBaseFunction = async () => {
+//   const leadResult = await Lead.updateMany(
+//     { corredor_name: "Nicky Free" },
+//     {
+//       $set: {
+//         status: "Sin contactar",
+//         status_op: "",
+//         llamados: 0,
+//         vendedor: "",
+//         vendedor_name: "",
+//         corredor: "",
+//         corredor_name: "",
+//         checked: false,
+//         freelancer: false,
+//         view: false,
+//         deleted: false,
+//       },
+//     }
+//     // {
+//     //   $set: {
+//     //     corredor_name: "Florencia Carballo",
+//     //   },
+//     // }
+//     // { vendedor: "undefined" },
+//     // {
+//     //   $set: {
+//     //     // level: "",
+//     //     status: "Sin contactar",
+//     //     status_op: "",
+//     //     llamados: 0,
+//     //     vendedor: "",
+//     //     vendedor_name: "",
+//     //     // corredor: "",
+//     //     // corredor_name: "",
+//     //     checked: true,
+//     //     view: true,
+//     //     deleted: false,
+//     //     // instagram: ""
+//     //   },
+//     // }
 
-    // { corredor: 'email' },
-    // {
-    //   $set: {
-    //     level: "",
-    //     status: "Sin contactar",
-    //     status_op: "",
-    //     llamados: 0,
-    //     vendedor: "",
-    //     vendedor_name: "",
-    //     corredor: "",
-    //     corredor_name: "",
-    //     checked: false,
-    //     view: false,
-    //     deleted: false,
-    //     instagram: ""
-    //   },
-    // }
+// { corredor: 'email' },
+// {
+//   $set: {
+//     level: "",
+//     status: "Sin contactar",
+//     status_op: "",
+//     llamados: 0,
+//     vendedor: "",
+//     vendedor_name: "",
+//     corredor: "",
+//     corredor_name: "",
+//     checked: false,
+//     view: false,
+//     deleted: false,
+//     instagram: ""
+//   },
+// }
 
-    //     //     { level: "" },
-    //     //     {
-    //     //       $set: {
-    //     //         level: "",
-    //     //         status: "Sin contactar",
-    //     //         status_op: "",
-    //     //         llamados: 0,
-    //     //         vendedor: "",
-    //     //         vendedor_name: "",
-    //     //         corredor: "",
-    //     //         corredor_name: "",
-    //     //         checked: false,
-    //     //         view: false,
-    //     //         deleted: false,
-    //     //         instagram: ""
-    //     //       },
-    //     //     }
-  );
-  return leadResult;
-};
+//     //     { level: "" },
+//     //     {
+//     //       $set: {
+//     //         level: "",
+//     //         status: "Sin contactar",
+//     //         status_op: "",
+//     //         llamados: 0,
+//     //         vendedor: "",
+//     //         vendedor_name: "",
+//     //         corredor: "",
+//     //         corredor_name: "",
+//     //         checked: false,
+//     //         view: false,
+//     //         deleted: false,
+//     //         instagram: ""
+//     //       },
+//     //     }
+//   );
+//   return leadResult;
+// };
 
-module.exports = limpiezaBaseFunction;
+// module.exports = limpiezaBaseFunction;
