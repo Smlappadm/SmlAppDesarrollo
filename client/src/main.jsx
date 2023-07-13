4bd5e0aed5dd85cc918a1c7a8327ff5088417de7
Jonavoe <voeffray.jonathan@gmail.com>
2023-07-13 15:48:25 -0300

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,14 +8,23 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import axios from "axios";
 
-// axios.defaults.baseURL = "http://localhost:3001/api";
-axios.defaults.baseURL = "https://smlapp.onrender.com/api";
-// axios.defaults.baseURL = "https://sml-app.com/api";
+const currentUrl = window.location.href;
+let baseURL;
+
+if (currentUrl.includes("https://sml-app.com")) {
+  baseURL = "https://sml-app.com/api";
+} else if (currentUrl.includes("https://smlapp.onrender.com")) {
+  baseURL = "https://smlapp.onrender.com/api";
+} else {
+  baseURL = "http://localhost:3001/api";
+}
+
+axios.defaults.baseURL = baseURL;
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
-  <IntlProvider locale="en-US" massages={{}}>
+  <IntlProvider locale="en-US" messages={{}}>
     <Provider store={store}>
       <BrowserRouter>
         <App />
@@ -24,8 +33,31 @@ root.render(
   </IntlProvider>
 );
 
+// import React from "react";
+// import App from "./App.jsx";
+// import "./index.css";
+// import { createRoot } from "react-dom/client";
+// import { BrowserRouter } from "react-router-dom";
+// import { IntlProvider } from "react-intl";
+// import { Provider } from "react-redux";
+// import store from "./redux/store";
+// import axios from "axios";
 
+// axios.defaults.baseURL = "http://localhost:3001/api";
+// // axios.defaults.baseURL = "https://smlapp.onrender.com/api";
+// // axios.defaults.baseURL = "https://sml-app.com/api";
 
+// const container = document.getElementById("root");
+// const root = createRoot(container);
+// root.render(
+//   <IntlProvider locale="en-US" massages={{}}>
+//     <Provider store={store}>
+//       <BrowserRouter>
+//         <App />
+//       </BrowserRouter>
+//     </Provider>
+//   </IntlProvider>
+// );
 
 // HOVER
 
