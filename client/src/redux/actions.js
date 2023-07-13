63a6da8cc7f99fdbc8b49dce2a2572afaa0ef30a
Jonavoe <voeffray.jonathan@gmail.com>
2023-07-13 14:49:17 -0300

    Update actions.js

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -266,7 +266,7 @@ export const findFreelancerByNameAllInfo = (
 ) => {
   return async (dispatch) => {
     const response = await axios.get(
-      `/lead/allinfofreelancer?freelancer=${freelancer}&fromDay=${fromDay}&toDay=${toDay}&profesion=${profesion}&country=${country}&category=${category}&level=${level}&status=${status}`
+      `/lead/allinfofreelancer?freelancer=${freelancer}&fromDay=${fromDay}&toDay=${toDay}&profesion=${profesion}&country=${country}&category=${category}&level=${level}&status=${status}&descargados=${descargados}`
     );
     const freelancerByNameAllInfo = response.data;
     dispatch({
