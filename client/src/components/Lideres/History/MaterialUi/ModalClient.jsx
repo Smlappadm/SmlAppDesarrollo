054862bf02a9979fcc7b96781919c3eb2822e59a
Nikitoo17 <nicolasfadel17@gmail.com>
2023-07-14 19:48:08 -0300

    Update ModalClient.jsx

--- client/src/components/Lideres/History/MaterialUi/ModalClient.jsx
+++ client/src/components/Lideres/History/MaterialUi/ModalClient.jsx
@@ -201,7 +201,7 @@ export default function BasicModal({ modalItems, open, handleClose }) {
               </div>
               <motion.div
                 initial={
-                  empresa === true
+                  corredor === true
                     ? { opacity: 0, height: 0, overflow: "hidden" }
                     : { opacity: 0.1, height: "auto", overflow: "hidden" }
                 }
