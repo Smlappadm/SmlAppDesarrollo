d8ce8f2a7de9b52d00b03a5b3b39e9f363068168
Nikitoo17 <nicolasfadel17@gmail.com>
2023-07-14 19:41:08 -0300

    Update ModalClient.jsx

--- client/src/components/Lideres/History/MaterialUi/ModalClient.jsx
+++ client/src/components/Lideres/History/MaterialUi/ModalClient.jsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
+import { motion } from "framer-motion";
 import { useState } from "react";
 
 const style = {
