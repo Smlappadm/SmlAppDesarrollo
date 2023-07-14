63a89d8981a33f90619da361efbb0033e083bfed
Nikitoo17 <nicolasfadel17@gmail.com>
2023-07-14 18:21:25 -0300

    Update ModalClient.jsx

--- client/src/components/Lideres/Dashboard/MaterialUi/ModalClient.jsx
+++ client/src/components/Lideres/Dashboard/MaterialUi/ModalClient.jsx
@@ -202,7 +202,7 @@ export default function BasicModal({ modalItems, open, handleClose }) {
                     : { opacity: 0.1, height: "auto", overflow: "hidden" }
                 }
                 animate={
-                  empresa === true
+                  freelance === true
                     ? { opacity: 1, height: "auto", overflow: "visible" }
                     : { opacity: 0, height: 0, overflow: "hidden" }
                 }
