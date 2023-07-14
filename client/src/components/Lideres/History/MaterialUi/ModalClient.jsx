a30a8388fb766dc01109c45786469d81fdd12efc
Nikitoo17 <nicolasfadel17@gmail.com>
2023-07-14 19:46:02 -0300

    Update ModalClient.jsx

--- client/src/components/Lideres/History/MaterialUi/ModalClient.jsx
+++ client/src/components/Lideres/History/MaterialUi/ModalClient.jsx
@@ -193,7 +193,11 @@ export default function BasicModal({ modalItems, open, handleClose }) {
                 className="w-full flex items-center justify-start mb-1"
                 onClick={corredorVisible}
               >
-                <h2 className="text-24 font-bold">Corredor</h2>
+                {corredor === false ? (
+                  <h2 className="text-24 font-bold">Freelancer ⮳</h2>
+                ) : (
+                  <h2 className="text-24 font-bold">Freelancer ⮷</h2>
+                )}
               </div>
               <div className="font-semibold flex gap-3">
                 <p>Corredor:</p>
