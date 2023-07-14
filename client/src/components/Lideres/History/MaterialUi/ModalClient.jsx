09b667a40998f0cb20360bdee4568e368758a9c7
Nikitoo17 <nicolasfadel17@gmail.com>
2023-07-14 19:44:48 -0300

    Update ModalClient.jsx

--- client/src/components/Lideres/History/MaterialUi/ModalClient.jsx
+++ client/src/components/Lideres/History/MaterialUi/ModalClient.jsx
@@ -75,101 +75,117 @@ export default function BasicModal({ modalItems, open, handleClose }) {
                   <h2 className="text-24 font-bold">Empresa ⮷</h2>
                 )}
               </motion.div>
-              <div className="font-semibold flex gap-3">
-                <p>Web:</p>
-                <a
-                  href={modalItems.url}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {modalItems.url}
-                </a>
-              </div>
-              <div className="font-semibold flex gap-3">
-                <p>Instagram:</p>
-                <a
-                  href={modalItems.instagram}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {modalItems.instagram}
-                </a>
-              </div>
-              <div className="font-semibold flex gap-3">
-                <p>Descripción:</p>
-                <p className="font-normal">{modalItems.description}</p>
-              </div>
-              <div className="font-semibold flex gap-3">
-                <p>Categoria:</p>
-                <p className="font-normal">{modalItems.category}</p>
-              </div>
-              <div className="font-semibold flex gap-3">
-                <p>Profesion:</p>
-                <p className="font-normal">{modalItems.profesion}</p>
-              </div>
-              <div className="font-semibold flex gap-3">
-                <p>Pais:</p>
-                <p className="font-normal">{modalItems.country}</p>
-              </div>
-              <div className="font-semibold flex gap-3">
-                <p>Provincia:</p>
-                <p className="font-normal">{modalItems.province}</p>
-              </div>
-              <div className="font-semibold flex gap-3">
-                <p>Ciudad:</p>
-                <p className="font-normal">{modalItems.city}</p>
-              </div>
-              <div className="font-semibold flex gap-3">
-                <p>Email:</p>
-                <p className="font-normal">{modalItems.email}</p>
-              </div>
-              <div className="font-semibold flex gap-3">
-                <p>Telefono:</p>
-                <p className="font-normal">{modalItems.telephone}</p>
-              </div>
-              <div className="font-semibold flex gap-3">
-                <p>Nivel:</p>
-                <p className="font-normal">{modalItems.level}</p>
-              </div>
-              <div className="font-semibold flex gap-3">
-                <p>Detalle:</p>
-                <p className="font-normal">
-                  {modalItems.status_op ? modalItems.status_op : "-"}
-                </p>
-              </div>
-              <div className="font-semibold flex gap-3">
-                <p>Marca Personal:</p>
-                <p className="font-normal">{modalItems.marca_personal}</p>
-              </div>
-              <div className="font-semibold flex gap-3">
-                <p>Creado:</p>
-                <p className="font-normal">
-                  {funcionHorario(modalItems.createdAt)}
-                </p>
-              </div>
-              <div className="font-semibold flex gap-3">
-                <p>Estado:</p>
-                <p className="font-normal">{modalItems.status}</p>
-              </div>
-              {modalItems.status === "Contratado" && (
-                <div>
-                  <div className="font-semibold flex gap-3">
-                    <p>Monto:</p>
-                    <p className="font-normal">{modalItems.pagos.monto}</p>
-                  </div>
-                  <div className="font-semibold flex gap-3">
-                    <p>Cuotas:</p>
-                    <p className="font-normal">
-                      {modalItems.pagos.cuotasPagadas} /{" "}
-                      {modalItems.pagos.cuotas}{" "}
-                    </p>
-                  </div>
-                  <div className="font-semibold flex gap-3">
-                    <p>Valor cuota:</p>
-                    <p className="font-normal">{modalItems.pagos.valorCuota}</p>
-                  </div>
+              <motion.div
+                initial={
+                  empresa === true
+                    ? { opacity: 0, height: 0, overflow: "hidden" }
+                    : { opacity: 0.1, height: "auto", overflow: "hidden" }
+                }
+                animate={
+                  empresa === true
+                    ? { opacity: 1, height: "auto", overflow: "visible" }
+                    : { opacity: 0, height: 0, overflow: "hidden" }
+                }
+                transition={{ duration: 0.5 }}
+              >
+                <div className="font-semibold flex gap-3">
+                  <p>Web:</p>
+                  <a
+                    href={modalItems.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {modalItems.url}
+                  </a>
                 </div>
-              )}
+                <div className="font-semibold flex gap-3">
+                  <p>Instagram:</p>
+                  <a
+                    href={modalItems.instagram}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {modalItems.instagram}
+                  </a>
+                </div>
+                <div className="font-semibold flex gap-3">
+                  <p>Descripción:</p>
+                  <p className="font-normal">{modalItems.description}</p>
+                </div>
+                <div className="font-semibold flex gap-3">
+                  <p>Categoria:</p>
+                  <p className="font-normal">{modalItems.category}</p>
+                </div>
+                <div className="font-semibold flex gap-3">
+                  <p>Profesion:</p>
+                  <p className="font-normal">{modalItems.profesion}</p>
+                </div>
+                <div className="font-semibold flex gap-3">
+                  <p>Pais:</p>
+                  <p className="font-normal">{modalItems.country}</p>
+                </div>
+                <div className="font-semibold flex gap-3">
+                  <p>Provincia:</p>
+                  <p className="font-normal">{modalItems.province}</p>
+                </div>
+                <div className="font-semibold flex gap-3">
+                  <p>Ciudad:</p>
+                  <p className="font-normal">{modalItems.city}</p>
+                </div>
+                <div className="font-semibold flex gap-3">
+                  <p>Email:</p>
+                  <p className="font-normal">{modalItems.email}</p>
+                </div>
+                <div className="font-semibold flex gap-3">
+                  <p>Telefono:</p>
+                  <p className="font-normal">{modalItems.telephone}</p>
+                </div>
+                <div className="font-semibold flex gap-3">
+                  <p>Nivel:</p>
+                  <p className="font-normal">{modalItems.level}</p>
+                </div>
+                <div className="font-semibold flex gap-3">
+                  <p>Detalle:</p>
+                  <p className="font-normal">
+                    {modalItems.status_op ? modalItems.status_op : "-"}
+                  </p>
+                </div>
+                <div className="font-semibold flex gap-3">
+                  <p>Marca Personal:</p>
+                  <p className="font-normal">{modalItems.marca_personal}</p>
+                </div>
+                <div className="font-semibold flex gap-3">
+                  <p>Creado:</p>
+                  <p className="font-normal">
+                    {funcionHorario(modalItems.createdAt)}
+                  </p>
+                </div>
+                <div className="font-semibold flex gap-3">
+                  <p>Estado:</p>
+                  <p className="font-normal">{modalItems.status}</p>
+                </div>
+                {modalItems.status === "Contratado" && (
+                  <div>
+                    <div className="font-semibold flex gap-3">
+                      <p>Monto:</p>
+                      <p className="font-normal">{modalItems.pagos.monto}</p>
+                    </div>
+                    <div className="font-semibold flex gap-3">
+                      <p>Cuotas:</p>
+                      <p className="font-normal">
+                        {modalItems.pagos.cuotasPagadas} /{" "}
+                        {modalItems.pagos.cuotas}{" "}
+                      </p>
+                    </div>
+                    <div className="font-semibold flex gap-3">
+                      <p>Valor cuota:</p>
+                      <p className="font-normal">
+                        {modalItems.pagos.valorCuota}
+                      </p>
+                    </div>
+                  </div>
+                )}
+              </motion.div>
             </div>
 
             <div className="">
