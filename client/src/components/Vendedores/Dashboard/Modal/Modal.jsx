b28552b120e2cf4637d27ceb045c8e0586a4efdc
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-07-17 17:05:07 -0300

    Update Modal.jsx

--- client/src/components/Vendedores/Dashboard/Modal/Modal.jsx
+++ client/src/components/Vendedores/Dashboard/Modal/Modal.jsx
@@ -507,13 +507,6 @@ function ConfirmacionEdicion({ handleConfirmEdit, id, emailValidator }) {
   return (
     <React.Fragment>
       <div className="flex justify-around items-center relative">
-        {/* <button
-          type="button"
-          className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-          onClick={handleCancel}
-        >
-          Close x
-        </button> */}
         {emailValidator ? (
           <BsCheck
             onClick={handleOpen}
@@ -527,13 +520,6 @@ function ConfirmacionEdicion({ handleConfirmEdit, id, emailValidator }) {
             </p>
           </>
         )}
-        {/* <button
-          type="button"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-          onClick={handleOpen}
-        >
-          Save Changes
-        </button> */}
       </div>
       <Modal
         open={openConfirmacionEdicion}
