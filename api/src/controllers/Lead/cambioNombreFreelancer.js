27e0c9e281f9ea37e155d50dcf3ad2be704c18ba
Jonavoe <voeffray.jonathan@gmail.com>
2023-07-17 15:56:50 -0300

    Update cambioNombreFreelancer.js

--- api/src/controllers/Lead/cambioNombreFreelancer.js
+++ api/src/controllers/Lead/cambioNombreFreelancer.js
@@ -1,7 +1,12 @@
+// Importar el modelo Lead desde la ruta especificada
 const Lead = require("../../models/Lead");
 
+// Función para cambiar el nombre del corredor y vendedor (si son freelancers) en la colección Lead
 const cambioNombreFreelancer = async (body) => {
+  // Extraer datos del cuerpo de la solicitud
   const { email, name } = body;
+
+  // Actualizar el nombre del corredor y vendedor en todos los registros donde el correo electrónico del corredor coincida y también sean freelancers
   const leadResult = await Lead.updateMany(
     { corredor: email, freelancer: true },
     {
@@ -9,7 +14,10 @@ const cambioNombreFreelancer = async (body) => {
       vendedor_name: name,
     }
   );
+
+  // Devolver el resultado de la actualización
   return leadResult;
 };
 
+// Exportar la función 'cambioNombreFreelancer' para su uso externo
 module.exports = cambioNombreFreelancer;
