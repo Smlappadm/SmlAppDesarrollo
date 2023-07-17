1f98bf7d45022ba453f087d15e3238f8703981fe
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-07-17 16:14:57 -0300

    Update updateBannedEmploy.js

--- api/src/controllers/Employees/updateBannedEmploy.js
+++ api/src/controllers/Employees/updateBannedEmploy.js
@@ -1,9 +1,16 @@
+const Lead = require("../../models/Lead");
 const Employees = require("../../models/Employees");
 
 const updateBannedEmploy = async (email, updatedData) => {
+  // const leadsFreelancer = await Lead.find();
+  //   console.log(leadsFreelancer);
+  const leads = await Lead.find();
+console.log(leads.length)
+
   const employ = await Employees.findOneAndUpdate({ email }, updatedData, {
     new: true,
   });
+  console.log(employ)
   return employ;
 };
 
