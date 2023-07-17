f971d049bc325c39818a5f7503660d734b60553a
Jonavoe <voeffray.jonathan@gmail.com>
2023-07-17 16:26:07 -0300

    Update getLeadChecked.js

--- api/src/controllers/Lead/getLeadChecked.js
+++ api/src/controllers/Lead/getLeadChecked.js
@@ -1,20 +1,19 @@
+// Importando el modelo Lead
 const Lead = require("../../models/Lead");
 
+// Función para obtener leads que han sido marcados como "checked" (verificados)
 const getLeadChecked = async () => {
+  // Busca todos los registros de lead en la colección 'Lead' que cumplen ciertas condiciones
   const leadChequed = await Lead.find({
-    checked: true,
-    freelancer: {$ne: true},
+    checked: true, // Campo "checked" debe ser igual a true (verificados)
+    freelancer: { $ne: true }, // Campo "freelancer" no debe ser igual a true (no freelancers)
     status: {
-      $nin: [
-        "No responde",
-        "Agendar 2do llamado",
-        "incidencia",
-        "discard",
-        "discard_bot",
-      ],
+      // Campo "status" no debe ser ninguno de los siguientes valores:
+      $nin: ["No responde", "Agendar 2do llamado", "incidencia", "discard", "discard_bot"],
     },
   });
-  return leadChequed;
+
+  return leadChequed; // Devuelve un array con los leads que cumplen con las condiciones
 };
 
 module.exports = getLeadChecked;
