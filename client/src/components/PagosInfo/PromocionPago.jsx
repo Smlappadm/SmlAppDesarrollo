e6b8b61d9c00e14201e3782a9ef570beb91652f0
Nikitoo17 <nicolasfadel17@gmail.com>
2023-07-18 15:03:59 -0300

    Create PromocionPago.jsx

---
+++ client/src/components/PagosInfo/PromocionPago.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function PromocionPago() {
+  return (
+    <div className="h-full w-full">
+      <div className="flex">
+        <p>Promocion</p>
+      </div>
+    </div>
+  );
+}
