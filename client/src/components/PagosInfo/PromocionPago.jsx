61293a3b3f40dff58b054c1861e901f475d53966
Nikitoo17 <nicolasfadel17@gmail.com>
2023-07-18 15:05:00 -0300

    Update PromocionPago.jsx

--- client/src/components/PagosInfo/PromocionPago.jsx
+++ client/src/components/PagosInfo/PromocionPago.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function PromocionPago() {
   return (
     <div className="h-screen w-screen bg-pink-400">
-      <div className="flex">
+      <div className="flex flex-col justify-center items-center">
         <p>Promocion</p>
       </div>
     </div>
