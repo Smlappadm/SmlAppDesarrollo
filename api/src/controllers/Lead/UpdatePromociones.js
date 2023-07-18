af6bc5e7283ba7d4d9b34d2c4801b2c8d56b0979
Nikitoo17 <nicolasfadel17@gmail.com>
2023-07-18 16:44:49 -0300

    Update UpdatePromociones.js

--- api/src/controllers/Lead/UpdatePromociones.js
+++ api/src/controllers/Lead/UpdatePromociones.js
@@ -1,11 +1,10 @@
 // Importando el modelo Lead
-const { trusted } = require("mongoose");
 const Lead = require("../../models/Lead");
 
 // Función para obtener un lead por su dirección de correo electrónico de la aplicación (emailApp)
-const UpdatePromociones = async ({ emailApp, updateBody }) => {
+const UpdatePromociones = async ({ emailApp, body }) => {
   // Busca un único registro de lead en la colección 'Lead' que coincida con la dirección de correo electrónico de la aplicación proporcionada (emailApp)
-  const client = await Lead.findOne({ emailApp }, updateBody, { new: true });
+  const client = await Lead.findOneAndUpdate(emailApp, { body }, { new: true });
 
   return client; // Devuelve el lead encontrado o 'null' si no se encuentra ninguno
 };
