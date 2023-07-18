e4bffc9827bd94b432d7360e902db98d7b6a180d
Nikitoo17 <nicolasfadel17@gmail.com>
2023-07-17 21:18:14 -0300

    Update getAllFreelancer.js

--- api/src/controllers/Freelancer/getAllFreelancer.js
+++ api/src/controllers/Freelancer/getAllFreelancer.js
@@ -4,7 +4,7 @@ const Freelancer = require("../../models/Freelancer");
 // Función asincrónica para obtener todos los freelancers de la base de datos
 const getAllFreelancers = async () => {
   // Buscamos todos los freelancers en la base de datos utilizando el modelo "Freelancer"
-  const freelancer = await Freelancer.find();
+  const freelancer = await Freelancer.find({ deleted: false });
 
   return freelancer; // Devolvemos la lista de freelancers encontrados
 };
