d28b7fd4cd964565e9f7bd0118808252b73fc5a9
Nikitoo17 <nicolasfadel17@gmail.com>
2023-07-19 16:25:23 -0300

    Update PromocionPago.jsx

--- client/src/components/PagosInfo/PromocionPago.jsx
+++ client/src/components/PagosInfo/PromocionPago.jsx
@@ -49,6 +49,7 @@ export default function PromocionPago({ tamañoPantalla }) {
       1: "Pago único de 4000€",
       2: "Cuotas de 2000€/mes, Total: 4000€",
       4: "Cuotas de 1000€/mes, Total: 4000€",
+      6: "Cuotas de 1000€/mes, Total: 4000€",
       10: "Cuotas de 500€/mes, Total: 5000€",
     },
     links: {
