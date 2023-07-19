c0e595e9cfeba3cff5637ade4c15b5a03f1227de
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-07-19 15:49:46 -0300

    Update updateLeadFreelanceById.js

--- api/src/controllers/Lead/updateLeadFreelanceById.js
+++ api/src/controllers/Lead/updateLeadFreelanceById.js
@@ -136,9 +136,17 @@ const updateLeadFreelanceById = async (id, updatedData) => {
     new: true,
   });
 
-  leadUpdate.observaciones_ventas.push(updatedData.dataObservaciones)
+  let leadUpdated = {}
 
-  const leadUpdated = await leadUpdate.save();
+  if (updatedData.dataObservaciones && 
+    (updatedData.dataObservaciones.observacion !== "" || 
+    updatedData.dataObservaciones.tipoContacto !== "")
+  ) {
+  const filter = { _id: id }; // Define la condición para encontrar el documento
+  const update = { $push: { observaciones_ventas: updatedData.dataObservaciones } };
+  const options = { new: true }; // Devuelve el documento actualizado
+  const leadUpdated = await Lead.findOneAndUpdate(filter, update, options);
+  }
   // Actualizamos el vendedor asociado al lead
   // let vendedor = [];
   // vendedor = await Vendedor.findOneAndUpdate(
