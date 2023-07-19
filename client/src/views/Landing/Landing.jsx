fd1f82044c685f95d5ce2e95b4657c46c5034822
Jonavoe <voeffray.jonathan@gmail.com>
2023-07-19 16:04:55 -0300

    Update Landing.jsx

--- client/src/views/Landing/Landing.jsx
+++ client/src/views/Landing/Landing.jsx
@@ -60,7 +60,7 @@ function Landing() {
 
   
   useEffect(() => {
-    if (corredor.name !== undefined) {
+    if (corredor && corredor.name && corredor.name !== undefined) {
       console.log("adentro", corredor.name);
       localStorage.setItem("corredorName", corredor.name);
     }
