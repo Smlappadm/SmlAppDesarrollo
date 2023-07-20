b3b50943fa208bb4cdb07998f4744dae8642ec5d
Nikitoo17 <nicolasfadel17@gmail.com>
2023-07-20 16:26:43 -0300

    Update Lead.js

--- api/src/models/Lead.js
+++ api/src/models/Lead.js
@@ -248,6 +248,10 @@ const LeadSchema = new mongoose.Schema(
       require: true,
       default: false,
     },
+    promocion: {
+      type: Object,
+      required: true,
+    },
     promocion1: {
       type: Date,
     },
