92c3041ae31fc055986bda9281669377a08ca92e
Nikitoo17 <nicolasfadel17@gmail.com>
2023-07-20 16:44:54 -0300

    Update UpdatePromociones.js

--- api/src/controllers/Lead/UpdatePromociones.js
+++ api/src/controllers/Lead/UpdatePromociones.js
@@ -1,32 +1,27 @@
+// Importando el modelo Lead
 const Lead = require("../../models/Lead");
 
+// Función para obtener un lead por su dirección de correo electrónico de la aplicación (emailApp)
 const UpdatePromociones = async (body) => {
-  const { emailApp, promociones } = body;
+  const { promociones, emailApp } = body;
+  // const promocion1Date = new Date(promocion1);
+  // const promocion2Date = new Date(promocion2);
 
-  // Convertir las fechas de promociones a objetos Date
   const promocionesObj = {};
   for (const key in promociones) {
     promocionesObj[key] = new Date(promociones[key]);
   }
+  console.log(promocionesObj);
 
-  // Buscar un único registro de lead en la colección 'Lead' que coincida con la dirección de correo electrónico de la aplicación proporcionada (emailApp)
-  const client = await Lead.findOne({ emailApp });
+  // Busca un único registro de lead en la colección 'Lead' que coincida con la dirección de correo electrónico de la aplicación proporcionada (emailApp)
+  const client = await Lead.findOneAndUpdate(
+    { emailApp },
+    { $push: { promociones: { $each: [promocion1Date, promocion2Date] } } },
+    { new: true }
+  );
 
-  if (!client) {
-    // Si no se encuentra el cliente, puedes manejar el caso según tus necesidades
-    return null;
-  }
-
-  // Actualizar las promociones con el objeto promocionesObj
-  client.promociones = promocionesObj;
-
-  // Obtén la cantidad de promociones
-  const cantidadPromociones = Object.keys(promocionesObj).length;
-
-  // Ahora, guarda los cambios actualizados en la base de datos
-  await client.save();
-
-  return client; // Devuelve el lead actualizado y la cantidad de promociones
+  return client; // Devuelve el lead encontrado o 'null' si no se encuentra ninguno
 };
 
+// Exporta la función para que pueda ser utilizada en otros módulos
 module.exports = UpdatePromociones;
