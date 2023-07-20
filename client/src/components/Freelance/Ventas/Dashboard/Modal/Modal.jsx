e0a2246591a60ab7dba13453367d5ccc99cda548
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-07-19 22:04:21 -0300

    Update Modal.jsx

--- client/src/components/Freelance/Ventas/Dashboard/Modal/Modal.jsx
+++ client/src/components/Freelance/Ventas/Dashboard/Modal/Modal.jsx
@@ -1325,8 +1325,9 @@ export default function NestedModal({
               >
                 Estado
               </label>
-              {item.status !== "Agendar 2do llamado" ? (
+              {item.status !== "Contactado" ? (
                 <>
+                {statusObj.status = "Contactado"}
                   <select
                     onChange={handleSelectChange}
                     name="status"
@@ -1334,12 +1335,12 @@ export default function NestedModal({
                     id="select1"
                     className="mb-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   >
-                    <option value="Sin contactar">Sin Contactar</option>
+                    {/* <option value="Sin contactar">Sin Contactar</option> */}
                     {/* <option value="Agendar 2do llamado">En proceso</option> */}
-                    <option value="Contratando">Contratando</option>
+                    <option value="Contactado">Contactado</option>
                     {/* <option value="Contratado">Contratado</option> */}
-                    <option value="Rechazado">Rechazado</option>
-                    <option value="No responde">No Responde</option>
+                    {/* <option value="Rechazado">Rechazado</option> */}
+                    {/* <option value="No responde">No Responde</option> */}
                   </select>
 
                   {statusObj.status === "Rechazado" && (
