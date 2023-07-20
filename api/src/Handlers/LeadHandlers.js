55100a21703a2535382d868f04019e762d11df47
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-07-19 22:04:28 -0300

    Update LeadHandlers.js

--- api/src/Handlers/LeadHandlers.js
+++ api/src/Handlers/LeadHandlers.js
@@ -42,7 +42,7 @@ const cambioNombreVendedor = require("../controllers/Lead/cambioNombreVendedor")
 const cambioNombreClevel = require("../controllers/Lead/cambioNombreClevel");
 const cambioNombreLeader = require("../controllers/Lead/cambioNombreLeader");
 const UpdatePromociones = require("../controllers/Lead/UpdatePromociones");
-const getAllLeadContratando = require("../controllers/Lead/getAllLeadsContratando");
+const getAllLeadContactado = require("../controllers/Lead/getAllLeadContactado");
 
 // Obtener todos los leads
 const getAllLeadHandler = async (req, res) => {
@@ -54,10 +54,10 @@ const getAllLeadHandler = async (req, res) => {
   }
 };
 // Obtener todos los leads
-const getAllLeadsContratandoHandler = async (req, res) => {
+const getAllLeadsContactadoHandler = async (req, res) => {
   try {
-    const leadContratando = await getAllLeadContratando();
-    res.status(200).json(leadContratando);
+    const leadContactado = await getAllLeadContactado();
+    res.status(200).json(leadContactado);
   } catch (error) {
     res.status(404).json({ error: error.message });
   }
@@ -646,6 +646,6 @@ module.exports = {
   cambioNombreVendedorHandler,
   cambioNombreClevelHandler,
   cambioNombreLeaderHandler,
-  getAllLeadsContratandoHandler,
+  getAllLeadsContactadoHandler,
   UpdatePromocionesHandler,
 };
