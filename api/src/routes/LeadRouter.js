88bf2b886f35b47ffa8dc8675a317bcb8e5cc5aa
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-07-19 22:04:24 -0300

    Update LeadRouter.js

--- api/src/routes/LeadRouter.js
+++ api/src/routes/LeadRouter.js
@@ -43,13 +43,13 @@ const {
   cambioNombreLeaderHandler,
   cambioNombreVendedorHandler,
   cambioNombreCorredorHandler,
-  getAllLeadsContratandoHandler,
+  getAllLeadsContactadoHandler,
   UpdatePromocionesHandler,
 } = require("../Handlers/LeadHandlers");
 const LeadRouter = Router();
 
 LeadRouter.get("/", getAllLeadHandler);
-LeadRouter.get("/contratando", getAllLeadsContratandoHandler);
+LeadRouter.get("/contactado", getAllLeadsContactadoHandler);
 LeadRouter.get("/profesion", getAllProfesionHandler);
 LeadRouter.get("/profesionFreelance", getAllProfesionFreelanceHandler);
 LeadRouter.get("/allcorredor", getCorredoresHandler);
