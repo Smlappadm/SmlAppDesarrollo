258139de3989cb75e32024824d7c8d0b3024a582
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-07-20 15:58:23 -0300

    Update VentasHistory.jsx

--- client/src/components/Freelance/Ventas/analytics/VentasHistory.jsx
+++ client/src/components/Freelance/Ventas/analytics/VentasHistory.jsx
@@ -501,7 +501,7 @@ const VentasHistory = () => {
                         )}
                         {item.status === "Agenda llamada" && (
                           <div className="bg-[#5bac42] w-44 h-11 flex flex-col justify-center items-center text-white rounded-3xl text-16">
-                            <p>En Proceso</p>
+                            <p>Agenda llamada</p>
                           </div>
                         )}
                         {item.status === "Contactado" && (
