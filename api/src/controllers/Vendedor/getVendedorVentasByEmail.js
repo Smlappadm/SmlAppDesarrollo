6c8b882f3625e559da10ceb34f79f2f8d6605b52
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-07-21 14:55:02 -0300

    Update getVendedorVentasByEmail.js

--- api/src/controllers/Vendedor/getVendedorVentasByEmail.js
+++ api/src/controllers/Vendedor/getVendedorVentasByEmail.js
@@ -7,7 +7,9 @@ const Lead = require("../../models/Lead");
 const getVendedorVentasByEmail = async (email) => {
   // Buscar todos los leads en la base de datos donde el campo "vendedor" coincide con el correo electrónico proporcionado
   // y el campo "status" es igual a "Agenda llamada"
-  const leads = await Lead.find({ vendedor: email, pagoRecibido: { $ne: true }, status:{ $in: ["Contactado", "No responde", "Agenda llamada"] } });
+  const leadsAgenda = await Lead.find({ vendedor: email, pagoRecibido: { $ne: true }, status:"Agenda llamada"});
+  const leads = await Lead.find({ vendedor: email, pagoRecibido: { $ne: true }, status:"Contactado"});
+  const leadsNoResponde = await Lead.find({ vendedor: email, pagoRecibido: { $ne: true }, status:"No responde"});
   // "Sin contactar",
   // "Rechazado",
   // "Contratado",
@@ -16,41 +18,69 @@ const getVendedorVentasByEmail = async (email) => {
   // "Contactado",
   // "incidencia",
   // "discard",
-
+  //-------------------------------------------------------------------------------------------------------------------------
   // Ordenar los leads encontrados por fecha de llamada de venta
-  // const sortClients = leads.sort((a, b) => {
-  //   Obtener las fechas de llamada de venta (se supone que están en la propiedad "llamada_venta" del lead)
-  //   const dateA = a.llamada_venta?.dateObject; // El operador opcional "?." se asegura de que "a.llamada_venta" exista
-  //   const dateB = b.llamada_venta?.dateObject; // El operador opcional "?." se asegura de que "b.llamada_venta" exista
+  const leadsAgendaSorted = leadsAgenda.sort((a, b) => {
+    //Obtener las fechas de llamada de venta (se supone que están en la propiedad "llamada_venta" del lead)
+    const dateA = a.llamada_venta?.dateObject; // El operador opcional "?." se asegura de que "a.llamada_venta" exista
+    const dateB = b.llamada_venta?.dateObject; // El operador opcional "?." se asegura de que "b.llamada_venta" exista
 
-  //   Comparar las fechas de llamada de venta para ordenar los leads en orden ascendente
-  //   if (!dateA.mes) return 1; // Si dateA no tiene mes, lo coloca después de dateB
-  //   if (!dateB.mes) return -1; // Si dateB no tiene mes, lo coloca antes de dateA
+    //Comparar las fechas de llamada de venta para ordenar los leads en orden ascendente
+    if (!dateA.mes) return 1; // Si dateA no tiene mes, lo coloca después de dateB
+    if (!dateB.mes) return -1; // Si dateB no tiene mes, lo coloca antes de dateA
 
-  //   Compara primero por año, luego por mes, día, hora y minutos en caso de empates
-  //   if (dateA.year !== dateB.year) {
-  //     return dateA.year - dateB.year;
-  //   }
-  //   if (dateA.mes !== dateB.mes) {
-  //     return dateA.mes - dateB.mes;
-  //   }
-  //   if (dateA.dia !== dateB.dia) {
-  //     return dateA.dia - dateB.dia;
-  //   }
-  //   if (dateA.hora !== dateB.hora) {
-  //     return dateA.hora - dateB.hora;
-  //   }
-  //   if (dateA.minutos !== dateB.minutos) {
-  //     return dateA.minutos - dateB.minutos;
-  //   }
+    //Compara primero por año, luego por mes, día, hora y minutos en caso de empates
+    if (dateA.year !== dateB.year) {
+      return dateA.year - dateB.year;
+    }
+    if (dateA.mes !== dateB.mes) {
+      return dateA.mes - dateB.mes;
+    }
+    if (dateA.dia !== dateB.dia) {
+      return dateA.dia - dateB.dia;
+    }
+    if (dateA.hora !== dateB.hora) {
+      return dateA.hora - dateB.hora;
+    }
+    if (dateA.minutos !== dateB.minutos) {
+      return dateA.minutos - dateB.minutos;
+    }
 
-  //   Si llega hasta aquí, significa que las fechas son iguales
-  //   return 0;
-  // });
+    //Si llega hasta aquí, significa que las fechas son iguales
+    return 0;
+  });
+  //-------------------------------------------------------------------------------------------------------------------------
+
+    const leadsNoRespondenSorted = leadsNoResponde.sort((a, b) => {
+    const dateA = a.updatedAt.toISOString();
+    const dateB = b.updatedAt.toISOString();
+    
+    if (dateA.slice(0, 4) !== dateB.slice(0, 4)) {
+      return dateA.slice(0, 4) - dateB.slice(0, 4);
+    }
+    
+    if (dateA.slice(5, 7) !== dateB.slice(5, 7)) {
+      return dateA.slice(5, 7) - dateB.slice(5, 7);
+    }
+    
+    if (dateA.slice(8, 10) !== dateB.slice(8, 10)) {
+      return dateA.slice(8, 10) - dateB.slice(8, 10);
+    }
+    
+    if (dateA.slice(11, 13) !== dateB.slice(11, 13)) {
+      return dateA.slice(11, 13) - dateB.slice(11, 13);
+    }
+    
+    if (dateA.slice(14, 16) !== dateB.slice(14, 16)) {
+      return dateA.slice(14, 16) - dateB.slice(14, 16);
+    }
+
+    return 0;
+  });
 
   // Devolver el resultado de la consulta (los leads de ventas ordenados por fecha de llamada de venta)
   // return sortClients;
-  return leads;
+  return [...leadsAgendaSorted, ...leads, ...leadsNoRespondenSorted];
 };
 
 // Exportar la función para que pueda ser utilizada en otros módulos
