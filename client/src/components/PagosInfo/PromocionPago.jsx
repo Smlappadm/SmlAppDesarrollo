ebace7a39a0d84ac91efff2cb83e4e0df6b5ba22
Nikitoo17 <nicolasfadel17@gmail.com>
2023-07-20 21:20:26 -0300

    Update PromocionPago.jsx

--- client/src/components/PagosInfo/PromocionPago.jsx
+++ client/src/components/PagosInfo/PromocionPago.jsx
@@ -315,14 +315,7 @@ export default function PromocionPago({ tamañoPantalla }) {
              
         
                
-            
-                  cuotas === "4"
-                    ? "rounded-md border border-black mr-2 bg-blue-500 text-black font-bold"
-                    : "rounded-md border border-white mr-2 font-bold"
-                }
-                onClick={() => CambiarCuota("4")}
-              >
-                <p className="py-3 px-4">4</p>
+            3 px-4">4</p>
               </div>
               <div
                 className={
