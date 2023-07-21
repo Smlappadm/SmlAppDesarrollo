0827cdb1559d5e64c4c48184eb2f86e4c68f6d2f
Nikitoo17 <nicolasfadel17@gmail.com>
2023-07-21 16:23:25 -0300

    Update ModalAddLeadVendedor.jsx

--- client/src/components/Vendedores/Dashboard/MUI/ModalAddLeadVendedor.jsx
+++ client/src/components/Vendedores/Dashboard/MUI/ModalAddLeadVendedor.jsx
@@ -154,7 +154,7 @@ export default function ModalAddLeadVendedor({
       freelancer: false,
       vendedor: OneVendedor && OneVendedor[0].email,
       vendedor_name: OneVendedor && OneVendedor[0].name,
-      from: OneFreelancer && OneFreelancer[0].email,
+      from: OneVendedor && OneVendedor[0].email,
       status: "Sin contactar",
       marca_personal: "No",
     };
