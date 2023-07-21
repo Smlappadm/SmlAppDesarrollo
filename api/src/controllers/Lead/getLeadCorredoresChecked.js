b51678536bb0e580a4754d4b0f4db4b46b705642
Jonavoe <voeffray.jonathan@gmail.com>
2023-07-20 21:02:07 -0300

    Update getLeadCorredoresChecked.js

--- api/src/controllers/Lead/getLeadCorredoresChecked.js
+++ api/src/controllers/Lead/getLeadCorredoresChecked.js
@@ -6,7 +6,7 @@ const getLeadCorredorChecked = async (email) => {
   // Utiliza el método 'find' para buscar todos los registros de lead en la colección 'Lead'
   // donde el campo 'corredor' es igual al valor de 'email' proporcionado y el campo 'checked' es igual a true (verificados)
   // Luego, utiliza el método 'exec()' para ejecutar la consulta
-  const leads = await Lead.find({ corredor: email, checked: true }).exec();
+  const leads = await Lead.find({ corredor: email, checked: true });
 
   return leads; // Devuelve un array con los leads verificados asociados al corredor específico
 };
