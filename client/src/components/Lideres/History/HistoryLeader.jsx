8e7581b3e3d7815717d60398adb7cb4319ed1409
Jonavoe <voeffray.jonathan@gmail.com>
2023-07-21 17:07:38 -0300

    Update HistoryLeader.jsx

--- client/src/components/Lideres/History/HistoryLeader.jsx
+++ client/src/components/Lideres/History/HistoryLeader.jsx
@@ -168,7 +168,7 @@ export const LideresHistory = () => {
               to={"/contratando/"}
             >
               <IoGrid className="text-[2rem] text-[#418df0] hover:text-[#3570bd]" />
-              <p className="text-white">Contratando</p>
+              <p className="text-white">Seguimiento</p>
             </Link>
             <Link
               className="flex items-center justify-center gap-2"
