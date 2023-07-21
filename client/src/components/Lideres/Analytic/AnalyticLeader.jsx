a6de7bc392cab3221d3d8128ce8d6d9bee5e20c6
Jonavoe <voeffray.jonathan@gmail.com>
2023-07-21 17:07:24 -0300

    Update AnalyticLeader.jsx

--- client/src/components/Lideres/Analytic/AnalyticLeader.jsx
+++ client/src/components/Lideres/Analytic/AnalyticLeader.jsx
@@ -157,7 +157,7 @@ export const AnalyticLeader = () => {
               to={"/contratando/"}
             >
               <IoGrid className="text-[2rem] text-[#418df0] hover:text-[#3570bd]" />
-              <p className="text-white">Contratando</p>
+              <p className="text-white">Seguimiento</p>
             </Link>
             <Link
               className="flex items-center justify-center gap-2"
