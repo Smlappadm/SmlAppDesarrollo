30cd34793851d531a0b0767b4d40aeea024f71b8
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-07-24 15:51:40 -0300

    Update VendedoresDashboard.jsx

--- client/src/components/Vendedores/Dashboard/VendedoresDashboard.jsx
+++ client/src/components/Vendedores/Dashboard/VendedoresDashboard.jsx
@@ -353,13 +353,14 @@ const VendedoresDashboard = () => {
                           )}
                         </div>
                         <div className=" w-[15%] flex justify-center items-center p-0 ">
-                          <div className="flex w-full justify-center items-center gap-2 relative">
+                        <div className="flex w-44 justify-start items-center gap-2 relative">
                             <p
                               onClick={() => handleCopyClick(item.telephone)}
                               className="text-start w-44 p-1 cursor-pointer  px-3 rounded-full text-ellipsis text-16 opacity-1 overflow-hidden whitespace-nowrap hover:overflow-visible hover:bg-[#e3e1e1] hover:w-fit hover:text-black z-111 "
                             >
                               {item.telephone}
                             </p>
+                            </div>
                             <a
                               href={`http://wa.me/${item.telephone.replace(
                                 /\s+/g,
@@ -369,7 +370,6 @@ const VendedoresDashboard = () => {
                             >
                               <FaWhatsapp className="text-[30px] block mr-5 text-[#9eabbe] cursor-pointer hover:text-green-500 hover:text-[33px]" />
                             </a>
-                          </div>
                         </div>
                         <div className=" w-[5%] flex justify-center items-center p-0">
                           {item.level !== "incidencia" ? (
