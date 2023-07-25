86378b24b018ba9081e2c91f49259d131a0f6da2
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-07-25 18:48:04 -0300

    Update VendedoresHistory.jsx

--- client/src/components/Vendedores/analytics/VendedoresHistory.jsx
+++ client/src/components/Vendedores/analytics/VendedoresHistory.jsx
@@ -547,7 +547,7 @@ const VendedoresHistory = () => {
                         )}
                       </div>
                       <div className=" w-[8%] flex justify-end items-center p-0 gap-3">
-                        {item.status === "Contratado" || item.status === "A pagar" (
+                        {(item.status === "Contratado" || item.status === "A pagar") && (
                           <ModalHistory
                             item={item}
                             SendLeadAlertBaja={SendLeadAlertBaja}
