fc82ea23e09225816b5d861d4b3d9e1388505508
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-07-25 16:53:38 -0300

    Update setPagoLink.js

--- api/src/controllers/Lead/setPagoLink.js
+++ api/src/controllers/Lead/setPagoLink.js
@@ -4,42 +4,20 @@ const Lead = require("../../models/Lead");
 // Función para cambiar el valor de un campo específico en un documento Lead
 const setPagoLink = async (body) => {
   // Verificar si el campo a cambiar es "emailApp"
-console.log(body)
+  console.log(body);
 
-    // Si es "emailApp", buscar y actualizar todos los registros que tengan el valor especificado
+  const leadUpdated = await Lead.findByIdAndUpdate(
+    body.id,
+    {
+      $set: {
+        linkActivado: true,
+        linkPago: body.linkPago,
+      },
+    },
+    { new: true }
+  );
 
-const lead = await Lead.find({ _id: body.id })
-
-
-
-    // const leadsearch = await Lead.updateOne(
-    //   { _id: body.id },
-    //   {
-    //     $set: {
-    //       linkActivado: true,
-    //       linkPago: body.linkPago
-    //     },
-    //   }
-    // );
-
-
-
-    // return leadsearch
-    return lead
-//     // Nota: Esta parte del código borra el valor actual de "emailApp" si hay coincidencias.
-
-//     // No se devuelve nada aquí, ya que la función principal solo debería retornar el lead actualizado.
-//   }
-
-//   // Buscar y actualizar el campo específico del documento Lead con el nuevo valor
-//   const lead = await Lead.findOneAndUpdate(
-//     { _id: id },
-//     { [key]: value },
-//     { new: true }
-//   );
-
-//   // Devolver el documento Lead actualizado
-//   return lead;
+  return leadUpdated;
 };
 
 // Exportar la función 'changeLeadEmail' para su uso externo
