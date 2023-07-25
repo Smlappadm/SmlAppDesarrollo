11429292c9ca2a6b49e1840c53dfdeb138086230
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-07-25 17:11:22 -0300

    Update setPagoLink.js

--- api/src/controllers/Lead/setPagoLink.js
+++ api/src/controllers/Lead/setPagoLink.js
@@ -1,12 +1,8 @@
 // Importar el modelo Lead desde la ruta especificada
 const Lead = require("../../models/Lead");
 
-// Función para cambiar el valor de un campo específico en un documento Lead
 const setPagoLink = async (body) => {
-  // Verificar si el campo a cambiar es "emailApp"
-  console.log(body);
-
-  const leadUpdated = await Lead.findByIdAndUpdate(
+  const lead = await Lead.findByIdAndUpdate(
     body.id,
     {
       $set: {
@@ -17,8 +13,7 @@ const setPagoLink = async (body) => {
     { new: true }
   );
 
-  return leadUpdated;
+  return lead;
 };
 
-// Exportar la función 'changeLeadEmail' para su uso externo
 module.exports = setPagoLink;
