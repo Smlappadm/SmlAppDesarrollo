30d37d376b0b7cab9c3f9ab132a299d2a218ee7e
Nikitoo17 <nicolasfadel17@gmail.com>
2023-07-26 16:46:36 -0300

    Update PromocionPago.jsx

--- client/src/components/PagosInfo/PromocionPago.jsx
+++ client/src/components/PagosInfo/PromocionPago.jsx
@@ -10,11 +10,11 @@ export default function PromocionPago({ tamañoPantalla }) {
   const url = new URL(window.location.href);
   const emailApp = url.searchParams.get("emailApp");
   const { clienteEmpresa, promociones } = useSelector((state) => state);
+  const [promos, setPromos] = useState([]);
   const [tiempoRestante, setTiempoRestante] = useState({});
   const [cliente, setCliente] = useState({});
   const [todasPromocionesCero, setTodasPromocionesCero] = useState(false);
   const [promocionActual, setPromocionActual] = useState(0);
-  const [promos, setPromos] = useState([]);
   const [cuotas, setCuotas] = useState("1");
   const dispatch = useDispatch();
 
