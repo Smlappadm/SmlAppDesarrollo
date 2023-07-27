d7de6ce6f2b83a65654d446ea304b684415b0af5
Nikitoo17 <nicolasfadel17@gmail.com>
2023-07-27 18:05:30 -0300

    Update VendedoresDashboard.jsx

--- client/src/components/Vendedores/Dashboard/VendedoresDashboard.jsx
+++ client/src/components/Vendedores/Dashboard/VendedoresDashboard.jsx
@@ -37,6 +37,10 @@ const VendedoresDashboard = () => {
   const [showEmail, setShowEmail] = useState(false);
   const [openModalPago, setOpenModalPago] = useState(false);
   const [saveEmailApp, setSaveEmailApp] = useState("");
+  const [profesion, setProfesion] = useState("");
+  const [country, setCountry] = useState("");
+  const [status, setStatus] = useState("");
+  const [freelancer, setFreelancer] = useState("");
 
   const user = useUser().user;
   const email = user?.emailAddresses[0]?.emailAddress;
@@ -45,8 +49,6 @@ const VendedoresDashboard = () => {
   let emailAddress = localStorage.getItem("email");
   const body = { name: fullName, email: emailAddress };
 
-  const [profesion, setProfesion] = useState("");
-  const [country, setCountry] = useState("");
   const [level, setLevel] = useState("");
 
   const notify = () => toast("Here is your toast.");
@@ -353,23 +355,23 @@ const VendedoresDashboard = () => {
                           )}
                         </div>
                         <div className=" w-[15%] flex justify-center items-center p-0 ">
-                        <div className="flex w-44 justify-start items-center gap-2 relative">
+                          <div className="flex w-44 justify-start items-center gap-2 relative">
                             <p
                               onClick={() => handleCopyClick(item.telephone)}
                               className="text-start w-44 p-1 cursor-pointer  px-3 rounded-full text-ellipsis text-16 opacity-1 overflow-hidden whitespace-nowrap hover:overflow-visible hover:bg-[#e3e1e1] hover:w-fit hover:text-black z-111 "
                             >
                               {item.telephone}
                             </p>
-                            </div>
-                            <a
-                              href={`http://wa.me/${item.telephone.replace(
-                                /\s+/g,
-                                ""
-                              )}`}
-                              target="blanck"
-                            >
-                              <FaWhatsapp className="text-[30px] block mr-5 text-[#9eabbe] cursor-pointer hover:text-green-500 hover:text-[33px]" />
-                            </a>
+                          </div>
+                          <a
+                            href={`http://wa.me/${item.telephone.replace(
+                              /\s+/g,
+                              ""
+                            )}`}
+                            target="blanck"
+                          >
+                            <FaWhatsapp className="text-[30px] block mr-5 text-[#9eabbe] cursor-pointer hover:text-green-500 hover:text-[33px]" />
+                          </a>
                         </div>
                         <div className=" w-[5%] flex justify-center items-center p-0">
                           {item.level !== "incidencia" ? (
