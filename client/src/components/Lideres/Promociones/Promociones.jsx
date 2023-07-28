7df6a21873b83addb483c4c2f579741316b91043
Nikitoo17 <nicolasfadel17@gmail.com>
2023-07-28 18:17:03 -0300

    Update Promociones.jsx

--- client/src/components/Lideres/Promociones/Promociones.jsx
+++ client/src/components/Lideres/Promociones/Promociones.jsx
@@ -164,7 +164,7 @@ export const Promociones = () => {
                   </div>
                 </div>
                 <div className="flex  justify-center items-center p-0">
-                  <div className="w-20 flex justify-center items-center p-0 ">
+                  <div className="w-1/12 flex justify-center items-center p-0 ">
                     <Text className="text-white ">{item.promocion.cuota}</Text>
                   </div>
                 </div>
