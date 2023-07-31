728b0d4050c7c4953f24358aa6ee43402d1dc850
Nikitoo17 <nicolasfadel17@gmail.com>
2023-07-31 16:58:52 -0300

    Update NavBar.jsx

--- client/src/components/Lideres/NavBar/NavBar.jsx
+++ client/src/components/Lideres/NavBar/NavBar.jsx
@@ -70,36 +70,41 @@ export default function NavBar() {
   return (
     <div className="flex gap-2">
       {titles.map((encabezado, index) => (
-        <div className="w-2/4">
-          <Link
-            className="flex items-center justify-center gap-2 "
-            to={encabezado.link}
-            key={index}
+        <Link
+          className="flex items-center justify-center gap-2 "
+          to={encabezado.link}
+          key={index}
+        >
+          {React.createElement(encabezado.icon, {
+            className: `text-[2rem] text-[#418df0] ${
+              encabezado.isHovered ? "hover:text-[#3570bd]" : ""
+            }`,
+            onMouseEnter: () => handleMouseEnter(index),
+            onMouseLeave: () => handleMouseLeave(index),
+          })}
+          <motion.div
+            className="flex items-center gap-2 text-white h-full "
+            style={{
+              width: encabezado.isHovered ? "fit-content" : "2rem",
+            }}
+            initial={{ width: path === encabezado.link ? 100 : 0 }}
+            animate={{
+              width:
+                path === encabezado.link ? 100 : encabezado.isHovered ? 100 : 0,
+            }}
+            transition={{ duration: 0.3, ease: "easeInOut" }}
           >
-            {React.createElement(encabezado.icon, {
-              className: `text-[2rem] text-[#418df0] ${
-                encabezado.isHovered ? "hover:text-[#3570bd]" : ""
-              }`,
-              onMouseEnter: () => handleMouseEnter(index),
-              onMouseLeave: () => handleMouseLeave(index),
-            })}
-            <motion.div
-              className="flex items-center gap-2 text-white h-full "
-              style={{
-                width: encabezado.isHovered ? "fit-content" : "2rem",
-              }}
-              initial={{ width: path === encabezado.link ? 100 : 0 }}
-              animate={{
-                width:
-                  path === encabezado.link
-                    ? 100
-                    : encabezado.isHovered
-                    ? 100
-                    : 0,
-              }}
-              transition={{ duration: 0.3, ease: "easeInOut" }}
-            >
-              {encabezado.isHovered ? (
+            {encabezado.isHovered ? (
+              <motion.p
+                className="text-white"
+                initial={{ opacity: 0, x: -10 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{ duration: 0.3, ease: "easeInOut" }}
+              >
+                {encabezado.title}
+              </motion.p>
+            ) : (
+              path === encabezado.link && (
                 <motion.p
                   className="text-white"
                   initial={{ opacity: 0, x: -10 }}
@@ -108,21 +113,10 @@ export default function NavBar() {
                 >
                   {encabezado.title}
                 </motion.p>
-              ) : (
-                path === encabezado.link && (
-                  <motion.p
-                    className="text-white"
-                    initial={{ opacity: 0, x: -10 }}
-                    animate={{ opacity: 1, x: 0 }}
-                    transition={{ duration: 0.3, ease: "easeInOut" }}
-                  >
-                    {encabezado.title}
-                  </motion.p>
-                )
-              )}
-            </motion.div>
-          </Link>
-        </div>
+              )
+            )}
+          </motion.div>
+        </Link>
       ))}
     </div>
   );
