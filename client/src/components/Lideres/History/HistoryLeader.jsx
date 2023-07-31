2a4a8c44d0fb3f7e4f99c20d00c7926a54e77e6f
Nikitoo17 <nicolasfadel17@gmail.com>
2023-07-31 16:44:36 -0300

    Update HistoryLeader.jsx

--- client/src/components/Lideres/History/HistoryLeader.jsx
+++ client/src/components/Lideres/History/HistoryLeader.jsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import style from "./HistoryLeader.module.css";
 import PaginationOutlined from "../../pagination/PaginationOutlined";
 import { Card, Text, Title } from "@tremor/react";
