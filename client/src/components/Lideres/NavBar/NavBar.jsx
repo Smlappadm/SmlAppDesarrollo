e361de2daaa14d4bee858ee913ee4716a1efb69f
Nikitoo17 <nicolasfadel17@gmail.com>
2023-07-31 16:59:01 -0300

    Update NavBar.jsx

--- client/src/components/Lideres/NavBar/NavBar.jsx
+++ client/src/components/Lideres/NavBar/NavBar.jsx
@@ -68,7 +68,7 @@ export default function NavBar() {
     });
   };
   return (
-    <div className="flex gap-2">
+    <div className="flex gap-2 bg-black">
       {titles.map((encabezado, index) => (
         <Link
           className="flex items-center justify-center gap-2 "
