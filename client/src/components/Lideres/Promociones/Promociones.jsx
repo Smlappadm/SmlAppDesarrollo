828568eb7ab9c4ccfe01017eb3cfad168cce2221
Nikitoo17 <nicolasfadel17@gmail.com>
2023-07-31 16:46:11 -0300

    Update Promociones.jsx

--- client/src/components/Lideres/Promociones/Promociones.jsx
+++ client/src/components/Lideres/Promociones/Promociones.jsx
@@ -2,13 +2,7 @@ import { Link } from "react-router-dom";
 import style from "./Promociones.module.css";
 import PaginationOutlined from "../../pagination/PaginationOutlined";
 import { Card, Text, Title } from "@tremor/react";
-import {
-  CiMail,
-  CiInstagram,
-  CiPhone,
-  CiWarning,
-  CiGlobe,
-} from "react-icons/ci";
+
 import Nav from "../../Nav/Nav";
 import { useEffect, useState } from "react";
 import "react-toastify/dist/ReactToastify.css";
