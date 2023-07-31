7dbb76138922081f53dc9ca5d22f30161f752a10
Jonavoe <voeffray.jonathan@gmail.com>
2023-07-31 19:54:57 -0300

    Update loginButton.jsx

--- client/src/views/Login/Material UI/loginButton.jsx
+++ client/src/views/Login/Material UI/loginButton.jsx
@@ -1,20 +1,21 @@
 import * as React from "react";
-import { styled } from "@mui/material/styles";
-import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
-import { blue } from "@mui/material/colors";
-
-const ColorButton = styled(Button)(({ theme }) => ({
-  color: theme.palette.getContrastText(blue[500]),
-  "&:hover": {
-    backgroundColor: blue[700],
-  },
-}));
 
 export default function CustomizedButtons() {
   return (
     <Stack spacing={2} direction="row">
-      <ColorButton variant="outlined">Ingresar</ColorButton>
+      <ColorButton
+        variant="outlined"
+        sx={{
+          color: "white",
+          borderColor: "#ae2dff",
+          "&:hover": {
+            borderColor: "#a020f0",
+          },
+        }}
+      >
+        Ingresar
+      </ColorButton>
     </Stack>
   );
 }
