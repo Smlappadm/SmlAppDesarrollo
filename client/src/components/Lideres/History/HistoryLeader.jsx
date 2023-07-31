cea1207cc6c59424d21c0b1037162a15adc81eca
Nikitoo17 <nicolasfadel17@gmail.com>
2023-07-31 16:43:54 -0300

    Update HistoryLeader.jsx

--- client/src/components/Lideres/History/HistoryLeader.jsx
+++ client/src/components/Lideres/History/HistoryLeader.jsx
@@ -170,41 +170,6 @@ export const LideresHistory = () => {
               Historial Empleados
             </Title>
             <NavBar />
-            {/* {titles.map((encabezado, index) => (
-              <Link
-                className="flex items-center justify-center gap-2 "
-                to={encabezado.link}
-                key={index}
-              >
-                {React.createElement(encabezado.icon, {
-                  className: `text-[2rem] text-[#418df0] ${
-                    encabezado.isHovered ? "hover:text-[#3570bd]" : ""
-                  }`,
-                  onMouseEnter: () => handleMouseEnter(index),
-                  onMouseLeave: () => handleMouseLeave(index),
-                })}
-                <motion.div
-                  className="flex items-center gap-2 text-white h-full "
-                  style={{
-                    width: encabezado.isHovered ? "fit-content" : "2rem",
-                  }}
-                  initial={{ width: 0 }}
-                  animate={{ width: encabezado.isHovered ? 100 : 0 }}
-                  transition={{ duration: 0.3, ease: "easeInOut" }}
-                >
-                  {encabezado.isHovered && (
-                    <motion.p
-                      className="text-white"
-                      initial={{ opacity: 0, x: -10 }}
-                      animate={{ opacity: 1, x: 0 }}
-                      transition={{ duration: 0.3, ease: "easeInOut" }}
-                    >
-                      {encabezado.title}
-                    </motion.p>
-                  )}
-                </motion.div>
-              </Link>
-            ))} */}
           </div>
 
           <label>Leads chequeados: {showData.length}</label>
