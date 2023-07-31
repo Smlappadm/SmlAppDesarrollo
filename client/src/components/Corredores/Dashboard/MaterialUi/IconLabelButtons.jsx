fe12f6be1d0c000e201e398c2bfedb5f14b299a0
Jonavoe <voeffray.jonathan@gmail.com>
2023-07-31 16:02:05 -0300

    Update IconLabelButtons.jsx

--- client/src/components/Corredores/Dashboard/MaterialUi/IconLabelButtons.jsx
+++ client/src/components/Corredores/Dashboard/MaterialUi/IconLabelButtons.jsx
@@ -1,12 +1,16 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import SendIcon from '@mui/icons-material/Send';
-import Stack from '@mui/material/Stack';
+import * as React from "react";
+import Button from "@mui/material/Button";
+import SendIcon from "@mui/icons-material/Send";
+import Stack from "@mui/material/Stack";
 
 export default function IconLabelButtons() {
   return (
     <Stack>
-      <Button variant="outlined" endIcon={<SendIcon />}>
+      <Button
+        variant="outlined"
+        style={{ color: "white", borderColor: "#ae2dff" }}
+        endIcon={<SendIcon style={{ color: "#ae2dff" }} />}
+      >
         Enviar
       </Button>
     </Stack>
