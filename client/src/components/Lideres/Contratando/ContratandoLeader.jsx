d71e64e8c58ce2570a47c18d4c0ed398670b0d92
Nikitoo17 <nicolasfadel17@gmail.com>
2023-08-01 14:59:30 -0300

    Update ContratandoLeader.jsx

--- client/src/components/Lideres/Contratando/ContratandoLeader.jsx
+++ client/src/components/Lideres/Contratando/ContratandoLeader.jsx
@@ -215,9 +215,6 @@ export const ContratandoLeader = () => {
       <Card className="w-full h-full bg-[#222131] rounded-none p-5">
         <div className="flex justify-between mx-5 mb-10">
           <div className="flex gap-5">
-            <Title className="font-bold text-[#e2e2e2] w-40 text-lg mx-5 mt-2">
-              Seguimiento
-            </Title>
             <NavBar />
           </div>
 
