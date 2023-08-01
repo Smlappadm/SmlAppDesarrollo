950a2c65f43a061fddc57b03dbd26d234b3dc0e8
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-08-01 19:17:43 -0300

    Update Modal.jsx

--- client/src/components/Vendedores/Dashboard/Modal/Modal.jsx
+++ client/src/components/Vendedores/Dashboard/Modal/Modal.jsx
@@ -1585,13 +1585,11 @@ export default function NestedModal({
                     >
                       Observaciones
                     </label>
-                    {/* <div className="flex justify-center items-center"> */}
                     <textarea
                       onChange={handleObservationChange}
                       type="text"
                       id="last_name"
                       name="observacion"
-                      // value={llamadoVenta.observaciones}
                       className="mt-3 bbg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-72 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                       placeholder=""
                       required
