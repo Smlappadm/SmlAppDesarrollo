eafb538cdf04d305fcbd90c71b276673067d8af8
Nikitoo17 <nicolasfadel17@gmail.com>
2023-08-01 16:04:08 -0300

    Update NavBar.jsx

--- client/src/components/C-Level/Employees/NavBar/NavBar.jsx
+++ client/src/components/C-Level/Employees/NavBar/NavBar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { IoGrid, IoCashOutline } from "react-icons/io5";
+import { FaBan } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 
