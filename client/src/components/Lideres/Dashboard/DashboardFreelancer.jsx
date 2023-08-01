677c06a29294453e6777e56d7d37934da622ec70
Nikitoo17 <nicolasfadel17@gmail.com>
2023-08-01 14:59:53 -0300

    Update DashboardFreelancer.jsx

--- client/src/components/Lideres/Dashboard/DashboardFreelancer.jsx
+++ client/src/components/Lideres/Dashboard/DashboardFreelancer.jsx
@@ -159,9 +159,6 @@ export const DashboardFreelancer = () => {
       <Card className="w-full h-full bg-[#222131] rounded-none p-5">
         <div className="flex justify-between mx-5 mb-10 ">
           <div className="flex gap-5">
-            <Title className="font-bold text-[#e2e2e2] w-40 text-lg mx-5 mt-2">
-              Historial Freelancer
-            </Title>
             <NavBar />
           </div>
 
