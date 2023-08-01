e7513ab966cd10f10a59bcc936a742b8aad5bfe8
Nikitoo17 <nicolasfadel17@gmail.com>
2023-08-01 16:08:34 -0300

    Update TableClevel.jsx

--- client/src/components/C-Level/Employees/TableEmployees/TableClevel.jsx
+++ client/src/components/C-Level/Employees/TableEmployees/TableClevel.jsx
@@ -10,7 +10,6 @@ import NestedModalEdit from "./MaterialUi/Edit/NestedModalEdit";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { IoGrid } from "react-icons/io5";
-import { Link } from "react-router-dom";
 import NavBar from "../NavBar/NavBar";
 
 const BannedEmployees = (name) => {
