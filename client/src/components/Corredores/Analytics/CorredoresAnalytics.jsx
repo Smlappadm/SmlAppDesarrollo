ae8ea0e755a0e24f4edbf8e225415179d74eeea5
Nikitoo17 <nicolasfadel17@gmail.com>
2023-08-01 16:10:34 -0300

    Update CorredoresAnalytics.jsx

--- client/src/components/Corredores/Analytics/CorredoresAnalytics.jsx
+++ client/src/components/Corredores/Analytics/CorredoresAnalytics.jsx
@@ -11,9 +11,6 @@ const CorredoresAnlaytics = () => {
       <div className="w-full m-5 bg-[#222131]">
         <div className="flex justify-between items-center">
           <div className="flex  mt-5 ">
-            <h1 className="font-bold text-[#e2e2e2] w-28 text-lg mx-5 mt-2">
-              History
-            </h1>
             <NavBar />
           </div>
         </div>
