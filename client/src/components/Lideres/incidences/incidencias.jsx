de474d351394f13cc8346e4ef6661eeef85712e0
Nikitoo17 <nicolasfadel17@gmail.com>
2023-08-02 15:40:03 -0300

    Update incidencias.jsx

--- client/src/components/Lideres/incidences/incidencias.jsx
+++ client/src/components/Lideres/incidences/incidencias.jsx
@@ -142,15 +142,11 @@ const Incidences = () => {
           <div className="text-white text-14 font-thin ">
             <div className="flex items-center justify-around p-3  ">
               <div className="flex justify-center items-center p-0">
-                <button onClick={() => handleOrderByClient()}>
-                  <Text className="text-start w-28 p-0 text-white">
-                    {headerClient()}
-                  </Text>
-                </button>
+                <Text className="text-start w-28 p-0 text-white">Cliente</Text>
               </div>
               <div className="flex justify-center items-center p-0">
                 <Text className="text-start w-28 p-0 text-white">
-                  Categoría
+                  Profesión
                 </Text>
               </div>
               <div className="flex justify-center items-center p-0">
