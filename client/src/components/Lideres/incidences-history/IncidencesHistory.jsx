cc8536ba010959572ffb2f51f77b510c8acff646
Nikitoo17 <nicolasfadel17@gmail.com>
2023-08-02 14:12:27 -0300

    Update IncidencesHistory.jsx

--- client/src/components/Lideres/incidences-history/IncidencesHistory.jsx
+++ client/src/components/Lideres/incidences-history/IncidencesHistory.jsx
@@ -187,13 +187,7 @@ export const IncidencesHistory = () => {
                   </Text>
                 </button>
               </div>
-              <div className="flex justify-center items-center p-0">
-                <button onClick={() => handlerFilter("status")}>
-                  <Text className="text-center w-48 p-0 text-white">
-                    Estado
-                  </Text>
-                </button>
-              </div>
+              <div className="flex justify-center items-center p-0 w-48"></div>
             </div>
           </div>
 
