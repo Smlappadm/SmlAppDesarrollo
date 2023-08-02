493de94967fd777029f3541079e26b2e4a3da333
Nikitoo17 <nicolasfadel17@gmail.com>
2023-08-02 14:55:39 -0300

    Update ModalDescartado.jsx

--- client/src/components/Lideres/incidences/MaterialUi/ModalDescartado.jsx
+++ client/src/components/Lideres/incidences/MaterialUi/ModalDescartado.jsx
@@ -1,7 +1,22 @@
 import { Box, Modal } from "@mui/material";
 import React from "react";
 
-export default function ModalDescartado(open, close) {
+const style = {
+  position: "absolute",
+  top: "40%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "rgb(55 65 81)",
+  boxShadow: 24,
+  p: 4,
+  textColor: "white",
+  color: "white",
+  height: 300,
+  borderRadius: "20px",
+};
+
+export default function ModalDescartado({ open, close }) {
   return (
     <div>
       <Modal
@@ -11,12 +26,14 @@ export default function ModalDescartado(open, close) {
         aria-describedby="modal-modal-description"
         BackdropProps={{
           sx: {
-            backgroundColor: "rgba(00, 00, 00, 0.3)",
+            backgroundColor: "rgba(00, 00, 00, 0.7)",
           },
         }}
       >
         <Box sx={style}>
-          <div className="flex flex-col justify-between h-full"></div>
+          <div className="flex flex-col justify-between h-full w-full">
+            <p>Hola</p>
+          </div>
         </Box>
       </Modal>
     </div>
