c864ea162e3822909752351470eabfe69fc71a44
Nikitoo17 <nicolasfadel17@gmail.com>
2023-08-03 18:18:46 -0300

    Update AnalyticLeader.jsx

--- client/src/components/Lideres/Analytic/AnalyticLeader.jsx
+++ client/src/components/Lideres/Analytic/AnalyticLeader.jsx
@@ -160,9 +160,7 @@ export const AnalyticLeader = () => {
                 </button>
               </div>
               <div className="flex justify-center items-center p-0">
-                <button onClick={() => handlerFilter("level")}>
-                  <Text className="text-center w-6 p-0 text-white">Nivel</Text>
-                </button>
+                <Text className="text-center w-6 p-0 text-white">Nivel</Text>
               </div>
               <div className="flex justify-center items-center p-0">
                 <Text className="text-center w-6 p-0 text-white">Web</Text>
