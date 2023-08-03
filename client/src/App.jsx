d703347e7f5bc536cad03a87e8ff5a3e08f6e00b
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-08-02 21:15:51 -0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -155,8 +155,8 @@ function ClerkProviderWithRoutes() {
           <Route
             path="/sign-in/*"
             element={
-              // redirectUrl === "/protected"  ? (
-              redirectUrl === "/protected" || redirectUrl === null ? (
+              redirectUrl === "/protected"  ? (
+              // redirectUrl === "/protected" || redirectUrl === null ? (
                 <SignIn
                   routing="path"
                   path="/sign-in"
