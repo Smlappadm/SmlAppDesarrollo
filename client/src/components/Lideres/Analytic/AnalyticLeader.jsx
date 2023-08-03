1bd61ce90def65ba5aeca2c4f50851726430f0f8
Nikitoo17 <nicolasfadel17@gmail.com>
2023-08-03 18:18:55 -0300

    Update AnalyticLeader.jsx

--- client/src/components/Lideres/Analytic/AnalyticLeader.jsx
+++ client/src/components/Lideres/Analytic/AnalyticLeader.jsx
@@ -153,11 +153,9 @@ export const AnalyticLeader = () => {
                 </button>
               </div>
               <div className="flex justify-center items-center p-0">
-                <button onClick={() => handleOrderByCategory()}>
-                  <Text className="text-start w-28 p-0 text-white">
-                    {headerCategory()}
-                  </Text>
-                </button>
+                <Text className="text-start w-28 p-0 text-white">
+                  {headerCategory()}
+                </Text>
               </div>
               <div className="flex justify-center items-center p-0">
                 <Text className="text-center w-6 p-0 text-white">Nivel</Text>
