52227278d6ee71aa40b31772dbd4bb7bc64b1bf7
Nikitoo17 <nicolasfadel17@gmail.com>
2023-08-03 18:24:42 -0300

    Update ContratandoLeader.jsx

--- client/src/components/Lideres/Contratando/ContratandoLeader.jsx
+++ client/src/components/Lideres/Contratando/ContratandoLeader.jsx
@@ -281,11 +281,7 @@ export const ContratandoLeader = () => {
                 <Text className="pr-3 text-center text-white">Chequeado</Text>
               </div>
               <div className="flex justify-center items-center p-0">
-                <button onClick={() => handlerFilter("sellers")}>
-                  <Text className="text-start w-28 p-0 text-white">
-                    Corredor
-                  </Text>
-                </button>
+                <Text className="text-start w-28 p-0 text-white">Corredor</Text>
               </div>
               <div className="flex justify-center items-center p-0">
                 <button onClick={() => handlerFilter("sellers")}>
