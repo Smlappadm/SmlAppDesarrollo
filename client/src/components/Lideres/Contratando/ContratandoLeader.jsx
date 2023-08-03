01adac6b13ba8f6b65c450eb9e27785d60a79ebb
Nikitoo17 <nicolasfadel17@gmail.com>
2023-08-03 18:26:05 -0300

    Update ContratandoLeader.jsx

--- client/src/components/Lideres/Contratando/ContratandoLeader.jsx
+++ client/src/components/Lideres/Contratando/ContratandoLeader.jsx
@@ -117,7 +117,6 @@ export const ContratandoLeader = () => {
   };
   const [clientOrder, setClientOrder] = useState("");
   const [categoryOrder, setCategoryOrder] = useState("");
-  const [filters, setFilters] = useState(false);
 
   const [open, setOpen] = useState(false);
   const [modalItems, setModalItems] = useState([]);
