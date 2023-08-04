f2f16bd6363add0d93e0afa6b865be2ac8afed67
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-08-04 19:01:16 -0300

    Update ResponsiveDateTimePickers.jsx

--- client/src/components/Freelance/Ventas/Dashboard/Modal/ResponsiveDateTimePickers.jsx
+++ client/src/components/Freelance/Ventas/Dashboard/Modal/ResponsiveDateTimePickers.jsx
@@ -5,7 +5,6 @@ import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { StaticDateTimePicker } from "@mui/x-date-pickers/StaticDateTimePicker";
 import "dayjs/locale/es";
-import style from "../Modal/ResponsiveDateTimePickers.module.css";
 
 export default function ResponsiveDateTimePickers({
   closeDateHour,
