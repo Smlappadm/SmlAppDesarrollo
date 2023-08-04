8032f1646c14875f2da65ec4a984e14ea88b0b36
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-08-04 19:54:03 -0300

    Update ModalHistory.jsx

--- client/src/components/Freelance/Ventas/analytics/Modal/ModalHistory.jsx
+++ client/src/components/Freelance/Ventas/analytics/Modal/ModalHistory.jsx
@@ -9,7 +9,6 @@ import { useUser } from "@clerk/clerk-react";
 import { AiOutlineInfoCircle } from "react-icons/ai";
 import ResponsiveDateTimePickers from "./ResponsiveDateTimePickers";
 import { ToastContainer, toast } from "react-toastify";
-import ClipboardJS from 'clipboard';
 // import toast, { Toaster } from 'react-hot-toast';
 import { CiWarning, CiInstagram, CiMail, CiGlobe } from "react-icons/ci";
 import { motion, spring } from "framer-motion";
@@ -556,34 +555,15 @@ export default function NestedModal({
     setUpdatedEmail(inputEmail);
   }, [updatedEmail]);
 
-  // const handleCopyClick = (copyToProps) => {
-  //   navigator.clipboard
-  //     .writeText(copyToProps)
-  //     .then(() => {
-  //       setShowCopiedMessage(true);
-  //       setTimeout(() => setShowCopiedMessage(false), 2000);
-  //     })
-  //     .catch((err) => alert(`Error al copiar: ${err}`));
-  // };
   const handleCopyClick = (copyToProps) => {
-    const clipboard = new ClipboardJS('.copy-button', {
-      text: () => copyToProps,
-    });
-  
-    clipboard.on('success', (e) => {
-      setShowCopiedMessage(true);
-      setTimeout(() => setShowCopiedMessage(false), 2000);
-    });
-  
-    clipboard.on('error', (e) => {
-      alert(`Error al copiar: ${e.text}`);
-    });
-  
-    clipboard.onClick(e);
+    navigator.clipboard
+      .writeText(copyToProps)
+      .then(() => {
+        setShowCopiedMessage(true);
+        setTimeout(() => setShowCopiedMessage(false), 2000);
+      })
+      .catch((err) => alert(`Error al copiar: ${err}`));
   };
-
-
-
 
   const handleOpen = () => {
     setOpen(true);
@@ -1065,19 +1045,16 @@ export default function NestedModal({
                       Copiar Link Pago
                     </p>
                     {window.location.hostname.includes("localhost") && (
-                      <button className="copy-button w-16 text-[#fff] font-bold flex justify-center gap-5 items-center rounded-xl py-2 ml-2 bg-[#474646] hover:bg-[#3f437a] cursor-pointer" onClick={() => handleCopyClick(`http://localhost:5173/promocion-pagos?emailApp=${inputEmailApp}`)}>
+                    <p
+                      onClick={() =>
+                        handleCopyClick(
+                          `http://localhost:5173/promocion-pagos?emailApp=${inputEmailApp}`
+                        )
+                      }
+                      className=" w-16 text-[#fff] font-bold flex justify-center gap-5 items-center rounded-xl py-2 ml-2 bg-[#474646] hover:bg-[#3f437a] cursor-pointer"
+                    >
                       Link
-                    </button>
-                    // <p
-                    //   onClick={() =>
-                    //     handleCopyClick(
-                    //       `http://localhost:5173/promocion-pagos?emailApp=${inputEmailApp}`
-                    //     )
-                    //   }
-                    //   className=" w-16 text-[#fff] font-bold flex justify-center gap-5 items-center rounded-xl py-2 ml-2 bg-[#474646] hover:bg-[#3f437a] cursor-pointer"
-                    // >
-                    //   Link
-                    // </p>
+                    </p>
                     )}
                     {window.location.hostname.includes("sml-app.vercel") && (
                     <p
