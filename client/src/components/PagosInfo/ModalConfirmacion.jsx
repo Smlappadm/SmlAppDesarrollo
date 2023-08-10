6ce790f9d07dd980f11dde60f74138c7ee0c0506
Nikitoo17 <nicolasfadel17@gmail.com>
2023-08-10 19:53:53 -0300

    Update ModalConfirmacion.jsx

--- client/src/components/PagosInfo/ModalConfirmacion.jsx
+++ client/src/components/PagosInfo/ModalConfirmacion.jsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import axios from "axios";
 import { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
