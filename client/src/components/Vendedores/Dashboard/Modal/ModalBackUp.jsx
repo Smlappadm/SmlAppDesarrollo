b58f16ab1c3707be0ed81c90df9eb24ee8c75fa9
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-08-10 17:31:55 -0300

    Update ModalBackUp.jsx

--- client/src/components/Vendedores/Dashboard/Modal/ModalBackUp.jsx
+++ client/src/components/Vendedores/Dashboard/Modal/ModalBackUp.jsx
@@ -7,7 +7,10 @@ import styles from "./Modal.module.css";
 import { CiEdit } from "react-icons/ci";
 import { MdPriceCheck } from "react-icons/md";
 import { useUser } from "@clerk/clerk-react";
-
+import ResponsiveDateTimePickers from "./ResponsiveDateTimePickers";
+import { ToastContainer, toast } from "react-toastify";
+import { CiWarning, CiInstagram, CiMail } from "react-icons/ci";
+import { motion, spring } from "framer-motion";
 import {
   AiOutlineConsoleSql,
   AiOutlinePhone,
