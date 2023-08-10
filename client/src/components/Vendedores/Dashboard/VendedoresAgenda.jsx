e805f683f762c4829852060eb6c96b63478b0c75
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-08-10 17:35:28 -0300

    Update VendedoresAgenda.jsx

--- client/src/components/Vendedores/Dashboard/VendedoresAgenda.jsx
+++ client/src/components/Vendedores/Dashboard/VendedoresAgenda.jsx
@@ -381,7 +381,7 @@ const VendedoresAgenda = () => {
                           )}
                           {item.status === "Agenda llamada" && (
                             <div className="bg-[#5bac42] w-44 h-11 flex flex-col justify-center items-center text-white rounded-3xl text-16">
-
+                              <p>Agenda llamada</p>
                             </div>
                           )}
                           {item.status === "En proceso" && (
