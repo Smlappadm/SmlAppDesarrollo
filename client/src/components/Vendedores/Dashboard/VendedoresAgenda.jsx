01f8c6246a0e6c388e033ae1fbf4e9ab386a62a3
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-08-16 18:28:05 -0300

    Update VendedoresAgenda.jsx

--- client/src/components/Vendedores/Dashboard/VendedoresAgenda.jsx
+++ client/src/components/Vendedores/Dashboard/VendedoresAgenda.jsx
@@ -43,6 +43,12 @@ const VendedoresAgenda = () => {
   // const body = { name: fullName, email: emailAddress };
   const body = { email: emailAddress };
 
+
+  const [loader, setLoader] = useState(false);
+  const loaderFuncion = (status) => {
+    setLoader(status);
+  };
+
   useEffect(() => {
     dispatch(
       getLeadsLLamadaVenta(body, profesion, country, status, level, freelancer)
