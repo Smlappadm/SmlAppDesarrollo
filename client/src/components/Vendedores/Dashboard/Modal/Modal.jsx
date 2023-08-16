0e0de9c91862a174e9051614c5fcf3aef8345b61
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-08-16 19:42:35 -0300

    Update Modal.jsx

--- client/src/components/Vendedores/Dashboard/Modal/Modal.jsx
+++ client/src/components/Vendedores/Dashboard/Modal/Modal.jsx
@@ -14,7 +14,7 @@ import { useLocation } from "react-router-dom";
 
 const style = {
   position: "absolute",
-  top: "45%",
+  top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
   width: 400,
